633cbfe00a3cddc2df5667f624cc0986fa549413
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-03-07 10:41:48 +0000

    Sync LeetCode submission Runtime - 5 ms (1.85%), Memory - 39.5 MB (100.00%)

---
+++ solutions/0031-next-permutation/solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public void nextPermutation(int[] arr) {
+         int a=0,b=0, index=0, s1=0, s2=0;
+        int n= arr.length;
+        for(int i=n-1;i>0;i--)
+        {
+             index= i-1;
+            if(arr[i]>arr[index])
+            {
+                index= i;
+                s1=index-1;
+                a= arr[index-1];
+                 b=arr[index];
+                break;
+            }
+            }
+      if(index !=0)
+      {
+        for(int j=index;j<n;j++)
+        {
+            if(arr[j]>a && arr[j]<=b )
+            {
+                b=arr[j];
+                 s2= j;
+            }
+        }
+        int temp=a;
+        arr[s1]= b;
+        arr[s2]= temp;
+      }
+        for(int i=arr.length-1; i>index; i--)
+        {
+            int swap= arr[index];
+            arr[index]= arr[i];
+            arr[i]= swap;
+            index++;
+        }
+        System.out.println("just next Permutation is ");
+          System.out.println(Arrays.toString(arr));
+       
+        
+    }
+}
