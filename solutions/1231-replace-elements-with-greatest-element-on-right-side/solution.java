61feef12665f23d8480cb4b5133bc55917e47a81
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-03-20 08:31:02 +0000

    Sync LeetCode submission Runtime - 1 ms (99.98%), Memory - 40.1 MB (100.00%)

---
+++ solutions/1231-replace-elements-with-greatest-element-on-right-side/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        if(arr.length==1) 
+        {
+            arr[0]=-1;
+            return arr;
+        }
+		 int max=-1;
+        for(int i=arr.length-1 ; i>=0 ; i--)
+        {
+            int a = arr[i];
+            arr[i]=max;
+            max=Math.max(max,a);
+        }
+        return arr;
+        
+    }
+}
