455721a5d09dc71aefe86acbd2e6e52518ecfc58
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-04-03 14:15:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 38.2 MB (100.00%)

---
+++ solutions/0045-jump-game-ii/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0, currentJumpEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currentJumpEnd) // if we have reach the current point jha se jump start ki thi ya krni h so jump++ kr dege and currentJump ko update kr dge ki ab wha se max kha tk ja sjte h.
+            {
+                jumps++;
+                currentJumpEnd = farthest;
+            }
+        }
+        return jumps;
+        
+    }
+}
