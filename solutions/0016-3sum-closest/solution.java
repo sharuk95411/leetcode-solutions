3c0a3be4f354ec28e681b97921919ef8b9bc2918
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-04-07 12:47:00 +0000

    Sync LeetCode submission Runtime - 3 ms (100.00%), Memory - 38.3 MB (100.00%)

---
+++ solutions/0016-3sum-closest/solution.java
@@ -0,0 +1,27 @@
+// Help from Nich White Video
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+	int closest = 0;
+	int minDiff = Integer.MAX_VALUE;
+	for (int i = 0; i < nums.length; i++) {
+		int j = i + 1;
+		int k = nums.length - 1;
+		while (j < k) {
+			int sum = nums[i] + nums[j] + nums[k];
+			if (sum == target) // agr is line ko na likhe to TC double ho jygi so while coding write each possibility . bcz yha sum target k brabr hne pr hme exact ans mil jyga so phr loop q chalana.
+				return sum;
+			if (sum < target)
+				j++;
+			else
+				k--;
+			int diff = Math.abs(sum - target);
+			if (diff < minDiff) {
+				minDiff = diff;
+				closest = sum;
+			}
+		}
+	}
+	return closest;
+    }
+}
