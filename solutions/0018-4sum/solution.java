e2ca710e55a1cfd4307a44936526f1ac139425b2
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-04-12 18:36:39 +0000

    Sync LeetCode submission Runtime - 16 ms (94.33%), Memory - 39.2 MB (100.00%)

---
+++ solutions/0018-4sum/solution.java
@@ -0,0 +1,46 @@
+// Logic TECh DOSE
+class Solution {
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+            HashSet<List<Integer>>h= new HashSet<>();
+        
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-3;i++)
+        {
+            //if(i>0 && arr[i]==arr[i-1]) continue; 
+            for(int j=i+1;j<arr.length-2;j++)
+            {
+               // if(j>i+1 && arr[j]==arr[j-1]) continue;
+                
+                int left=j+1;
+                int right= arr.length-1;
+                while(left<right)
+                {
+                    int sum= arr[i]+arr[j]+arr[left]+arr[right];
+                    if(sum==target)
+                    {  
+                        List<Integer>list= new ArrayList<>();
+                        list.add(arr[i]);
+                        list.add(arr[j]);
+                        list.add(arr[left]);
+                        list.add(arr[right]);
+                        h.add(list);
+                        left++;
+                        }
+                        else if(sum>target)right--;
+                        else left++;
+                        
+                        
+                }
+            }
+        }
+    
+      
+    List<List<Integer>>result= new ArrayList<>();
+        for(List l1: h)
+        {
+            result.add(l1);
+        }
+        return result;
+        
+    }
+}
