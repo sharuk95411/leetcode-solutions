c638c517275974fc3384fd1a8d3dad53e858d414
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-05-06 22:57:39 +0000

    Sync LeetCode submission Runtime - 25 ms (77.44%), Memory - 40.8 MB (100.00%)

---
+++ solutions/1320-remove-all-adjacent-duplicates-in-string-ii/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+         Stack<pair>stack= new Stack<>();
+       stack.push(new pair(1,s.charAt(0)));
+       for(int i=1;i<s.length();i++)
+       {
+           if(stack.size()>0 && stack.peek().c==s.charAt(i))
+           {
+               int count=stack.pop().freq;
+               stack.push(new pair(count+1,s.charAt(i)));
+           }
+           else
+           {
+               stack.push(new pair(1,s.charAt(i)));
+           }
+           if(stack.peek().freq==k)
+           {
+               stack.pop();
+           }
+       }
+       StringBuilder st= new StringBuilder();
+        while(stack.size()>0)
+       {
+           while(stack.peek().freq>0)
+           {
+               st.append(stack.peek().c);
+               stack.peek().freq--;
+           }
+           stack.pop();
+       }
+       
+        return(st.reverse().toString());
+        
+    }
+     class pair
+    {
+        char c;
+        int freq;
+        pair(int freq,char c)
+        {
+            this.freq=freq;
+            this.c=c;
+        }
+    }
+}
