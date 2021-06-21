977732dad9a1f6147fb9bc65e30b5a4cfe7df07e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-06-21 12:35:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 38.3 MB (100.00%)

--- solutions/0033-search-in-rotated-sorted-array/solution.java
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Isme hme rotated array dia jyga and ye rotation kse hga wo bataya gya h quesion me so hme rotated array me element index find krna h 
     public int search(int[] arr, int target) {
            int start=0;
         int end= arr.length-1;
@@ -13,7 +14,7 @@ class Solution {
             {
                 if(target>=arr[start] && target<=arr[mid])
                 {
-                    end= mid-1;;
+                    end= mid-1;
                 }
                 else
                 {
