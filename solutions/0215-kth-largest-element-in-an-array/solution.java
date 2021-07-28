27dee174fcce1f423dfb1d2c00ec58eb5fd259ee
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-07-28 06:46:02 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 39.6 MB (100.00%)

---
+++ solutions/0215-kth-largest-element-in-an-array/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findKthLargest(int[] arr, int k) {
+        int n= arr.length;
+        int ans=0;
+         for(int i=n/2-1;i>=0;i--)
+        heapify(arr,i,n);
+        
+        for(int i=0;i<k;i++)
+        {
+        ans=arr[0];
+        arr[0]=arr[n-1];
+        n--;
+        heapify(arr,0,n);
+        }
+        return ans;
+    }
+     public void heapify(int a[],int i,int n)
+    {
+        int max=i;
+        int l=2*i+1;
+        int r=2*i+2;
+        if(l<n && a[l]>a[max])
+        max=l;
+        if(r<n && a[r]>a[max])
+        max=r;
+        if(i!=max)
+        {
+            int swap=a[i];
+            a[i]=a[max];
+            a[max]=swap;
+            heapify(a,max,n);
+        }
+    }
+}
