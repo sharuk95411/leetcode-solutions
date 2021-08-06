7e7e0413a211c63c5fa61d2bbe783e71361d7f2e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-08-06 05:42:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.8 MB (100.00%)

---
+++ solutions/0733-flood-fill/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+     
+       if(sr<0 || sc>=image[0].length || sc<0 || sr>=image.length )
+           return image;
+      if( image[sr][sc]== newColor) return image;
+        int currC= image[sr][sc];
+        fill(image,sr,sc,currC,newColor);
+        return image;
+    }
+     public void fill (int[][] image, int sr, int sc, int currcolor, int newColor) {
+    if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] !=  currcolor) return;
+        image[sr][sc] = newColor;
+        fill(image, sr + 1, sc, currcolor, newColor);
+        fill(image, sr - 1, sc, currcolor, newColor);
+        fill(image, sr, sc + 1, currcolor, newColor);
+        fill(image, sr, sc - 1, currcolor, newColor);
+    }
+
+}
