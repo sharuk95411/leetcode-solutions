e97abb8fa43d9f5a0f577a5a266c2795f0352f9d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-08-06 10:02:09 +0000

    Sync LeetCode submission Runtime - 26 ms (47.38%), Memory - 39.2 MB (100.00%)

---
+++ solutions/0133-clone-graph/solution.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node){
+        if(node==null) return null;
+        Map<Node,Node>map= new HashMap<>();
+        Queue<Node>q= new LinkedList<>();
+        q.add(node);
+        map.put(node ,new Node(node.val,new ArrayList<Node>()));
+        
+        while(q.size()>0)
+        {
+            Node h= q.poll();
+            
+            for(Node neighbor: h.neighbors)
+            {
+            if(map.containsKey(neighbor)==false)
+            {
+                map.put(neighbor,new Node(neighbor.val,new ArrayList<Node>()));
+                q.add(neighbor);
+            }
+                map.get(h).neighbors.add(map.get(neighbor));
+            }
+        }
+        
+        
+        return map.get(node);
+    }
+}
