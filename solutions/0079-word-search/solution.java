281cae7117a30432e8943f732cdd89a8c2a40228
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-08-31 14:49:50 +0000

    Sync LeetCode submission Runtime - 86 ms (97.00%), Memory - 37.2 MB (100.00%)

--- solutions/0079-word-search/solution.java
+++ solutions/0079-word-search/solution.java
@@ -1,29 +1,38 @@
 class Solution {
+    boolean ans= false;
     public boolean exist(char[][] board, String word) {
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0) && dfs(board, word, 0, i, j))
-                    return true;
+     if(board.length==0)return ans;
+        else if(word.length()==0) return ans;
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(board[i][j]==word.charAt(0))
+                {
+                    dfs(board,word,i,j,0);
+                        if(ans==true) return ans;
+                }
             }
         }
-        return false;
-        
+        return ans;
     }
-     public static boolean dfs(char[][] board, String word, int index,  int i, int j){
-        if(index == word.length())
-            return true;
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index))
-            return false;
-       
+    
+    private void dfs(char board[][],String word,int i,int j,int index)
+    {
+        if(index==word.length())
+        {
+            ans= true;
+            return ;
+        }
+    if(i<0 || j<0|| i>=board.length|| j>=board[0].length || board[i][j]!=word.charAt(index)) return;
+        
         char temp = board[i][j];
-        board[i][j] = '*'; // yha boolean array ki jgh is trike ko use kia gya h jisse hmra extra space save ho rha h 
-        
-        boolean result = dfs(board, word, index + 1, i-1, j) || 
-                         dfs(board, word, index + 1, i+1, j) || 
-                         dfs(board, word, index + 1, i, j-1) || 
-                         dfs(board, word, index + 1, i, j+1);
-        
-        board[i][j] = temp; // BackTraking
-        return result;
+        board[i][j]= '*';// yha boolean array ki jgh is trike ko use kia gya h jisse hmra extra space save ho rha h 
+        dfs(board,word,i,j+1,index+1);
+        dfs(board,word,i+1,j,index+1);
+        dfs(board,word,i,j-1,index+1);
+        dfs(board,word,i-1,j,index+1);
+        board[i][j]= temp; // Backtraking
+        return ;
     }
 }
