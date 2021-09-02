4abc8ac719e2aeadc2c99c39143949f25390e10f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-02 08:03:26 +0000

    Sync LeetCode submission Runtime - 1 ms (99.85%), Memory - 49.5 MB (100.00%)

--- solutions/0238-product-of-array-except-self/solution.java
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -1,50 +1,40 @@
 class Solution {
-    // logic self and prefer Notes if not able to solve
-    public int[] productExceptSelf(int[] arr) {
-     int n= arr.length;
-        int product=1;
-       int count =0;
-       int index=-1;
-       for(int i=0;i<n;i++)
-       {
-           if(arr[i]==0 &&count ==0 )
-           {
-               index=i;
-               count ++;
-               continue;
-           }
-           else if(arr[i]==0&& count>0)
-           {
-               product=0;
-           }
-           else
-           {
-               product=product*arr[i];
-           }
-       }
-       if(product==0)
-       {
-           for(int i=0;i<n;i++)
-           {
-               arr[i]=0;
-           }
-       }
-       else if(count==1)
-       {
-           for(int i=0;i<n;i++)
-           {
-               if(i==index) arr[i]=product;
-               else
-               arr[i]=0;
-           }
-       }
-       else
-       {
-           for(int i=0;i<n;i++)
-           {
-               arr[i]= product/arr[i];
-           }
-       }
-        return arr;
+    public int[] productExceptSelf(int[] nums) {
+        int arr[]= new int[nums.length];
+        
+        int count=0; 
+        int a=1;
+        int index_of_zero=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0) 
+            {count++;
+             index_of_zero=i;
+            }
+            else
+            {
+                a= nums[i]*a;
+            }
+            
+        }
+        if(count>1)
+        {
+            Arrays.fill(arr, 0);
+            return arr;
+        }
+        else if(count==1)
+        {
+            Arrays.fill(arr,0);
+            arr[index_of_zero]=a;
+            return arr;
+        }
+        else
+        {
+            for(int i=0;i<nums.length;i++)
+            {
+                arr[i]= a/nums[i];
+            }
+            return  arr;
+        }
     }
 }
