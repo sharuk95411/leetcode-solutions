e475629c99f0d1efb1abac31d7abfa775acdc645
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-02 06:59:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 56.1 MB (99.90%)

--- solutions/0189-rotate-array/solution.java
+++ solutions/0189-rotate-array/solution.java
@@ -1,26 +1,21 @@
 class Solution {
-    public void rotate(int[] arr, int k) {
-         k= k%arr.length; // ye line imp h when arr [1,2,3] k=4 and ans is arr[3,1,2] bcz yha k ki value 1 aygi arr ki length 3 h to 3 steps reverse krne pr whi arr ayga and 1 steps hme aur roatte krna hga but k ko modulo krne se only 1 times krna pdega .ye tb k liye h jb k>= arr.length. isme k wp steps h jitna hme rotate krna h na ki index no;
+    public void rotate(int[] nums, int k) {
+        k = k%nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
         
-        int end= arr.length-k-1;
-        int start=0;
-        reverse(start,end,arr); // reverse the left half until <k
-        start=arr.length-k;
-        end= arr.length-1;
-           reverse(start,end,arr);
-            start=0;
-        end= arr.length-1;
-        reverse(start,end,arr);
     }
-    static void reverse(int start,int end,int arr[])
-        {
-        while(start<end && start>=0)
+    
+   private void reverse( int arr[],int start,int end)
+    {
+        while(start<end)
         {
             int temp= arr[end];
-            arr[end]= arr[start];
+            arr[end]=arr[start];
             arr[start]= temp;
             start++;
             end--;
         }
-        }
+    }
 }
