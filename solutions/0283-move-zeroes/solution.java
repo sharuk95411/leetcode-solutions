a709bbf98e4c2c9ed23b98b168baa5ca578ed17b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-02 09:01:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.8 MB (100.00%)

--- solutions/0283-move-zeroes/solution.java
+++ solutions/0283-move-zeroes/solution.java
@@ -1,24 +1,22 @@
 class Solution {
-    public void moveZeroes(int[] arr) {
-     
-        int temp=0;
-        int curr=0;
-        for(int i=0;i<arr.length-1;i++)
-        {
-            if(arr[i]==0 && arr[i+1]==0)
-            {
-                continue;
-            }
-            if(arr[i]==0 && arr[i+1]!=0)
-            {
-                temp= arr[i+1];
-                arr[i+1]= arr[curr];
-                arr[curr]= temp;
-                
-            }
-            curr++;
+//isko O(1) space me krne me mza a and h very simple isme non zero digit ko left shift krna h bs 
+    public void moveZeroes(int[] nums) {
         
+        
+        int index=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[index]=nums[i];
+                index++;
+            }
+            
         }
-
+        while(index<nums.length)
+        {
+            nums[index]=0;
+            index++;
+        }
     }
 }
