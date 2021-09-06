bc80b4bcffbbb97203bf83e7906904664212ee09
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-06 05:33:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 37.6 MB (100.00%)

--- solutions/0289-game-of-life/solution.java
+++ solutions/0289-game-of-life/solution.java
@@ -1,86 +1,73 @@
 class Solution {
-    public void gameOfLife(int[][] arr) {
-          int row= arr.length;
-        int col= arr[0].length;
-        int arr1[][]= new int[row][col];
-         for(int i=0;i<row;i++)
-         {
-             for(int j=0;j<col;j++)
-             {
-                 arr1[i][j]= arr[i][j];
-             }
-         }
-         int one=0, zero=0;
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                int x= arr[i][j];
-                
-               if(i<row && j-1>=0 ) // left 
-                {
-                    int left= arr[i][j-1];
-                    if(left==1)one++;
-                    else zero++;
-                }
-                if(i>=0 && j+1<col)// right
-                {
-                    int right= arr[i][j+1];
-                    if(right==1)one++;
-                    else zero++;
-                }
-                 if(i-1>=0 &&j<col) // up
-                {
-                    int up= arr[i-1][j];
-                    if(up==1)one++;
-                    else zero++;
-                }
-                 if(i+1<row && j>=0) // down
-                 {
-                     int down= arr[i+1][j];
-                     if(down==1)one++;
-                    else zero++;
-                 }
-                 if(i-1>=0 && j+1<col)
-                 
-                 {
-                     int a= arr[i-1][j+1];
-                    if(a==1)one++;
-                    else zero++;
-                 }
-                 if(i-1>=0 && j-1>=0)
-                 {
-                     int b= arr[i-1][j-1];
-                     if(b==1)one++;
-                    else zero++;
-                 }
-                 if(i+1<row && j+1<col)
-                 {
-                     int c= arr[i+1][j+1];
-                     if(c==1)one++;
-                    else zero++;
-                 }
-                 if(i+1<row && j-1>=0)
-                 {
-                     int d= arr[i+1][j-1];
-                     if(d==1)one++;
-                    else zero++;
-                 }
-                 if(x==0 && one==3) arr1[i][j]=1;
-                 else if ((x==1)&&(one <2 || one>3)) arr1[i][j]=0 ;
-                   
-                 one =0;
-                 zero=0;
-            }
-            
-        }
- for(int i=0;i<row;i++) // yha new array ka data orignal arrray me copy kr rhe h
-         {
-             for(int j=0;j<col;j++)
-             {
-                 arr[i][j]= arr1[i][j];
-             }
-         }
+    public void gameOfLife(int[][] board) {
+        int arr[][]= new int[board.length][board[0].length];
         
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                A(i,j,board,arr);
+            }
+        }
+        
+         for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                board[i][j]= arr[i][j];
+            }
+        }
+        
+        
+    }
+    private void A(int i,int j,int board[][],int arr[][])
+    {
+        int count_live=0,count_dead=0;
+        if(i<board.length && j+1<board[0].length)
+        {
+        if(board[i][j+1]==1) count_live++;
+        else count_dead++;
+        }
+        
+         if(i+1<board.length && j+1<board[0].length)
+         {
+        if(board[i+1][j+1]==1) count_live++;
+        else count_dead++;
+         }
+         if(i+1<board.length && j<board[0].length)
+        if(board[i+1][j]==1) count_live++;
+        else count_dead++;
+        
+         if(i+1<board.length && j-1>=0)
+        if(board[i+1][j-1]==1) count_live++;
+        else count_dead++;
+        
+         if(i<board.length && j-1>=0)
+        if(board[i][j-1]==1) count_live++;
+        else count_dead++;
+        
+         if(i-1>=0 && j-1>=0)
+        if(board[i-1][j-1]==1) count_live++;
+        else count_dead++;
+        
+         if(i-1>=0 && j<board[0].length)
+        if(board[i-1][j]==1) count_live++;
+        else count_dead++;
+        
+         if(i-1>=0 && j+1<board[0].length)
+        if(board[i-1][j+1]==1) count_live++;
+        else count_dead++;
+        
+        if(board[i][j]==0)
+        {
+            if(count_live==3) arr[i][j]=1;
+    
+        }
+        else
+        {
+            if(count_live<2) arr[i][j]=0;
+            else if(count_live==2 || count_live==3) arr[i][j]=1;
+            else if(count_live>3)arr[i][j]=0;
+        }
     }
 }
