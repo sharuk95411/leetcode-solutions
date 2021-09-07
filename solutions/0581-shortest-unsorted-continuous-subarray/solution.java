681e1797a7c18fb79b757f97f3ae53783149fd43
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-07 08:15:38 +0000

    Sync LeetCode submission Runtime - 1 ms (91.00%), Memory - 39.9 MB (100.00%)

--- solutions/0581-shortest-unsorted-continuous-subarray/solution.java
+++ solutions/0581-shortest-unsorted-continuous-subarray/solution.java
@@ -1,46 +1,35 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-         
-          // base cases
         
-        int n = nums.length;
         
-        if(n<=1)
-        return 0;
-        
-        //iterate from beginning of array
-        //find the last element which is smaller than the last seen max from 
-        //its left side and mark it as end
-        
-        int end = -2;
-        
-        int mx  = Integer.MIN_VALUE;
-        
-        for(int i=0;i<n;i++)
+     /*   int  arr[] = nums.clone();
+        Arrays.sort(arr);
+        int start_index=nums.length,end_index=0;
+        for(int i=0;i<nums.length;i++)
         {
-            mx = Math.max(mx,nums[i]);
-            
-            if(nums[i]<mx)
-                end = i;
+            if(nums[i]!=arr[i]) 
+            {
+                 start_index=Math.min(start_index,i);
+                end_index= Math.max(end_index,i);
+            }
         }
         
-        //iterate from end of array
-        //find the last element which is bigger than the last seen min from 
-        //its right side and mark it as begin
+            if(end_index==0) return 0;
+        else return end_index-start_index+1;*/
         
-        int begin = -1;
-        
-        int mn  = Integer.MAX_VALUE;
-        
-        for(int i=n-1;i>=0;i--)
+       int low=-1,high=nums.length-1;
+        int a= Integer.MAX_VALUE;
+        for(int i= nums.length-1;i>=0;i--)
         {
-            mn = Math.min(mn,nums[i]);
-            
-            if(nums[i]>mn)
-                begin = i;
+            a= Math.min(a,nums[i]);
+            if(nums[i]>a) low=i;
         }
-        
-        return end - begin + 1;
-        
+        a= Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            a= Math.max(a,nums[i]);
+            if(a>nums[i]) high=i;
+        }
+        return low==-1 ? 0: high-low+1;
     }
 }
