7a31de91f1ab1864cc4643d9a063aaae0c213342
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-08 05:44:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 38.3 MB (100.00%)

--- solutions/1421-find-numbers-with-even-number-of-digits/solution.java
+++ solutions/1421-find-numbers-with-even-number-of-digits/solution.java
@@ -17,9 +17,8 @@ class Solution {
         int count = 0;
         
         for(int num: nums) {
-            if(num > 9 && num < 100) count++;
-            if(num > 999 && num < 10000) count++;
-            if(num > 99999 && num < 1000000) count++;
+if((num > 9 && num < 100) ||(num > 999 && num < 10000) ||(num > 99999 && num < 1000000) )
+    count++;
         }
         return count;
     }
