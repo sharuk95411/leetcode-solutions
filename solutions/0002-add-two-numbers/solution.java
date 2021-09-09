e96670a1819d31677c4b8a502aa93c50c706b2c7
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-09 21:37:28 +0000

    Sync LeetCode submission Runtime - 2 ms (6.09%), Memory - 39 MB (100.00%)

--- solutions/0002-add-two-numbers/solution.java
+++ solutions/0002-add-two-numbers/solution.java
@@ -1,4 +1,3 @@
-// logic Hello World isko copy paste kia h to self try krna only logic dkhkr
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,23 +10,25 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ans = new ListNode(0);
-        ListNode p = l1, q=l2, curr = ans;
-        int carry=0;
-        while(p!=null || q!=null){
-            int x = (p!=null) ? p.val : 0;
-            int y = (q!=null) ? q.val : 0;
-            int sum = carry + x+ y;
-            carry = sum/10;
-            curr.next = new ListNode(sum%10);
-            curr = curr.next;
-            if(p!=null) p = p.next;
-            if(q!=null) q = q.next;
-        }
-        if(carry>0){
-            curr.next = new ListNode(carry);
-        }
-        return ans.next;
         
+        ListNode dummy= new ListNode(0);
+        ListNode temp1=l1,temp2=l2,curr=dummy;
+        int carry=0;
+        while(temp1!=null || temp2!=null)
+        {
+            int a= temp1!=null ? temp1.val: 0;
+            int b= temp2!=null ? temp2.val: 0;
+            int sum= carry+a+b;
+            carry= sum/10;
+            curr.next= new ListNode(sum%10);
+            curr= curr.next;
+            if(temp1!=null) temp1= temp1.next;
+            if(temp2!=null) temp2= temp2.next;
+        }
+        if(carry>0)
+        {
+            curr.next= new ListNode(carry);
+        }
+        return dummy.next;
     }
 }
