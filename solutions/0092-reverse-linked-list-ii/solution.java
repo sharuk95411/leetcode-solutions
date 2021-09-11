7413df61d8e258c6fb33d0103161fd5adcd83a99
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-11 14:53:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 38.9 MB (100.00%)

--- solutions/0092-reverse-linked-list-ii/solution.java
+++ solutions/0092-reverse-linked-list-ii/solution.java
@@ -1,6 +1,3 @@
-// Logic Self
-// step 1- find the postion of Nodes Left and right jha ki list reverse krni h
-// step 2- Now is list ko revere kro and left se phle ek pointer rkh lo and right k bd ka taki list reverse hne k bad merge kr skte .
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,50 +10,31 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-    if(left==right) return head;
-    int a=1;
-     ListNode m=head, n=head;
-     ListNode prev=head,current= head;
-     ListNode temp= head;
-     while(temp!=null)
-     {
-         if(a==left)
-         {
-             n=m;
-             prev= temp;
-         }
-         else if(a==right)
-         {
-             current= temp;
-             break;
-         }
-         a++;
-         m=temp;
-         temp=temp.next;
-       
-     }
-     m= current.next;
-     ListNode r= reverse(prev,current);
-     n.next=r;
-     prev.next=m;
-     if(left==1)
-     return r;
-     else return head;
-        
-    }
-    static ListNode reverse(ListNode l1,ListNode l2)
- {
-    ListNode prev=null;
-    ListNode curr=l1,next=l1.next;
-    while(curr!=l2)
-    {
-         next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        int pos=1;
+        ListNode dummy= new ListNode(0),prev=dummy,curr= head;
+        while(pos!=left)
+        {
+            prev.next=curr;
+            prev=curr;
+            curr=curr.next;
+            pos++;
+        }
+    
+    ListNode a= curr;
+        ListNode b=curr.next;
+        while(pos<right)
+        {
+            ListNode c= b.next;
+            b.next=a;
+            a=b;
+            b=c;
+            pos++;
             
+            
+        }
+        prev.next=a;
+        curr.next=b;
+         return dummy.next;
     }
-    curr.next=prev;
-    return curr;
- }
+   
 }
