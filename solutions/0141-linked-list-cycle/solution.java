ee8be4a2acfffb9a1e69284e5ed0c1e12ef7c0e3
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-13 09:56:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.9 MB (100.00%)

--- solutions/0141-linked-list-cycle/solution.java
+++ solutions/0141-linked-list-cycle/solution.java
@@ -18,15 +18,12 @@ public class Solution {
 
         ListNode slow = head;
         ListNode fast = head.next;
-    while (slow != fast) // agr cycle bnegi to wo kbhi na kbhi ek dsre k equal zrur hge ye wo cond h
-        {
-            if (fast ==null || fast.next==null) //yha fast ka case null kb hga wo h ek br even no of nodes rkh lo without cycle and ek br odd so ye condition smjh aa jygi 
-            {
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
                 return false;
             }
             slow = slow.next;
-            fast = fast.next.next; // yha two steps age islye move kia gya h agr ek step krege to cycle hne pr h kbhi slow and fast equal ni ho pyge and loop infinite chlte rhega
-        
+            fast = fast.next.next;
         }
         return true;
 
