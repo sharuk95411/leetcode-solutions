30cd7421dd55ca0838c2dcfc70b844c7429d861a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-14 08:32:48 +0000

    Sync LeetCode submission Runtime - 10 ms (52.51%), Memory - 53.4 MB (100.00%)

--- solutions/0148-sort-list/solution.java
+++ solutions/0148-sort-list/solution.java
@@ -10,70 +10,57 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null)
-       return head;
-       ListNode mid= getmid(head);
-       ListNode l1= sortList(head);
-       ListNode l2= sortList(mid);
-       return merge (l1,l2);
+        
+        if(head==null || head.next==null) return head;
+        
+        ListNode mid= getmid(head);
+      ListNode left= sortList(head);
+        ListNode right= sortList(mid);
+        return merge(left,right);
         
     }
-    static ListNode getmid(ListNode head)
-   {
-       ListNode prev= null;
-       ListNode slow= head;
-        ListNode fast= head;
+    private ListNode getmid(ListNode head)
+    {
+        ListNode slow= head,fast= slow.next,prev=slow;
+        
         while(fast!=null && fast.next!=null)
         {
-            prev= slow;
+            prev=slow;
             slow= slow.next;
             fast= fast.next.next;
         }
-        prev.next=null;// yha listNode ko 2 parts me break kia gya h bcz slow mid node dega so mid wle se pehle
-       // wla me null rkhna h jisse list 2 parts me devide ho jygi
-        return(slow);
-   }
-   static ListNode merge(ListNode l1,ListNode l2)
-   {
-       if(l1==null)return l2;
-       if(l2==null)return l1;
-         ListNode head= l1;
-        ListNode small= null;
-        if(l1.val> l2.val) // yhe head ko km value wale node me rkhna h
+        if(fast==null)
         {
-         small= l2;
-            head= l2;
-            l2= l2.next;
+            prev.next=null;
+            return slow;
         }
         else
         {
-         small =l1;
-        l1=l1.next;
+            prev=slow.next;
+            slow.next=null;
+            return prev;
         }
-       ListNode current= head;
-        while(l1!=null&& l2!=null)
+    }
+    private ListNode merge(ListNode l1,ListNode l2)
+    {
+        ListNode dummy= new ListNode(0),curr= dummy;
+        while(l1 !=null && l2!=null)
         {
-            if(l2.val>l1.val)
-            {
-                current.next= l1;
-                current= l1;
-                l1= l1.next;
-         }
-            else
-            {
-                current.next= l2;
-                current=l2;
-                l2=l2.next;
-            }
-        }
-        if(l1!=null)
+        if(l1.val<=l2.val)
         {
-            current.next=l1;
+        curr.next= l1;
+            l1=l1.next;
+            
         }
         else
         {
-            current.next=l2;
+            curr.next=l2;
+            l2=l2.next;
         }
-        return small;
-   }
+        curr=curr.next;
+        }
+        curr.next= l1!=null ? l1:l2;
+        return dummy.next;
+    }
+ 
 }
