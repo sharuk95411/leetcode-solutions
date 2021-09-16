88d87c0a85b9d57e8473d4a568d51c856f301c9c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-16 14:17:23 +0000

    Sync LeetCode submission Runtime - 1 ms (60.10%), Memory - 42.1 MB (100.00%)

--- solutions/0042-trapping-rain-water/solution.java
+++ solutions/0042-trapping-rain-water/solution.java
@@ -1,80 +1,28 @@
-// MonoTonic Stack ka concept use hua h isme TC is O(n) but SC is O(n).
-//Monotonic stack k bre me notes me likha h.
 class Solution {
     public int trap(int[] arr) {
-        if(arr.length<3)
-        {
-            return 0;
-        }
+      
         
-        int leftMax[]= new int[arr.length];
-        leftMax[0]=0;
-        Stack<Integer>s1= new Stack<>();
-        s1.push(arr[0]);
-        for(int i=1;i<arr.length;i++)
+        int trapped_water=0;
+        int left_max= arr[0];
+        int right_max= arr[arr.length-1];
+        int left= 1;
+            int right= arr.length-2;
+        while(left<=right)
         {
-            if(s1.peek()>arr[i])
+            if(right_max>left_max)
             {
-                leftMax[i]=s1.peek();
+                if(left_max>arr[left])trapped_water= trapped_water+(left_max-arr[left]);
+                left_max=Math.max(left_max,arr[left]);
+                left++;
+                
             }
             else
             {
-                while(s1.size()>0 && arr[i]>s1.peek())
-                {
-                    s1.pop();
-                }
-                if(s1.size()==0)
-                {
-                    leftMax[i]=0;
-                }
-                else
-                {
-                    leftMax[i]= s1.peek();
-                }
-                s1.push(arr[i]);
+                if(right_max>arr[right]) trapped_water= trapped_water+(right_max-arr[right]);
+                right_max= Math.max(right_max,arr[right]);
+                    right--;
             }
         }
-        
-        int rightMax[]= new int[arr.length];
-        
-        leftMax[arr.length-1]=0;
-        Stack<Integer>s2= new Stack<>();
-        s2.push(arr[arr.length-1]);
-        for(int i= arr.length-2;i>=0;i--)
-        {
-           if(s2.peek()>arr[i])
-            {
-                rightMax[i]=s2.peek();
-            }
-            else
-            {
-                while(s2.size()>0 && arr[i]>s2.peek())
-                {
-                    s2.pop();
-                }
-                if(s2.size()==0)
-                {
-                    rightMax[i]=0;
-                }
-                else
-                {
-                    rightMax[i]= s2.peek();
-                }
-                s2.push(arr[i]);
-            }
-        }
-        
-        int result=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            int min= Math.min(leftMax[i],rightMax[i]);
-            int z= min-arr[i];
-            if(z>0)
-            {
-                result=result+z;
-            }
-        }
-        return(result);
-        
+        return trapped_water;
     }
 }
