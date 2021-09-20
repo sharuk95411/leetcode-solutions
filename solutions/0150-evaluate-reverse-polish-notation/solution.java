90ea77f8b19e684463018537c0437b597681c949
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-09-20 15:13:00 +0000

    Sync LeetCode submission Runtime - 4 ms (97.63%), Memory - 38.2 MB (100.00%)

--- solutions/0150-evaluate-reverse-polish-notation/solution.java
+++ solutions/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,33 +1,39 @@
 class Solution {
-    public int evalRPN(String[] s1) {
-     
-              Stack<Integer>stack= new Stack<>();
-        
-    for(String s :s1)
-      {
-          if("+-/*".contains(s))
-          {
-                 char ch=s.charAt(0);
-                 int v1= stack.pop();
-                 int v2=stack.pop();
-                 int ans= operation(v1,v2,ch);
-                 stack.push(ans);
-          }
-          else
-          {
-              stack.push(Integer.parseInt(s));
-          }
-      } 
-        return stack.peek();
-    }
-     static int operation(int v1,int v2,char ch)
-    {
-        if(ch=='+') return v1+v2;
-        else if (ch=='-') return v2-v1;
-        else if(ch=='/') 
+    public int evalRPN(String[] arr) {
+        int ans=0;
+        if(arr.length==0) return ans;
+        Stack<Integer>s= new Stack<>();
+        for(int i=0;i<arr.length;i++)
         {
-        return v2/v1;
+            if(arr[i].equals("+")==true) 
+            {
+                int a =s.pop();
+                int b= s.pop();
+                s.push(a+b);
+            }
+           else if(arr[i].equals("-")==true) 
+            {
+                                int a =s.pop();
+                int b= s.pop();
+                s.push(b-a);
+            }
+            else if(arr[i].equals("/")==true) 
+            {
+                                int a =s.pop();
+                int b= s.pop();
+                s.push(b/a);
+            }
+           else if(arr[i].equals("*")==true) 
+            {
+                                int a =s.pop();
+                int b= s.pop();
+                s.push(a*b);
+            }
+            else
+            {
+                s.push(Integer.parseInt(arr[i]));
+            }
         }
-        else return v1*v2;
+            return s.peek();
     }
 }
