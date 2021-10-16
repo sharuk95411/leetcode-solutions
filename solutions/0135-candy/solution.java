fe3f0c9fe25912bc245b4f7a6de80ee7c579a59d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-16 10:45:12 +0000

    Sync LeetCode submission Runtime - 2 ms (99.46%), Memory - 40.6 MB (100.00%)

---
+++ solutions/0135-candy/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    // TC O(n) SC O(n) logic from TECHDOSE TYN
+// Optimal- TC O(n) and SC O(1) ye bd me krna h 
+    public int candy(int[] arr) {
+        
+        int n = arr.length;
+        int left[]= new int[n];
+        Arrays.fill(left,1);
+        int right[]= new int[n];
+        Arrays.fill(right,1);
+        
+        for(int i=1;i<n;i++)
+        {
+            if( arr[i]>arr[i-1])
+            {
+                left[i]= left[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                right[i]= right[i+1]+1;
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            count=count+Math.max(left[i],right[i]);
+        }
+        return count;
+    }
+}
