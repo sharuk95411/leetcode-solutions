9673600775b21c2fbec01d8de6b97b921bf265d8
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-17 16:03:00 +0000

    Sync LeetCode submission Runtime - 24 ms (31.69%), Memory - 38.9 MB (100.00%)

--- solutions/0005-longest-palindromic-substring/solution.java
+++ solutions/0005-longest-palindromic-substring/solution.java
@@ -1,39 +1,44 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        
-         int n= s.length();
-        int maxLength=1;
-        int start=0,high=0,low=0;
-        for(int i=1;i<n;i++)
-        { 
-            // when the length of palindromic substring is even
-             low=i-1;
-            high= i;
-            while(low>=0 && high<n && s.charAt(low)==s.charAt(high) )
-            {
-                if((high-low+1)>maxLength)
-                {
-                    start=low;
-                    maxLength= high-low+1;
-                }
-                low--;
-                high++;
-            }
-           // when the length of palindromic substring is odd
-            low=i-1;
-            high= i+1;
-            while(low>=0 && high<n && s.charAt(low)==s.charAt(high) )
-            {
-                if((high-low+1)>maxLength)
-                {
-                    start=low;
-                    maxLength= high-low+1;
-                }
-                low--;
-                high++;
-            }
-        }
-        return (s.substring(start,start+maxLength));
     
+    // Isko dp se krna h bcz ye qustion DP ka h 
+    // Approach- agr koi b string palindrome hgi to usme mid se left jane pr and right jane pr element equla ate jyge like aabaa yha b mid h and hm iske left and as well as right ja rhe h aur compare kr rhe h Charcaters ko so isse pta chl jyga ye palindrome h
+    // Ab agr string even hui aabb to hum dno ko equla part me devide krke whi check krne ka km start kr dege. left part me aa and right part me bb a jyga bs bs  yhi logic niche lgaya h
+    // TC O(n2) and SC O(1)
+    public String longestPalindrome(String s) {
+        // Nick White
+     
+        if (s == null || s.length() < 1) return "";
+    int start = 0, end = 0;
+        String ans= "";
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = expandAroundCenter(s, i, i); // for odd length;
+      //  System.out.println(len1);
+        
+        int len2 = expandAroundCenter(s, i, i + 1);// for even length
+    //    System.out.println(len2);
+        int len = Math.max(len1, len2);
+       // System.out.println(len);
+        if (len > end - start)  // yha len palindrome wli string ki length h agr ye phle se di hui plaindrome string ki length jo ki right-left se niklegi agr usse zyda hui tbhi ye andr jygi.
+        {
+            if(len>ans.length())// ye case uske liye h jb hme new palindromic string mili h but agr uski length already c
+            {
+            start = i - (len - 1) / 2;// ye strt position nikale ka hugad h
+            end = i + len / 2;  // ye end position of string nikale ka jugad h
+            ans= s.substring(start,end+1);
+            }
+            //length=start+end+1;
+            
+        }
     }
+    return ans;
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R - L - 1; // ye palindromic string ki length return kr rha h .
+}
 }
