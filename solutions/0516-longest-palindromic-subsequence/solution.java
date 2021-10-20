d2165ab4e9089792a0c879d2698786e0ffa6381b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-20 10:33:40 +0000

    Sync LeetCode submission Runtime - 131 ms (5.15%), Memory - 81.5 MB (6.13%)

--- solutions/0516-longest-palindromic-subsequence/solution.java
+++ solutions/0516-longest-palindromic-subsequence/solution.java
@@ -1,24 +1,36 @@
 class Solution {
-    public int longestPalindromeSubseq(String str1) {
-         StringBuilder sb= new StringBuilder(str1);
-        String str2= sb.reverse().toString();
-        int n= str1.length();
-	    int m= str2.length();
-	    int t[][]= new int[n+1][m+1];
-	    
-	    for(int i=1;i<n+1;i++)
-	    {
-	        for(int j=1;j<m+1;j++)
-	        {
-	           if(str1.charAt(i-1)==str2.charAt(j-1))
-	           {
-	               t[i][j]= 1+t[i-1][j-1];
-	           }
-	           else
-	           t[i][j]= Math.max(t[i-1][j],t[i][j-1] );
-	            
-	        }
-	    }
-	    return t[n][m];
+    public int longestPalindromeSubseq(String s1) {
+        
+        String s2="";
+    for(int i=s1.length()-1;i>=0;i--)
+    {
+        s2= s2+s1.charAt(i);
+    }
+        
+        int x= s1.length();
+        int y= s2.length();
+        
+        
+        int dp[][]= new int[x+1][y+1];
+        
+        for(int i=1;i<x+1;i++)
+        {
+            for(int j=1;j<y+1;j++)
+            {
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                {
+                    dp[i][j]= 1+dp[i-1][j-1];
+                    
+                }
+                else
+                {
+                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[x][y];
+        
+        
+        
     }
 }
