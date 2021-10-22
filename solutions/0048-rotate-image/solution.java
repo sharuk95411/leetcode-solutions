bd8892d1f0fcde24826ab2bcac080a8b978c815b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-22 12:01:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.4 MB (100.00%)

--- solutions/0048-rotate-image/solution.java
+++ solutions/0048-rotate-image/solution.java
@@ -1,21 +1,36 @@
 class Solution {
-    public void rotate(int[][] arr) {
-             Queue<Integer> q = new LinkedList<Integer>();
-          for(int i=0;i<arr.length;i++)
-          {
-              for(int j=arr.length-1;j>=0;j--)
-              {
-                  q.add(arr[j][i]);
-              }
-          }
-          
-for(int i=0;i<arr.length;i++)
-{
-    for(int j=0;j<arr.length;j++)
-    {
-      arr[i][j]= q.poll();
-    }
-}
+    public void rotate(int[][] matrix) {
+ if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+
+        int n = matrix.length;
+
+        // Rotate the original matrix on horizontal axis. (swap rows)
+        for (int i = 0; i < n / 2; i++) {
+            int[] tempRow = matrix[i]; // Only reference will be saved
+            matrix[i] = matrix[n - i - 1];
+            matrix[n - i - 1] = tempRow;
+        }
+
+    /*        for (int i = 0; i < n ; i++) { // Selecting a row
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+                System.out.println();
+        }*/
+        
+        
+        // Transpose the rotated matrix
+        for (int i = 0; i < n - 1; i++) { // Selecting a row
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
         
     }
 }
+
