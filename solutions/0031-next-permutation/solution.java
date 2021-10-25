18f4a365c7eb0ab0a1a80e264305b1bde91eeb6b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-25 09:32:38 +0000

    Sync LeetCode submission Runtime - 1 ms (10.36%), Memory - 40.6 MB (100.00%)

--- solutions/0031-next-permutation/solution.java
+++ solutions/0031-next-permutation/solution.java
@@ -1,45 +1,31 @@
 class Solution {
-    
-    public void nextPermutation(int[] arr) {
-         int a=0,b=0, index=0, s1=0, s2=0;
-        int n= arr.length;
-        if(n==1 || n==0) return ;
-        for(int i=n-1;i>0;i--)
-        {
-             index= i-1;
-            if(arr[i]>arr[index])
-            {
-                index= i;
-                s1=index-1;
-                a= arr[index-1];
-                 b=arr[index];
-                break;
-            }
-            }
-      if(index !=0)
-      {
-        for(int j=index;j<n;j++)
-        {
-            if(arr[j]>a && arr[j]<=b )
-            {
-                b=arr[j];
-                 s2= j;
-            }
-        }
-        int temp=a;
-        arr[s1]= b;
-        arr[s2]= temp;
-      }
-        for(int i=arr.length-1; i>index; i--)
-        {
-            int swap= arr[index];
-            arr[index]= arr[i];
-            arr[i]= swap;
-            index++;
-        }
-        System.out.println("just next Permutation is ");
-        //  System.out.println(Arrays.toString(arr));
-       
+    public void nextPermutation(int[] nums) {
         
+        int i= nums.length-2;
+        
+        while(i>=0 && nums[i]>=nums[i+1])i--;
+        if(i>=0)
+        {
+            int j= nums.length-1;
+            while(nums[i]>=nums[j]) j--;
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1,nums.length-1);
+    }
+    
+    private void swap(int nums[],int i,int j)
+    {
+        int temp= nums[i];
+        nums[i]= nums[j];
+        nums[j]= temp;
+    }
+    private void reverse(int nums[],int i,int j)
+    {
+        while(i<j)
+        {
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
     }
 }
