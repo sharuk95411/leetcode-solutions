c4ac7b41710aaf5c32728d97d4ea0ab592d22c07
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-25 07:01:30 +0000

    Sync LeetCode submission Runtime - 5 ms (47.43%), Memory - 42.4 MB (100.00%)

---
+++ solutions/0383-ransom-note/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String r, String m) {
+        
+        int freq[]= new int[26];
+        
+        for(int i=0;i<m.length();i++)
+        {
+            freq[m.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<r.length();i++)
+        {
+            if(freq[r.charAt(i)-'a']==0) return false;
+            else
+            {
+                freq[r.charAt(i)-'a']--;
+            }
+        }
+        return true;
+    
+    }
+}
