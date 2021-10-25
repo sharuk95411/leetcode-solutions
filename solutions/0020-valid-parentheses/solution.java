bb067f8b145a8df52e98dbc59195353962a4c7d7
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-25 11:23:25 +0000

    Sync LeetCode submission Runtime - 3 ms (87.19%), Memory - 38.8 MB (100.00%)

--- solutions/0020-valid-parentheses/solution.java
+++ solutions/0020-valid-parentheses/solution.java
@@ -1,53 +1,28 @@
-// good ques
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()==1 || s.length()%2 !=0)
-        {
-            return false;
-        }
+        
+    
         Stack<Character>stack= new Stack<>();
+        
         for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i)=='(' || s.charAt(i)=='{'|| s.charAt(i)=='[')
+            char c= s.charAt(i);
+            if(c==')' || c=='}'|| c==']')
             {
-                stack.push(s.charAt(i));
+                if(stack.size()==0) return false;
+                else
+                {
+                
+                    if(stack.peek()=='(' && c==')') stack.pop();
+                     else if(stack.peek()=='[' && c==']') stack.pop();
+                    else if(stack.peek()=='{' && c=='}') stack.pop();
+                    else return false;
+                }
                 
             }
-             if (stack.isEmpty()) 
-                
-             {return false ;}
-            
-            char check; 
-            switch (s.charAt(i)) { 
-            case ')': 
-                check = stack.pop(); 
-                if (check == '{' || check == '[') 
-                    return false; 
-                break; 
-  
-            case '}': 
-                check = stack.pop(); 
-                if (check == '(' || check == '[') 
-                    return false; 
-                break; 
-  
-            case ']': 
-                check = stack.pop(); 
-                if (check == '(' || check == '{') 
-                    return false; 
-                break; 
-            
-            
-            
+            else stack.push(c);
         }
-    }
-        if(stack.isEmpty())
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        
+        return stack.size()==0 ? true :false;
     }
 }
