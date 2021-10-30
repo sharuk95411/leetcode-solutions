219b315e7fcd8679fbde5f04d0ce111541a684e0
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-30 17:46:58 +0000

    Sync LeetCode submission Runtime - 4 ms (99.55%), Memory - 39 MB (100.00%)

---
+++ solutions/2163-kth-distinct-string-in-an-array/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        
+         Map<String,Integer> map=new HashMap<>();
+        
+        for(String s:arr){
+            
+            if(map.containsKey(s)) map.put(s,map.get(s)+1);
+             else map.put(s,1);
+        }
+        for(String s:arr){
+            if(map.get(s)==1){
+                if(k==1) 
+                    return s;
+                k--;
+                } 
+                 
+        }
+        return "";
+    }
+}
