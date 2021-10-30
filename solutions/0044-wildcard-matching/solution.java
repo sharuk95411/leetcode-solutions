a35e31793dea0079d74bc7311944c667b22745f8
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-10-30 09:07:15 +0000

    Sync LeetCode submission Runtime - 26 ms (61.20%), Memory - 41.4 MB (100.00%)

--- solutions/0044-wildcard-matching/solution.java
+++ solutions/0044-wildcard-matching/solution.java
@@ -1,61 +1,48 @@
 class Solution {
-     Boolean t[][];
-    public boolean isMatch(String s, String pattern) {
-        
-       t= new Boolean[s.length()+1][pattern.length()+1];
-       return B(s,pattern,0,0);
+    public boolean isMatch(String s, String p) {
+        Boolean dp[][]= new Boolean[s.length()+1][p.length()+1];
+        return A(s,0,p,0,dp);
     }
-    public boolean B(String s,String pattern,int i,int j)
-    {
-        if(i!=s.length() && j==pattern.length()) return false; // i show string length and j shows pattern length.
-        if(i==s.length()&& j== pattern.length()) return true;
-        
-        if(i==s.length()&& j!=pattern.length())
+    
+    private boolean A( String s,int i,String pattern,int j,Boolean dp[][])
+    {       
+        if(j==pattern.length() && i==s.length()) return true;
+       
+        if(i==s.length() && j!=pattern.length())
         {
-           int  index=j;
-            while(index<pattern.length())
+            for(int index=j;index<pattern.length();index++)
             {
                 if(pattern.charAt(index)!='*') return false;
-                
-            index++;
             }
             return true;
         }
-        if(t[i][j]!=null) return t[i][j];
-        boolean b=false;
-        if(pattern.charAt(j)=='?')
-        {
-            if(B(s,pattern,i+1,j+1))
-            {
-                b=true;
-            }
-            
-        }
-       else if(pattern.charAt(j)=='*')
-        {
-            if(B(s,pattern,i,j+1))
-            {
-                b=true;
-            }
-            if(B(s,pattern,i+1,j+1))
-            {
-                b=true;
-            }
-            if(B(s,pattern,i+1,j))
-            {
-                b=true;
-            }
-        }
+    if(j==pattern.length() && i!=s.length()) return false;
+    
+    
+    boolean ans = false;
+        if(dp[i][j]!=null) return dp[i][j];
+    
+    if(pattern.charAt(j)=='?')
+    {
+        if(A(s,i+1,pattern,j+1,dp))
+            ans= true;
+    }
+    else if(pattern.charAt(j)=='*')
+    {
+        if(A(s,i,pattern,j+1,dp)) ans= true;
+        if(A(s,i+1,pattern,j,dp)) ans= true;
+        if(A(s,i+1,pattern,j+1,dp)) ans= true;
+        
+    }
         else
         {
-            if(s.charAt(i)==pattern.charAt(j) && B(s,pattern,i+1,j+1))
-            {
-                    b=true;
-            }
-        
-        
-        }
-        t[i][j]=b;
-        return b;
+    if(s.charAt(i)==pattern.charAt(j))
+    {
+         if(A(s,i+1,pattern,j+1,dp)) ans=true;
     }
+        }
+        dp[i][j]= ans;
+    
+    return ans;
+    }    
 }
