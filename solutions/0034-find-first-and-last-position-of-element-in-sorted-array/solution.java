e7857bcff7e6ef974bf801ab161403877b6dc6c0
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-01 06:58:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (100.00%)

--- solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,53 +1,59 @@
-// simple logic h 2 binary serch mthd bnakr leftmost and rightmost element k liye chla do and ye sol 100 acceptance rate ay h iska.
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int a[]= new int[2];
-        
-        a[0]= binary_search(0,nums.length-1,nums,target);
-        if(a[0]==-1) return new int[]{-1,-1};
-         a[1]= binarysearch(0,nums.length-1,nums,target); 
-        
-        return a;
-       // return new int[]{1,2};
+     
+        int arr[]= new int[2];
+        arr[0]=startIndex(nums,target);
+     
+        arr[1]= endIndex(nums,target);
+       return arr;
     }
     
-    private int binary_search(int left,int right,int arr[],int target)
-    {
-        while(left<=right)
-        {
-            int mid = left+ (right-left)/2;
-            if(arr[mid]==target)
-            {
-                if(mid>0 && arr[mid-1]==target)
-                {
-                    right= mid-1;
-                }
-                else
-                    return mid;
-            }
-            else if(arr[mid]>target) right= mid-1;
-            else left=mid+1;
-        }
-        return -1;
-    }
-    private int binarysearch(int left,int right,int arr[],int target)
-    {
-        while(left<=right)
-        {
-            int mid= left+(right-left)/2;
-            if(arr[mid]==target)
-            {
-                if(mid<=arr.length-2 && arr[mid+1]==target)
-                {
-                    left= mid+1;
-                }
-                else
-                    return  mid;
-            }
-            if(arr[mid]>target) right= mid-1;
-            else left= mid+1;
-        }
-        return -1;
-    }
+     public int startIndex(int arr[],int target)
+   {
+       int index=-1;
+       int start=0;
+       int end= arr.length-1;
+       while(start<=end)
+       {
+           int mid= start+ (end-start)/2;
+           if(arr[mid]>=target)
+           {
+               end=mid-1;
+           }
+           else
+           {
+            start= mid+1;   
+           }
+           if(arr[mid]==target)
+           {
+               index=mid;
+           }
+       }
+    
+       return index;
+   }
+   public int endIndex(int arr[],int target)
+   {
+       int index=-1;
+       int start=0;
+       int end= arr.length-1;
+       while(start<=end)
+       {
+           int mid= start+ (end-start)/2;
+           if(arr[mid]<=target)
+           {
+               start=mid+1;
+           }
+           else
+           {
+            end= mid-1;   
+           }
+       
+       if(arr[mid]==target)
+       {
+           index= mid;
+       }
+       }
+       return index;
+   }
 }
