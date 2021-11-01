905b801442e47a4b419219dfccc302e87e3ec87e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-01 18:35:59 +0000

    Sync LeetCode submission Runtime - 1 ms (80.82%), Memory - 37.2 MB (100.00%)

--- solutions/0091-decode-ways/solution.java
+++ solutions/0091-decode-ways/solution.java
@@ -1,30 +1,30 @@
-// logic codebix.
-// bht pyra question h must try yourself.
-//Recursion me hum probelm ko subprbmlem me devide kr lte h jo ki isme hga isko phle recusion then Memoization then tabulation se krna .
-// isme hmne char ko integer me bdhle k liye function b seekha h.
-
 class Solution {
+    int count=0;
     public int numDecodings(String s) {
-        int n= s.length();
-        int t[]= new int[n+1];
-        if(n==0) return 0;
         
-        t[0]=1;
-        t[1]= s.charAt(0)!='0' ? 1:0 ;
-            for(int i=2;i<n+1;i++)
-            {
-                int first= Integer.parseInt(s.substring(i-1,i));
-                int second= Integer.parseInt(s.substring(i-2,i)) ;
-                    if(first>0 && first<=9)
-                    {
-                        t[i]= t[i-1];
-                        
-                    }
-                if(second>9 && second<=26)
-                {
-                    t[i]= t[i]+t[i-2];
-                }
-            }
-        return t[n];
+        int arr[]= new int[s.length()+1];
+          Arrays.fill(arr, -1);
+          return A(s,0,arr);
+        
+    }
+    
+    private int A(String s,int index,int arr[])
+    {
+        if(index==s.length()) return 1 ; 
+        
+        if(s.charAt(index)=='0') return 0;
+    
+        if(index==s.length()-1) return 1;
+        
+        if(arr[index]!=-1) return arr[index];
+        
+        arr[index]=  A(s,index+1,arr);
+    
+        if((Integer.parseInt(s.substring(index,index+2))<=26))
+        {
+               arr[index]= A(s,index+2,arr)+arr[index];
+            
+        }
+        return arr[index];
     }
 }
