3ceefa58a096e969f551c5f265730b82f5c99376
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-04 19:45:59 +0000

    Sync LeetCode submission Runtime - 60 ms (77.81%), Memory - 52.7 MB (100.00%)

--- solutions/0084-largest-rectangle-in-histogram/solution.java
+++ solutions/0084-largest-rectangle-in-histogram/solution.java
@@ -1,45 +1,40 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        // validate input
-        if(heights == null || heights.length == 0) {
-            return 0;
-        }
+   public int largestRectangleArea(int[] arr) {
+        // Logic- Pepcoding 
+        // TC O(n) and SC O(2n) 
+        int n= arr.length;
         
-        // init
-        int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int result = 0;
-        
-        // build left
-        left[0] = 0;
-        for(int i = 1; i < n; i++) {
-            int currentLeft = i-1;
-            while(currentLeft >= 0 && heights[currentLeft] >= heights[i]) {
-                currentLeft = left[currentLeft]-1;
+        int left[]= new int[n];
+        left[0]=0;
+        for(int i=1;i<n;i++)
+        {
+            int j=i-1;
+            while(j>=0 && arr[j]>=arr[i])
+            {//j--; and isse O(n2) me
+                j= left[j]-1;  // is Logic se ye O(n) me chl rha h and yhi main logic h is program ko O(n) me krne ka
             }
-                
-            left[i] = currentLeft+1;
+            left[i]= j+1;
         }
-        System.out.println(Arrays.toString(left));
-        
-        // build right
-        right[n-1] = n-1;
-        for(int i = n-2; i >= 0; i--) {
-            int currentRight = i+1;
-            while(currentRight < n && heights[i] <= heights[currentRight]) {
-                currentRight = right[currentRight]+1;
+         System.out.println(Arrays.toString(left));
+         int right[]= new int[n];
+        right[n-1]=n-1;
+        for(int i= n-2;i>=0;i--)
+        {
+            int j=i+1;
+            while(j<n && arr[j]>=arr[i])
+            {
+                //j++;  and isse O(n2) mw
+                j=right[j]+1; // is Logic se ye O(n) me chl rha h and yhi main logic h is program ko O(n) me krne ka
             }
-                
-            right[i] = currentRight-1;
+            right[i]= j-1;
         }
           System.out.println(Arrays.toString(right));
-        // compare height
-        for(int i = 0; i < n; i++) {
-            result = Math.max(result, (right[i]-left[i]+1)*heights[i]);
+        int area=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            area= Math.max(area,(right[i]-left[i]+1)*arr[i]);
         }
+        return area;
         
-        // return
-        return result;
     }
 }
