1f3e13ae1c3b032bc374a4eec3fda1e40ff56482
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-10 14:40:21 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 39 MB (100.00%)

--- solutions/0260-single-number-iii/solution.java
+++ solutions/0260-single-number-iii/solution.java
@@ -1,28 +1,25 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-     
-      // Pass 1 : 
-        // Get the XOR of the two numbers we need to find
-        int diff = 0;
-        for (int num : nums) {
-            diff ^= num;
-        }
-        // Get its last set bit
-        diff &= -diff;
-        
-        // Pass 2 :
-        int[] rets = {0, 0}; // this array stores the two numbers we will return
-        for (int num : nums)
+      
+     int temp=0;
+        for(int a :nums)
         {
-            if ((num & diff) == 0) // the bit is not set
-            {
-                rets[0] ^= num;
-            }
-            else // the bit is set
-            {
-                rets[1] ^= num;
-            }
+            temp=temp^a;
         }
-        return rets;
+        temp= temp & -temp;
+        
+        int x=0;
+        int y=0;
+        
+        for(int a :nums)
+        {
+            if((temp & a)==0)
+            {
+                x=x^a;
+            }
+            else  y=y^a;
+        }
+        
+        return new int[]{x,y};
     }
 }
