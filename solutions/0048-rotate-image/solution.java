8bccc8e67675dd9d31fe291e4a74496cf2923ada
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-17 10:02:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39 MB (100.00%)

--- solutions/0048-rotate-image/solution.java
+++ solutions/0048-rotate-image/solution.java
@@ -1,4 +1,7 @@
 class Solution {
+    // Isko O(1) Space me krna h 
+    // Step 1- swap the horizonatal wise of row
+    // Step 2- Traverse the matrix
     public void rotate(int[][] matrix) {
  if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return;
@@ -7,22 +10,18 @@ class Solution {
         int n = matrix.length;
 
         // Rotate the original matrix on horizontal axis. (swap rows)
+        // yha hum row ko utha rhe h and puri row ko swal kr rhe h and isme jo kia gya h usse hme reference mil rhe h to ye new chz seekhi h single loop me swap krna.
         for (int i = 0; i < n / 2; i++) {
             int[] tempRow = matrix[i]; // Only reference will be saved
             matrix[i] = matrix[n - i - 1];
             matrix[n - i - 1] = tempRow;
         }
-
-    /*        for (int i = 0; i < n ; i++) { // Selecting a row
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j]+" ");
-            }
-                System.out.println();
-        }*/
+        
         
         
         // Transpose the rotated matrix
-        for (int i = 0; i < n - 1; i++) { // Selecting a row
+        // Yha diagonal wise traverse kr rhe h  and next time ye loop khd se bnane ko try krna 
+        for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
