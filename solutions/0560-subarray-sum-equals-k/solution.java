aa8b42b31965a367b4e7a535d96087a006a89686
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-19 05:18:07 +0000

    Sync LeetCode submission Runtime - 17 ms (99.88%), Memory - 41.4 MB (100.00%)

--- solutions/0560-subarray-sum-equals-k/solution.java
+++ solutions/0560-subarray-sum-equals-k/solution.java
@@ -1,7 +1,9 @@
 class Solution {
+    // Logic- Pepcoding 
+    // hint- PrefixSum And HashMap use hga with No recursion 
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer,Integer>h= new HashMap<>();
-        h.put(0,1);
+  h.put(0,1); // {3,2,6} and k=5 so in cases k liye hmne ye add kia h and ye vdio me btaya gya h
         int count=-0;
         int prefix_sum=0;
         
