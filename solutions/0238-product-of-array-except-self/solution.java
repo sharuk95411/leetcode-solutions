d380f413b3e20700e511bdab1e9d0dc2b43924d2
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-19 05:18:20 +0000

    Sync LeetCode submission Runtime - 1 ms (99.85%), Memory - 49.7 MB (100.00%)

--- solutions/0238-product-of-array-except-self/solution.java
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -1,19 +1,23 @@
+
 class Solution {
+    // Ye solution O(n) and O(1) me h and !00% submission rate h 
+    // Is question me only zero ka khyl rkhna h q ki prblm whi krega so whi hmne kia h 
     public int[] productExceptSelf(int[] nums) {
         int arr[]= new int[nums.length];
         
         int count=0; 
-        int a=1;
+        int multiplication_result=1;
         int index_of_zero=-1;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==0) 
-            {count++;
+            {
+                count++;
              index_of_zero=i;
             }
             else
             {
-                a= nums[i]*a;
+                multiplication_result= nums[i]*multiplication_result;
             }
             
         }
@@ -25,14 +29,14 @@ class Solution {
         else if(count==1)
         {
             Arrays.fill(arr,0);
-            arr[index_of_zero]=a;
+            arr[index_of_zero]=multiplication_result;
             return arr;
         }
         else
         {
             for(int i=0;i<nums.length;i++)
             {
-                arr[i]= a/nums[i];
+                arr[i]= multiplication_result/nums[i];
             }
             return  arr;
         }
