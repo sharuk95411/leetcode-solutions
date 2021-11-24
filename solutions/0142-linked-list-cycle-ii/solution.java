33e61b4c6f17e44f8551b97551d6e9aaf55576bf
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-11-24 16:15:28 +0000

    Sync LeetCode submission Runtime - 1 ms (19.28%), Memory - 41.8 MB (100.00%)

--- solutions/0142-linked-list-cycle-ii/solution.java
+++ solutions/0142-linked-list-cycle-ii/solution.java
@@ -10,8 +10,10 @@
  * }
  */
 public class Solution {
+    // logic Naresh Gupta and its easy logic
+    // hint slow and fast start with head . slow,fast= head;
+    // TC is O(2n)==O(n) and SC is O(1)
     public ListNode detectCycle(ListNode head) {
-        
         if(head==null) return null;
         ListNode slow= head,fast=head;
         while(fast!=null && fast.next!=null )
