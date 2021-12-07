f1b34d7d720272ac46a53923f1f85dc3356a5659
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-12-07 05:45:51 +0000

    Sync LeetCode submission Runtime - 8 ms (1.70%), Memory - 38.7 MB (100.00%)

--- solutions/1411-convert-binary-number-in-a-linked-list-to-integer/solution.java
+++ solutions/1411-convert-binary-number-in-a-linked-list-to-integer/solution.java
@@ -10,13 +10,41 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        int num=0;
-        while(head!=null)
-        {
-            num= 2*num+ head.val;
-            head= head.next;
-        }
-        return num;
         
+        if(head==null) return 0;
+        
+        int arr[]= new int[30];
+        
+        ListNode temp= head;
+        int i= 0;
+        int count=0;
+        while(temp!=null)
+        {
+            arr[i]= temp.val;
+            temp= temp.next;
+            i++;
+            count++;
+        }
+        
+       // System.out.println(Arrays.toString(arr));
+        System.out.println("count "+count);
+        int start_position= count-1;
+        int ans=0;
+        int multiplier=1;
+        while(start_position>=0)
+        {
+           // System.out.println("mul "+multiplier);
+            if(arr[start_position]!=0)
+            {
+                ans= ans+multiplier;
+               // System.out.println("ans "+ans);
+                
+            }
+            
+            start_position--;
+            multiplier=multiplier*2;
+            
+        }
+        return ans;
     }
 }
