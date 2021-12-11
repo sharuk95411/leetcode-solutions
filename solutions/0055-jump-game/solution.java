af39158dd820020f9175c9731a4efa2b2b689dee
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-12-11 09:02:41 +0000

    Sync LeetCode submission Runtime - 346 ms (7.03%), Memory - 40.5 MB (100.00%)

--- solutions/0055-jump-game/solution.java
+++ solutions/0055-jump-game/solution.java
@@ -1,17 +1,46 @@
 class Solution {
-    public boolean canJump(int[] arr) {
-      int reachable =0;
-        int i=0;
-        while(i<=reachable)
+    boolean dp[];
+    
+    public boolean canJump(int[] nums) {
+     
+        
+        if(nums.length==0) return false;
+        if(nums[0]==0 && nums.length==1) return true;
+        dp=new boolean[nums.length];
+        Arrays.fill(dp,true); 
+        return A(nums,0);
+        
+    }
+    
+    private boolean A(int arr[],int index)
+    {
+        
+        if(index==arr.length-1)
         {
-            reachable= Math.max(reachable,arr[i]+i);
-            if(reachable>=arr.length-1)
-            {
-                return true;
-            }
-            i++;
+            return true;
         }
-        return false;
+        if(arr[index]==0)
+        {
+            dp[index]=false;
+            return false;
+        }   
+        int previous= index;
+        int end= arr[index];
+        if(dp[index]!=true) return dp[index];
+        for(int i=1;i<=end;i++)
+        {
+           if( A(arr,i+previous))
+           {
+               dp[i]=true;
+               return true;
+           }
+        }
+        
+         dp[index]= false;
+        return dp[index];
+        
+        
+        
         
     }
 }
