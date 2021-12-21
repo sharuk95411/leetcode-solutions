6628f4f7d0b8a3100dc6174c9e6a19ed2c80459a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-12-21 06:58:28 +0000

    Sync LeetCode submission Runtime - 1 ms (96.56%), Memory - 35.9 MB (100.00%)

--- solutions/0231-power-of-two/solution.java
+++ solutions/0231-power-of-two/solution.java
@@ -1,18 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        
-/* check if n can be divided by 2. If yes, divide n by 2 and check it repeatedly.
-
-if (n <= 0) return false;
-while (n%2 == 0) n/=2;
-return n == 1;
-Time complexity = O(log n)
-
-*/
-        if(n<=0) return false;
+          if(n<=0) return false;
         
         return (n & (n-1))==0? true :false;
-        
-        
     }
 }
