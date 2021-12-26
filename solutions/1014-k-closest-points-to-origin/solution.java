945dda24e6ed9294afe50b6acd5f63d3251bb131
sharuk <78816538+sharuk95411@users.noreply.github.com>
2021-12-26 07:44:18 +0000

    Sync LeetCode submission Runtime - 250 ms (5.06%), Memory - 48.2 MB (100.00%)

--- solutions/1014-k-closest-points-to-origin/solution.java
+++ solutions/1014-k-closest-points-to-origin/solution.java
@@ -1,6 +1,4 @@
-class Solution {
-    public int[][] kClosest(int[][] p, int k) {
-    /*  Sol 1- TC is NlogN 
+ /*  Sol 1- TC is NlogN 
     Arrays.sort(p,(p1,p2)-> p1[0]*p1[0]+p1[1]*p1[1] -p2[0]*p2[0]-p2[1]*p2[1]);
         
         int ans[][]= new int[k][2];
@@ -9,8 +7,9 @@ class Solution {
             ans[i][0]= p[i][0];
             ans[i][1]= p[i][1];
         }
-        return ans;*/
-//PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        return ans; */
+//======================================================================================
+/*Soln 2- TC is Nlog(k)         
 PriorityQueue<int[]>pq= new PriorityQueue<>((p1,p2)->p2[0]*p2[0]+p2[1]*p2[1] -p1[0]*p1[0]-p1[1]*p1[1]);
             
             for(int arr[]: p)
@@ -29,6 +28,51 @@ PriorityQueue<int[]>pq= new PriorityQueue<>((p1,p2)->p2[0]*p2[0]+p2[1]*p2[1] -p1
             ans[i]= pq.poll();
             i++;
         }
+        return ans;*/
+//==========================================================================================
+
+// question easy h but isme bht kuch seekhne ko mila h 
+
+class Solution {
+    // Ye mmne khud se kia h and isme float type k based pr kse sort krte h PQ me wo seekha h and sol-1 and sol-2 are both are awesomee so un dno ko zruru ache se smjh lena 
+    class pair
+    {
+        int x,y;
+        Double distance;
+        pair(int x,int y, Double distance)
+        {
+            this.x=x;
+            this.y= y;
+            this.distance=distance;
+        }
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        
+        int no_of_points= points.length;
+        
+    int ans[][]= new int[k][2];
+        
+PriorityQueue<pair>pq= new PriorityQueue<pair>((p1,p2)->(p1.distance.compareTo(p2.distance)));
+   // yha compare to krne se hi double me sort krege withouut iske PQ integer k base pr sort krta h so ye new chz seekhi h      
+        
+        
+        for(int i=0;i<points.length;i++)
+        {
+            int x2=points[i][0];
+            int y2= points[i][1];
+           //System.out.println(x2+" "+y2);
+                double distance= Math.sqrt((x2*x2)+(y2*y2));
+           System.out.println(distance);
+            pq.add(new pair(x2,y2,distance));
+        }
+        
+        for(int i=0;i<k;i++)
+        {
+            pair p= pq.poll();
+            ans[i][0]= p.x;
+            ans[i][1]=p.y;
+        }
         return ans;
+        
     }
 }
