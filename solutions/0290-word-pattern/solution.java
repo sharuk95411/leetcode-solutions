89ffcf425245bb67792446ba4610a7c5f3c975db
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-01-17 18:08:00 +0000

    Sync LeetCode submission Runtime - 13 ms (1.55%), Memory - 39.5 MB (100.00%)

--- solutions/0290-word-pattern/solution.java
+++ solutions/0290-word-pattern/solution.java
@@ -1,12 +1,49 @@
 class Solution {
-    public boolean wordPattern(String pattern, String str) {
-    String[] words = str.split(" ");
-    if (words.length != pattern.length())
-        return false;
-    Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-            return false;
-    return true;
-}
+    public boolean wordPattern(String pattern, String s) {
+        
+        HashMap<Character,String>h= new HashMap<>();
+        List<String>list= new ArrayList<>();
+        int j=0;
+        
+        for(int i=0;i<pattern.length();i++)
+        {
+            char c= pattern.charAt(i);
+            String temp="";
+            if(h.get(c)==null)
+               
+            {
+                 if(j>=s.length()) return false;
+                while(j<s.length() && s.charAt(j)!=' ')
+                {
+                    temp=temp+s.charAt(j);
+                    j++;
+                }
+                j++;
+                if(list.contains(temp)) return false;
+                else list.add(temp);
+                // System.out.println(temp);
+                h.put(c,temp);
+                
+            }
+            else
+            {
+                while(j<s.length() && s.charAt(j)!=' ')
+                {
+                    temp= temp+s.charAt(j);
+                    j++;
+                }
+              // System.out.println(temp);
+                j++;
+                if(h.get(c).equals(temp)==false) 
+                {
+                    System.out.println(h.get(c));
+                     System.out.println(temp);
+                    return false;
+                }
+            }
+        }
+        if(j<s.length()) return false;
+        
+        return true;
+    }
 }
