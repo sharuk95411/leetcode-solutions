0634691e38683ea654791c3cae7ff0c117bc503b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-01-17 11:26:12 +0000

    Sync LeetCode submission Runtime - 2 ms (31.82%), Memory - 46.1 MB (99.91%)

--- solutions/0045-jump-game-ii/solution.java
+++ solutions/0045-jump-game-ii/solution.java
@@ -1,17 +1,18 @@
-// logic Anurag Codes and Greedy approach use hgi isme 
 class Solution {
-    public int jump(int[] nums) {
-        int jumps = 0, currentJumpEnd = 0, farthest = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
+    public int jump(int[] arr) {
+     
+    int curr_reach=0,max_jump=0,jump=0;
+        for(int i=0;i<arr.length-1;i++)
+        {
             
-            farthest = Math.max(farthest, i + nums[i]);
-            if (i == currentJumpEnd) // if we have reach the current point jha se jump start ki thi ya krni h so jump++ kr dege and currentJump ko update kr dge ki ab wha se max kha tk ja sjte h.
+            max_jump= Math.max(max_jump,i+arr[i]);
+            if(i==curr_reach)
             {
-                jumps++;
-                currentJumpEnd = farthest;
+                jump++;
+                curr_reach= max_jump;
+                
             }
         }
-        return jumps;
-        
+        return jump;
     }
 }
