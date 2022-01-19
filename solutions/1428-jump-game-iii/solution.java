1949f30cfea90882d925632cf08a351f944fabd5
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-01-19 17:00:39 +0000

    Sync LeetCode submission Runtime - 4 ms (50.71%), Memory - 56.2 MB (11.00%)

--- solutions/1428-jump-game-iii/solution.java
+++ solutions/1428-jump-game-iii/solution.java
@@ -3,18 +3,10 @@ class Solution {
          if(start<0 || start>=arr.length || arr[start]==-1) return false;
          if(arr[start]==0)return true;
         int val= arr[start];
-        arr[start]=-1;  // 0 is unaffected // used to track  traversed elements
+        arr[start]=-1;  
        
         
         return canReach(arr,start+val) || canReach(arr,start-val);
         
-        
-       /* if(start < 0 || start >= arr.length || arr[start] == -1) return false;
-        if(arr[start] == 0) return true;
-        int val = arr[start];
-        arr[start] = -1;
-        boolean plus = canReach(arr, start + val);
-        boolean minus = canReach(arr, start - val);
-        return plus || minus;*/
     }
 }
