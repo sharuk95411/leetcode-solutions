27c8bd0300c937a19a64aeecd3c60dc3725ca6d6
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-01-20 15:33:48 +0000

    Sync LeetCode submission Runtime - 42 ms (5.13%), Memory - 51.9 MB (5.69%)

---
+++ solutions/0907-koko-eating-bananas/solution.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minEatingSpeed(int[] arr, int h) {
+     
+        Arrays.sort(arr);
+        int ans=arr[arr.length-1];
+        
+        int left=1;
+        int right= arr[arr.length-1];
+        while(left<=right)
+        {
+            int mid= left+(right-left)/2;
+            if(check(mid,arr,h))
+            {
+                ans= Math.min(mid,ans);
+                right=mid-1;
+            }
+            else
+            {
+                left=mid+1;
+            }
+            
+        }
+        return ans;
+            
+    }
+    private boolean check(int speed,int arr[],int final_hour)
+    {
+         int calculate_hour=0;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(speed>=arr[i])
+            {
+                calculate_hour= calculate_hour+1;
+            }
+            else if(arr[i]%speed==0)
+            {
+                calculate_hour=calculate_hour+ arr[i]/speed;
+            }
+            else
+            {
+                calculate_hour=calculate_hour+ arr[i]/speed+1;
+            }
+        }
+        
+        if(calculate_hour<=final_hour) return true;
+        else return false;
+        
+    }
+}
