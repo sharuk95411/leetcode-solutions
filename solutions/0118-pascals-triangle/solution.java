90c93738cfda76a06f56d50f499aa15b9a44ebe0
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-01-25 07:20:27 +0000

    Sync LeetCode submission Runtime - 1 ms (94.05%), Memory - 38.4 MB (100.00%)

--- solutions/0118-pascals-triangle/solution.java
+++ solutions/0118-pascals-triangle/solution.java
@@ -1,29 +1,49 @@
 class Solution {
-    
-    // badhya Question h TYN
-    //logic- Codebix
-    public List<List<Integer>> generate(int n) {
+    public List<List<Integer>> generate(int numRows) {
         
-        List<List<Integer>>outer= new ArrayList<>();
-        if(n==0) return outer;
-          List<Integer>inner= new ArrayList<>();
+        List<Integer>inner= new ArrayList<>();
+        List<List<Integer>>ans= new ArrayList<>();
+        
         inner.add(1);
+        ans.add(new ArrayList<>(inner));
+        inner.clear();
         
-        outer.add(inner);
-    
+        int step=numRows;
+        List<Integer>sum_store= new ArrayList<>();
         
         
-        for(int i=1;i<n;i++)
+        int index=1;
+        while(index<step)
         {
-        inner= new ArrayList<>();
-            inner.add(1);
-            for(int j=1;j<outer.size();j++)
+         
+            for(int i=0;i<=ans.size();i++)
             {
-                inner.add(outer.get(i-1).get(j-1)+outer.get(i-1).get(j));
+                if(i==0)
+                {
+                    inner.add(1);
+                }
+                else if(i==ans.size())
+                {
+                    inner.add(1);
+                }
+                else
+                {
+                    inner.add(sum_store.get(i-1));
+                }
+                
+                
             }
-            inner.add(1);
-            outer.add(inner);
+            
+            sum_store.clear();
+            for(int j=0;j<inner.size()-1;j++)
+            {
+                sum_store.add(inner.get(j)+inner.get(j+1));
+            }
+            ans.add(new ArrayList<>(inner));
+            inner.clear();
+            index++;
+            
         }
-        return outer;
+        return ans;
     }
 }
