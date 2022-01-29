45476cb1b283ed0403917612d96e3f3f69a77fd5
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-01-29 09:14:53 +0000

    Sync LeetCode submission Runtime - 1846 ms (5.87%), Memory - 130.3 MB (5.11%)

--- solutions/0084-largest-rectangle-in-histogram/solution.java
+++ solutions/0084-largest-rectangle-in-histogram/solution.java
@@ -1,40 +1,45 @@
 class Solution {
-   public int largestRectangleArea(int[] arr) {
-        // Logic- Pepcoding 
-        // TC O(n) and SC O(2n) 
-        int n= arr.length;
+    public int largestRectangleArea(int[] arr) {
         
+        if(arr.length==0) return 0;
+        if(arr.length==1) return arr[0];
+        int n= arr.length;
         int left[]= new int[n];
+        int right[]= new int[n];
+        
         left[0]=0;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<arr.length;i++)
         {
             int j=i-1;
             while(j>=0 && arr[j]>=arr[i])
-            {//j--; and isse O(n2) me
-                j= left[j]-1;  // is Logic se ye O(n) me chl rha h and yhi main logic h is program ko O(n) me krne ka
+            {
+                j= left[j]-1;
             }
             left[i]= j+1;
         }
-         System.out.println(Arrays.toString(left));
-         int right[]= new int[n];
-        right[n-1]=n-1;
-        for(int i= n-2;i>=0;i--)
+        
+        System.out.println(Arrays.toString(left));
+        
+        right[n-1]= n-1;
+        for(int i=n-2;i>=0;i--)
         {
             int j=i+1;
-            while(j<n && arr[j]>=arr[i])
+            while(j<arr.length && arr[j]>=arr[i])
             {
-                //j++;  and isse O(n2) mw
-                j=right[j]+1; // is Logic se ye O(n) me chl rha h and yhi main logic h is program ko O(n) me krne ka
+                j= right[j]+1;
             }
             right[i]= j-1;
         }
-          System.out.println(Arrays.toString(right));
-        int area=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++)
-        {
-            area= Math.max(area,(right[i]-left[i]+1)*arr[i]);
-        }
-        return area;
+        System.out.println(Arrays.toString(right));
         
+        int maxArea= 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            int area= Math.abs(right[i]-left[i]+1)*arr[i];
+            System.out.println(area);
+            maxArea= Math.max(maxArea,area);
+        }
+        return maxArea;
+    
     }
 }
