a88f91776e189153cb1ce9045267dd602ccba4b4
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-01 13:53:20 +0000

    Sync LeetCode submission Runtime - 14 ms (6.05%), Memory - 55 MB (6.09%)

--- solutions/0056-merge-intervals/solution.java
+++ solutions/0056-merge-intervals/solution.java
@@ -1,31 +1,46 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-         Arrays.sort(arr, (a,b) -> a[0] - b[0]);
-         Stack<int []>s= new Stack<>();
-         s.add(arr[0]);
-         for(int i=1;i<arr.length;i++)
-         {
-             int start2= arr[i][0];
-             int finish2= arr[i][1];
-             int a[]= s.peek();
-             if(a[1]>=start2)
-             {
-                 a[1]= Math.max(a[1],finish2);
-             }
-             else
-             {
-                 s.add(arr[i]);
-             }
-         }
-         System.out.println(s.size());
-        int arr1[][]= new int[s.size()][2];
-        for(int i=arr1.length-1;i>=0;i--)
+    
+    class Pair{
+        int start,end;
+        Pair(int start,int end)
         {
-            int b[]= s.pop();
-            arr1[i][0]=b[0];
-            arr1[i][1]=b[1];
+            this.start= start;
+            this.end= end;
         }
-        return arr1;
+    }
+    public int[][] merge(int[][] arr) {
         
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        List<Pair>list= new ArrayList<>();
+        
+        int start= arr[0][0];
+    
+        int end= arr[0][1];
+        for(int i=1;i<arr.length;i++)
+        {
+                if(end>=arr[i][0])
+                {
+                    if(end>arr[i][1]) continue;
+                    else   end= arr[i][1];
+                    
+                }
+            else if(arr[i][0]>end)
+            {
+                list.add(new Pair(start,end));
+                start= arr[i][0];
+                end= arr[i][1];
+                
+            }
+            
+        }
+        list.add(new Pair(start,end));
+        int ans[][]= new int [list.size()][2];
+        for(int i=0;i<list.size();i++)
+        {
+            Pair p= list.get(i);
+            ans[i][0]=p.start;
+            ans[i][1]= p.end;
+        }
+        return ans;
     }
 }
