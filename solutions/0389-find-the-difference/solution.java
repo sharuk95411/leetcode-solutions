b4541f33757788531bc436544fc490e1a81e9fc7
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-07 04:21:45 +0000

    Sync LeetCode submission Runtime - 3 ms (41.37%), Memory - 41.7 MB (99.96%)

---
+++ solutions/0389-find-the-difference/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+     
+        int freq[]= new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            char c= s.charAt(i);
+            freq[c-'a']++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            char c= t.charAt(i);
+            if(freq[c-'a']==0) return c;
+            else
+                freq[c-'a']--;
+        }
+        return 'c';
+    }
+}
