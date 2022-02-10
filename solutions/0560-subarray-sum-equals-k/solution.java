d2a0811ae7d68777e0b2b18f83b49ef9eab763f9
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-10 10:18:15 +0000

    Sync LeetCode submission Runtime - 42 ms (26.13%), Memory - 62.2 MB (5.73%)

--- solutions/0560-subarray-sum-equals-k/solution.java
+++ solutions/0560-subarray-sum-equals-k/solution.java
@@ -1,24 +1,26 @@
 class Solution {
-    // Logic- Pepcoding 
-    // hint- PrefixSum And HashMap use hga with No recursion 
     public int subarraySum(int[] nums, int k) {
+
         HashMap<Integer,Integer>h= new HashMap<>();
-  h.put(0,1); // {3,2,6} and k=5 so in cases k liye hmne ye add kia h and ye vdio me btaya gya h
-        int count=-0;
-        int prefix_sum=0;
         
-        for(int i=0;i<nums.length;i++)
+        h.put(0,1);
+    int pSum=0;
+        
+    int count=0;
+        
+    for(int i=0;i<nums.length;i++)
+    {
+        pSum= pSum+nums[i];
+        
+        if(h.containsKey(pSum-k))
         {
-            prefix_sum=prefix_sum+nums[i];
-            
-            if(h.containsKey(prefix_sum-k))
-            {
-                count=count+h.get(prefix_sum-k);
-                h.put(prefix_sum,h.getOrDefault(prefix_sum,0)+1 );
-            }
-            else
-            h.put(prefix_sum,h.getOrDefault(prefix_sum,0)+1);
+            count= count+h.get(pSum-k);
+            h.put(pSum,h.getOrDefault(pSum,0)+1);
         }
+        else
+            h.put(pSum,h.getOrDefault(pSum,0)+1);
+        
+    }
         return count;
         
     }
