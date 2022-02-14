ef95d52659df036effa4fe38c5ea2622619033ab
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-14 10:37:05 +0000

    Sync LeetCode submission Runtime - 1 ms (99.29%), Memory - 53.6 MB (11.90%)

--- solutions/1428-jump-game-iii/solution.java
+++ solutions/1428-jump-game-iii/solution.java
@@ -1,12 +1,24 @@
 class Solution {
+    boolean b[];
     public boolean canReach(int[] arr, int start) {
-         if(start<0 || start>=arr.length || arr[start]==-1) return false;
-         if(arr[start]==0)return true;
-        int val= arr[start];
-        arr[start]=-1;  
-       
         
-        return canReach(arr,start+val) || canReach(arr,start-val);
+        b= new boolean[arr.length];
+        return A(arr,start);
+    }
+    
+    private boolean A(int arr[],int index)
+    {
+        if(index>=arr.length || index<0) return false;
+        
+        else if(b[index]==true ) return false;
+        
+        else if(b[index]==false)
+        {
+            b[index]=true;
+            if(arr[index]==0) return true;
+        }
+
+        return (A(arr,index+arr[index]) || A(arr,index-arr[index]));
         
     }
 }
