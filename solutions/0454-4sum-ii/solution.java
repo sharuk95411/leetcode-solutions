1ffe1f75259f32896514af9c0035a23aa8f5c630
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-15 08:12:16 +0000

    Sync LeetCode submission Runtime - 180 ms (5.48%), Memory - 62.5 MB (5.54%)

--- solutions/0454-4sum-ii/solution.java
+++ solutions/0454-4sum-ii/solution.java
@@ -1,32 +1,36 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         
-        Map<Integer,Integer>map= new HashMap<>();
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // Arrays.sort(nums3);
+        // Arrays.sort(nums4);
         
-        for(int a :nums4)
+        int count=0;
+        
+        HashMap<Integer,Integer>h= new HashMap<>(); // sum and freq type ka lia h
+        for(int i=0;i<nums1.length;i++)
         {
-            for(int b : nums3)
-            map.put(a+b,map.getOrDefault(a+b,0)+1);
+            for(int j=0;j<nums2.length;j++)
+            {
+                int sum= nums1[i]+nums2[j];
+                h.put(sum,h.getOrDefault(sum,0)+1);
+                
+            }
         }
         
-        int n= nums1.length;
-        int count=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums3.length;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<nums4.length;j++)
             {
-                    int sum= nums1[i]+nums2[j];
-                    if(sum>0)
-                    {
-                        count= count+ map.getOrDefault(-sum,0);
-                    }
-                    else
-                    {
-                        count= count+map.getOrDefault(-sum,0);
-                    }
-                    
+                int sum= nums3[i]+nums4[j];
+                if(h.containsKey(-sum))
+                {
+                    count= count+h.get(-sum);
+                }
             }
         }
         return count;
+        
     }
 }
