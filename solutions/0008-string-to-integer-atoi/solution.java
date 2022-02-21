342614153539241e0aeb550f5d858be1ef11923d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-21 06:56:55 +0000

    Sync LeetCode submission Runtime - 4 ms (4.79%), Memory - 41.9 MB (99.99%)

--- solutions/0008-string-to-integer-atoi/solution.java
+++ solutions/0008-string-to-integer-atoi/solution.java
@@ -1,47 +1,57 @@
 class Solution {
-    public int myAtoi(String str) {
-        
-        final int len = str.length();
-        
-        if(len == 0){
-            return 0;
-        }
-        
-        int index = 0;
-        while(index < len && str.charAt(index) == ' '){
-            index++;
-        }
-        
-        if(index == len){
-            return 0;
-        }
-        
-        char ch;
-        boolean isNegative = (ch = str.charAt(index)) ==  '-';
-        
-        if(isNegative || ch == '+'){
-            ++index;
-        }
-            
-        final int maxLimit = Integer.MAX_VALUE / 10;
-        int result = 0;
-        while(index < len && isDigit(ch = str.charAt(index))){
-            
-            int digit = ch - '0';
-            
-            if(result > maxLimit || (result == maxLimit && digit > 7)){
-                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            }
-            
-            result = (result * 10) + digit;
-            
-            ++index;
-        }
-        
-        return isNegative ? -result : result;
+  public int myAtoi(String str) {
+    
+    
+    
+    if (str.length() == 0){
+        return 0;
     }
     
-    private boolean isDigit(char ch){
-        return ch >= '0' && ch <= '9';
+    int index = 0;
+    
+      str= str.trim();
+      int len = str.length();
+      if(len==0) return 0;
+      System.out.println(index);
+    
+    boolean isNegative = false;
+    
+    // to handle sign cases
+      
+      if (str.charAt(index) == '-') {
+        isNegative = true;
+        index++;
+      } else if (str.charAt(index) == '+'){
+         index++;
+      }
+      
+    
+       int result = 0;
+    
+    // converting digit(in character form) to integer form
+    // iterate until non-digit character is not found or we can say iterate till found character is a digit
+    while (index < len && isDigit(str.charAt(index))) {
+      
+      /* str.charAt(index) - '0' is to convert the char digit into int digit eg: '5' - '0' --> 5
+      or else it will store the ASCII value of 5 i.e. 53,
+      so we do 53(ASCII of 5) - 48(ASCII of 0(zero)) to get 5 as int*/
+      int digit = str.charAt(index) - '0';
+      
+      // to avoid integer overflow
+      if (result > (Integer.MAX_VALUE / 10) || (result == (Integer.MAX_VALUE / 10) && digit > 7)){
+          return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+      }
+      
+      // adding digits at their desired place-value
+      result = (result * 10) + digit;
+      
+      ++index;
     }
+      
+    return isNegative ? -result : result;
+  }
+  
+  private boolean isDigit(char ch) {
+    return ch >= '0' && ch <= '9';
+  }
 }
