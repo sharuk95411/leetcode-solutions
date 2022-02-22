30b1305a5a341d8702a3c35420b2fac99aea0961
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-02-22 11:56:39 +0000

    Sync LeetCode submission Runtime - 18 ms (3.64%), Memory - 67.3 MB (7.99%)

--- solutions/0189-rotate-array/solution.java
+++ solutions/0189-rotate-array/solution.java
@@ -1,31 +1,42 @@
 class Solution {
-    public void rotate(int[] arr, int k) {
+    public void rotate(int[] arr, int s) {
         
-        if(k== arr.length)return ;
-        if(k>arr.length) k= k%arr.length;
-        if(k==0) return;
         
-      //  k= arr.length-k;
-        System.out.println(k);
-        Reverse(arr,0,arr.length-1);
-        System.out.println(Arrays.toString(arr));
-        Reverse(arr,0,k-1);
-     //   System.out.println(Arrays.toString(arr));
-        Reverse(arr,k,arr.length-1);
-       // System.out.println(Arrays.toString(arr));
+        s= s%arr.length;
+        if(s==0) return ;
         
-    }
-    private void Reverse(int arr[],int left,int right)
-    {
-        while(left<right)
+        int i=0;
+        int j= arr.length-1;
+        
+        while(i<j)
         {
-            int temp= arr[left];
-            arr[left]= arr[right];
-            arr[right]= temp;
-            left++;
-            right--;
+            int temp= arr[i];
+            arr[i]= arr[j];
+            arr[j]= temp;
+            i++;
+            j--;
         }
+        System.out.println(Arrays.toString(arr));
+        i=0;
+        j= s-1;
+        while(i<j)
+        {
+            int temp= arr[i];
+            arr[i]= arr[j];
+            arr[j]= temp;
+            i++;
+            j--;
+        }
+        i= s;
+        j= arr.length-1;
+         while(i<j)
+        {
+            int temp= arr[i];
+            arr[i]= arr[j];
+            arr[j]= temp;
+            i++;
+            j--;
+        }
+      
     }
-    
-    
 }
