328d2c7f37a96bd416c2486d18402bd47eda8f2d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-03-05 18:15:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.1 MB (100.00%)

--- solutions/0413-arithmetic-slices/solution.java
+++ solutions/0413-arithmetic-slices/solution.java
@@ -1,10 +1,26 @@
 class Solution {
-    public int numberOfArithmeticSlices(int[] nums) {
-        var slices = 0;
-	for (int i = 2, prev = 0; i < nums.length; i++)
-		slices += (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) ? 
-				  ++prev : 
-				  (prev = 0);
-	return slices;
+    public int numberOfArithmeticSlices(int[] arr) {
+        
+        if(arr.length<3) return 0;
+        
+        int prev_diff= arr[1]-arr[0];
+        int ans=0;
+        int count=0;
+        
+        for(int i=1;i<arr.length-1;i++)
+        {
+            int diff= arr[i+1]-arr[i];
+            if(diff==prev_diff)
+            {
+                count++;
+            }
+            else
+            {
+                prev_diff= diff;
+                count=0;
+            }
+            ans= ans+count;
+        }
+        return ans;
     }
 }
