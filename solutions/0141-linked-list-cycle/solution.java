7d5acbee69d6672eb370af60336e8c7368901d98
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-03-08 14:07:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.5 MB (100.00%)

--- solutions/0141-linked-list-cycle/solution.java
+++ solutions/0141-linked-list-cycle/solution.java
@@ -12,21 +12,17 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
-        if (head == null ) {
-            return false;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while (slow != fast) {
-            if (fast == null || fast.next == null) {
-                return false;
-            }
-            slow = slow.next;
-            fast = fast.next.next;
+        if(head==null) return false;
+        ListNode slow= head;
+        ListNode fast= head.next;
+        
+        while(slow!=fast)
+        {
+            if(fast==null || fast.next==null) return false;
+            
+            fast= fast.next.next;
+            slow= slow.next;
         }
         return true;
-
-        
     }
 }
