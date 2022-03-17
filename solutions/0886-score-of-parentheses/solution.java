3ff378462407c11b88b331700d1a2ff0422c5c3d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-03-17 09:51:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40 MB (100.00%)

--- solutions/0886-score-of-parentheses/solution.java
+++ solutions/0886-score-of-parentheses/solution.java
@@ -1,47 +1,29 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        int ans=0;
-        if(s.length()==0) return ans;
-        boolean flag=false;
-        Stack<Character>stack= new Stack<>();
+     
+    
+        // "(()(()))"
+        // (())()
+        // ()(())
+        //        
+        Stack<Integer>stack= new Stack<>();
+        
+    int score=0;
+        
         for(int i=0;i<s.length();i++)
         {
-            
-            char c=s.charAt(i);
-            if(c=='(') stack.push(c);
-            
+            char c= s.charAt(i);
+            if(c=='(')
+            {
+                stack.push(score);
+                score=0;
+            }
             else
             {
-                
-                while(stack.size()>0 && stack.peek()!='(')
-                {
-                  
-                  ans= ans+stack.pop()-'0';
-                    flag=true;
-                }
-                stack.pop();
-                
-                if(flag==true)
-                {
-                    ans=2*ans;
-                    stack.push((char)(ans+'0'));
-                    flag=false;
-                    ans=0;
-                }
-                else
-                {
-                    stack.push('1');
-                }
-                
-
+                score= stack.pop()+ Math.max(2*score,1);
             }
         }
-       // System.out.println(stack);
-        while(stack.size()>0)
-        {
-                ans= ans+stack.pop()-'0';
-        }
+        return score;
         
-        return ans;
     }
 }
