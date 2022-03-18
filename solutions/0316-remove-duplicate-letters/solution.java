6a02c0845beba37e8f8bd925779d4e4141bcb8e4
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-03-18 14:42:30 +0000

    Sync LeetCode submission Runtime - 5 ms (11.78%), Memory - 42.8 MB (99.95%)

--- solutions/0316-remove-duplicate-letters/solution.java
+++ solutions/0316-remove-duplicate-letters/solution.java
@@ -1,43 +1,29 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
         
-        int n= s.length();
-        boolean visited[]= new boolean[26];
-        int freq[]= new int[26];
-        char ch[]= s.toCharArray();
-        
-        for(int i=0;i<n;i++)
-        {
-            freq[ch[i]-'a']= freq[ch[i]-'a']+1;
+      int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            lastIndex[s.charAt(i) - 'a'] = i; // track the lastIndex of character presence
         }
         
-        int index;
-        Stack<Character>stack= new Stack<>();
+        boolean[] seen = new boolean[26]; // keep track seen
+        Stack<Integer> st = new Stack();
         
-        for(int i=0;i<n;i++)
-        {
-            if(visited[ch[i]-'a']) 
-            {  
-                 freq[ch[i]-'a']= freq[ch[i]-'a']-1;
-                   continue;
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            
+            if (seen[curr]) continue; // if seen continue as we need to pick one char only
+           
+            while (!st.isEmpty() && st.peek() > curr && i < lastIndex[st.peek()]){
+                seen[st.pop()] = false; // pop out and mark unseen
             }
-            else
-            {
-                while(stack.size()>0 &&(stack.peek()>ch[i])&& freq[stack.peek()-'a']>0)
-                {
-                    visited[stack.pop()-'a']=false;
-                }
-                stack.push(ch[i]);
-                visited[ch[i]-'a']=true;
-                freq[ch[i]-'a']= freq[ch[i]-'a']-1;
-            }
+            st.push(curr); // add into stack
+            seen[curr] = true; // mark seen
         }
-         StringBuilder sb = new StringBuilder();
-    //pop character from stack and build answer string from back
-    while(!stack.isEmpty()){
-        sb.insert(0,stack.pop()); // yhe 0 pr insert krwa rhe h agr wha koi element hua to wo right side move ho jyga nice approcach.
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty())
+            sb.insert(0,(char)(st.pop()+'a'));
+        return sb.toString();
     }
-    return sb.toString();
-    }
-    
 }
