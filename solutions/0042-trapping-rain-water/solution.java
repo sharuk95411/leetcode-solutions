9e2496673f7a5df408f891292f4a8d2985ce89d9
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-09-18 18:07:09 +0000

    Sync LeetCode submission Runtime - 2 ms (5.99%), Memory - 48.5 MB (28.72%)

--- solutions/0042-trapping-rain-water/solution.java
+++ solutions/0042-trapping-rain-water/solution.java
@@ -1,6 +1,35 @@
 class Solution {
     public int trap(int[] arr) {
-      
+       if(arr.length<3) return 0;
+   /*     int left_max=Integer.MIN_VALUE;
+        int right_max= Integer.MIN_VALUE;
+        int trapped_water=0;
+        for(int i=1;i<arr.length-1;i++)
+        {
+            int j=0;
+            while(j<i)
+            {
+                left_max= Math.max(left_max,arr[j]);
+                j++;
+                
+            }
+           
+            j++;
+            while(j<arr.length)
+            {
+                right_max= Math.max(right_max,arr[j]);
+                j++;
+            }
+            int a= Math.min(left_max,right_max);
+            if(a>arr[i])
+            {
+            trapped_water= trapped_water+(a-arr[i]);
+            }
+            left_max=-1;
+            right_max=-1;
+                
+        }
+        return trapped_water;*/
         
         int trapped_water=0;
         int left_max= arr[0];
