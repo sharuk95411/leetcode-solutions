71ad9cad5910e6a6ac5b6c4289536b7368bcabdc
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-11-19 12:26:41 +0000

    Sync LeetCode submission Runtime - 1 ms (61.62%), Memory - 42.5 MB (98.88%)

--- solutions/1693-sum-of-all-odd-length-subarrays/solution.java
+++ solutions/1693-sum-of-all-odd-length-subarrays/solution.java
@@ -1,29 +1,15 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         
-      /* This is BF TC O(n2) & SC is(1) isko b try krna h and ye khd se kia h 
-      int ans=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            ans=ans+arr[i];
-            int sum=arr[i];
-            int j=i+1;
-            int count=1;
-            while(j<arr.length)
-            {
-                sum= sum+arr[j];
-                count++;
-                if(count%2!=0) ans= ans+sum;
-                
-                j++;
-            }
-        }
-        return ans;  */
-        
         int sum=0;
+        int n= arr.length;
         for(int i=0;i<arr.length;i++)
         {
-    sum= sum+(((i+1)*(arr.length-i)+1)/2)*arr[i]; // logic behind this formula Shashwat Tiwari and iski help se even length ka b sum easily nikal skte ho 
+             int start= n-i;
+            int end= i+1;
+            int total= start*end;
+            
+            sum+= (total+1)/2*arr[i];
         }
         return sum;
     }
