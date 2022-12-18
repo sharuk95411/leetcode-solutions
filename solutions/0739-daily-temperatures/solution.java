5e8ed86dc2dc88f8dccd756741e76cea63d53b06
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-18 07:59:12 +0000

    Sync LeetCode submission Runtime - 181 ms (5.48%), Memory - 54.3 MB (100.00%)

--- solutions/0739-daily-temperatures/solution.java
+++ solutions/0739-daily-temperatures/solution.java
@@ -1,52 +1,23 @@
 class Solution {
-    // Hint-Monotonic Stack used
-     class Pair
-    {
-        int value,index;
-        Pair(int value,int index)
-        {
-            this.value=value;
-            this.index=index;
-        }
-    }
     public int[] dailyTemperatures(int[] arr) {
-        int n= arr.length;
-         Stack<Pair>stack= new Stack<>();
-        stack.push(new Pair(arr[n-1],n-1));
-        arr[n-1]=0;
         
-        for(int i=n-2;i>=0;i--)
-        {
-            
-            Pair p= stack.peek();
-            if(arr[i]>=p.value)
+            int n= arr.length;
+            int ans[]= new int[n];
+
+Stack<Integer>stack= new Stack<>();
+            for(int i=n-1;i>=0;i--)
             {
-                while(stack.size()>0 && arr[i]>=p.value)
-                {
-                    stack.pop();
-                    if(stack.size()>0)   p=stack.peek();
-                }
-                if(stack.size()==0) 
-                {
-                    stack.push(new Pair(arr[i],i));
-                    arr[i]=0;
-                }
-                else
-                {
-                    stack.push(new Pair(arr[i],i));
-                    arr[i]=p.index-i;
-                }
-                
-            } // if closes
-            else
-            {
-                p =stack.peek();
-                stack.push(new Pair(arr[i],i));
-                arr[i]= p.index-i;
+       while(!stack.isEmpty()&&arr[i]>=arr[stack.peek()])
+       {
+           stack.pop();
+       }
+
+       if(!stack.isEmpty())
+       {
+                    ans[i]= stack.peek()-i;
+       }
+       stack.push(i);
             }
-        }
-        
-                           return arr;
-        
+            return ans;  
     }
 }
