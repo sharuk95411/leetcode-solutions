5c1b0e22de22ce1b27bda8470b5e35ea834e6d0f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-20 18:05:59 +0000

    Sync LeetCode submission Runtime - 26 ms (5.31%), Memory - 42.1 MB (100.00%)

--- solutions/0871-keys-and-rooms/solution.java
+++ solutions/0871-keys-and-rooms/solution.java
@@ -1,27 +1,33 @@
 class Solution {
-    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+    public boolean canVisitAllRooms(List<List<Integer>> arr) {
         
-        List<Integer>visited= new ArrayList<>();
-        visited.add(0);
-        Stack<Integer>stack= new Stack<>();
-        stack.add(0);
-        
-        while(stack.size()>0)
+        List<Integer>keys= new ArrayList<>();
+        boolean visit[]= new boolean[arr.size()];
+        visit[0]= true;
+        keys= arr.get(0);
+
+        for(int i=0;i<keys.size();i++)
         {
-            List<Integer>keys= rooms.get(stack.pop());
-            
-            for(int key: keys)
-            {
-                if(visited.contains(key)==false)
-                {
-                    visited.add(key);
-                    stack.push(key);
-                }
-            }
+              int no= keys.get(i);
+              if(visit[no]==false)
+              {
+                  visit[no]= true;
+                  for(int j=0;j<arr.get(no).size();j++)
+                  {
+                      int a= arr.get(no).get(j);
+                      if(keys.contains(a)==false)
+                      {
+                          keys.add(a);
+                      }
+                  }
+                  i=-1;
+              }
         }
-        
-        if(visited.size()==rooms.size()) return true;
-        else return false;
-        
+
+        for(int i=0;i<visit.length;i++)
+        {
+            if(visit[i]==false) return false;
+        }
+        return true;
     }
 }
