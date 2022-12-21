70db6cdcd5a22b76ca8b889c191b01b7b104d766
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-21 19:03:59 +0000

    Sync LeetCode submission Runtime - 15 ms (25.39%), Memory - 55.6 MB (5.25%)

--- solutions/0049-group-anagrams/solution.java
+++ solutions/0049-group-anagrams/solution.java
@@ -1,38 +1,20 @@
-// Logic -Pepcoding  (Leetcode 49)
-// Good question isme HashMap of HashMap use krna sikha h  this is BFA and isko optimise krna bcha h pehle ye khd se try krna then optimise krna sikhna.
-// HashMap me value key k base pr sort hokr store hti h and isme character k base pr sort hgi so yhi property ka use hua h
 class Solution {
-    public List<List<String>> groupAnagrams(String[] s) {
-    HashMap<HashMap<Character, Integer>, List<String>>h1= new HashMap<>();
-    for(String str: s)
-    {
-        HashMap<Character,Integer>h2= new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
+         HashMap<String,List<String>> map=new HashMap<>();
         
-        for(int j=0;j<str.length();j++)
-        {
-            char ch=str.charAt(j);
-       h2.put(ch,h2.getOrDefault(ch,0)+1);
-        }
-        if(h1.containsKey(h2)==true)
-        {
-            List<String>l= h1.get(h2);
-            l.add(str);
+        for(int i=0;i<strs.length;i++){
+            String s1=strs[i];
+            char[] arr=s1.toCharArray();
+            Arrays.sort(arr);
+            String str=new String(arr);
             
+            if(map.containsKey(str)){
+                map.get(str).add(s1); 
+            }else{
+                map.put(str,new ArrayList<>());
+                map.get(str).add(s1);
+            }
         }
-        else
-        {
-            List<String>l= new ArrayList<>();
-            l.add(str);
-            h1.put(h2,l);
-        }
-        }
-        
-    List<List<String>>L= new ArrayList<>();
-    for(List<String>l : h1.values())  // yha only values ko traverse kr rhe h ye b nai chz h 
-    {
-        L.add(l);
-    }
-    return L;
-    
+        return new ArrayList<>(map.values());
     }
 }
