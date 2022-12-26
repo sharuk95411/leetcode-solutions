1bf33813b8f4e319361af47fda74f77babe14a7d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-26 18:17:08 +0000

    Sync LeetCode submission Runtime - 26 ms (95.01%), Memory - 49.9 MB (100.00%)

--- solutions/0215-kth-largest-element-in-an-array/solution.java
+++ solutions/0215-kth-largest-element-in-an-array/solution.java
@@ -1,37 +1,15 @@
 class Solution {
-    // Logic-Tech Dose
-    // TC is Klog(n)
-    // Hint= Max Heap ki help se hga 
     public int findKthLargest(int[] arr, int k) {
-        int n= arr.length;
-        int ans=0;
-         for(int i=n/2-1;i>=0;i--)
-        heapify(arr,i,n);
-        
-        for(int i=0;i<k;i++)
-        {
-        ans=arr[0];
-        arr[0]=arr[n-1];
-        n--;
-        heapify(arr,0,n);
-        }
-        return ans;
-    }
-     public void heapify(int a[],int i,int n)
+      Arrays.sort(arr);
+int i=0;
+
+        for(int j=arr.length-1;j>=0;j--)
     {
-        int max=i;
-        int l=2*i+1;
-        int r=2*i+2;
-        if(l<n && a[l]>a[max])
-        max=l;
-        if(r<n && a[r]>a[max])
-        max=r;
-        if(i!=max)
-        {
-            int swap=a[i];
-            a[i]=a[max];
-            a[max]=swap;
-            heapify(a,max,n);
-        }
+        i++;
+        if(i==k) return arr[j];
     }
+      return 5;
+    }
+
+  
 }
