385f1219d34583fa1c88e858c33d3e87e5d26ed7
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-26 17:39:35 +0000

    Sync LeetCode submission Runtime - 1 ms (87.38%), Memory - 42.8 MB (100.00%)

--- solutions/0046-permutations/solution.java
+++ solutions/0046-permutations/solution.java
@@ -1,31 +1,38 @@
 class Solution {
-    
-    List<List<Integer>>outer = new ArrayList<>();
+    List<List<Integer>>outer= new ArrayList<>();
     List<Integer>inner= new ArrayList<>();
-    public List<List<Integer>> permute(int[] nums) {
-        int n = nums.length;
-        boolean check[]= new boolean[n];
-        A(nums,check);
-        return outer;
+     boolean check[];
+    public List<List<Integer>> permute(int[] arr) {
+        
+      //int n= nums.length;
+       check= new boolean[arr.length];
+      A(arr);
+      return outer;
     }
 
-    public void A( int arr[], boolean check[])
-{
-    if(inner.size()==arr.length)
+    public void A(int arr[])
     {
-        outer.add(new ArrayList<>(inner));
-        return ;
-    }
-    for(int i=0;i<arr.length;i++)
-    {
-        if(check[i]==false)
+        if(inner.size()==arr.length)
+            {
+                outer.add(new ArrayList<>(inner));
+                return;
+            }
+        for(int i=0;i<arr.length;i++)
         {
+
+               
+            if(check[i]==false)
+            {
             inner.add(arr[i]);
-            check[i]= true;
-            A(arr,check);
-            inner.remove(inner.size()-1);
-            check[i]= false;
+             check[i]= true;
+            A(arr);
+           inner.remove(inner.size()-1);
+           check[i]= false;
+               
+            }
+            
+           
         }
+        
     }
-}
 }
