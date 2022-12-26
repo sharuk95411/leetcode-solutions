0b645fa9cd2e92a3e6c475dcd11dd60a5b56bcef
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-26 18:07:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.6 MB (99.98%)

--- solutions/0078-subsets/solution.java
+++ solutions/0078-subsets/solution.java
@@ -1,25 +1,24 @@
 class Solution {
     List<List<Integer>>outer= new ArrayList<>();
     List<Integer>inner= new ArrayList<>();
-    public List<List<Integer>> subsets(int[] nums) {
-
-     int i=0;
-     A(nums,i);
-     return outer;
+    public List<List<Integer>> subsets(int[] arr) {
+        
+   A(0,arr);
+   return outer;
     }
 
-    public void A(int arr[], int i)
+    public void A(int i,int arr[])
     {
         if(i==arr.length)
         {
             outer.add(new ArrayList<>(inner));
-            return ;
+            return;
         }
+        if(i>arr.length) return ;
 
-          inner.add(arr[i]);
-          A(arr,i+1);
-          inner.remove(inner.size()-1);
-
-          A(arr,i+1);
+        inner.add(arr[i]);
+        A(i+1,arr);
+        inner.remove(inner.size()-1);
+        A(i+1,arr);
     }
 }
