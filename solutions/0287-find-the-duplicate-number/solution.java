dcdba785845034e7c31314736a31cab717c8103b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2022-12-30 19:38:17 +0000

    Sync LeetCode submission Runtime - 3 ms (93.37%), Memory - 60.2 MB (99.97%)

--- solutions/0287-find-the-duplicate-number/solution.java
+++ solutions/0287-find-the-duplicate-number/solution.java
@@ -1,13 +1,18 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        
-        int index=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            index= Math.abs(nums[i]);
-            if(nums[index-1]<0) return index;
-            else nums[index-1]= -nums[index-1];
-        }
-        return index;
+             int index=0;
+
+             for(int i=0;i<nums.length;i++)
+             {
+                 index= Math.abs(nums[i]);
+                 {
+                     if(nums[index]<0) return index;
+                     else
+                     {
+                         nums[index]= -nums[index];
+                     }
+                 }
+             }
+             return index;
     }
 }
