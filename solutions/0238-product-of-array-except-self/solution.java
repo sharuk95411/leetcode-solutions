f2824921934e124a3cd7462af58739fc4d5c6867
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-07 07:16:50 +0000

    Sync LeetCode submission Runtime - 1 ms (99.85%), Memory - 50.4 MB (100.00%)

--- solutions/0238-product-of-array-except-self/solution.java
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -1,24 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] arr) {
         
-        int ans[]= new int[arr.length];
-        for(int i=0;i<arr.length;i++)
-        {
-            ans[i]=1;
-        }
+        int left[]= new int[arr.length];
+        int right[]= new int[arr.length];
 
+         left[0]=1;
+         right[arr.length-1]=1;
+        int product=1;
         for(int i=1;i<arr.length;i++)
         {
-            ans[i]= ans[i-1]*arr[i-1];
+         product= product*arr[i-1];
+            left[i]= product;
         }
 
-        int temp=arr[arr.length-1];
-
+        int temp=1;
         for(int i=arr.length-2;i>=0;i--)
         {
-              ans[i]= ans[i]*temp;
-              temp= temp*arr[i];
+            temp=temp*arr[i+1];
+            left[i]= left[i]*temp;
         }
-              return ans;
+      
+       return left;
+        
     }
 }
