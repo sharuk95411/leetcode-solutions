a9de8879b2ecf43ed227b9fb48dab5b72601b005
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-09 17:31:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.7 MB (100.00%)

--- solutions/0075-sort-colors/solution.java
+++ solutions/0075-sort-colors/solution.java
@@ -1,41 +1,21 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int zero=0;
-        int one=0;
-        int two=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==0)
-            {
-               zero++;
-            }
-            else if (nums[i]==1)
-            {
-                 one++;
-            }
-            else
-            {
-              two++;
-            }
-        }
-
-        for(int i=0;i<nums.length;i++)
-        {
-            if(zero>0)
-            {
-                 nums[i]=0;
-                 zero--;
-            }
-            else if(one>0)
-            {
-                   nums[i]=1;
-                   one--;
-            }
-            else if(two>0)
-            {
-                   nums[i]=2;
-                   two--;
-            }
-        }
-    }
+   
+   public void sortColors(int[] nums) {
+	int zeroIndex = 0, twoIndex = nums.length - 1, i = 0;
+	while( i <= twoIndex ) {
+		if( nums[i] == 0 ) 
+			swap(nums, zeroIndex++, i++);
+		else if( nums[i] == 2)
+			swap(nums, twoIndex--, i);    
+		else
+			i++;
+	}
 }
+
+public void swap(int[] nums, int i, int j) {
+	int temp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = temp;
+}
+}
+
