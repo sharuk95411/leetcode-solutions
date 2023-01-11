80793267cec8c665755daa9d9c0f5ad4c4dc6a26
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-11 14:00:52 +0000

    Sync LeetCode submission Runtime - 36 ms (18.38%), Memory - 59.6 MB (100.00%)

--- solutions/0128-longest-consecutive-sequence/solution.java
+++ solutions/0128-longest-consecutive-sequence/solution.java
@@ -1,35 +1,29 @@
 class Solution {
-    public int longestConsecutive(int[] arr) {
-        if(arr.length==0) return 0;
-        
-        HashMap<Integer,Boolean>h= new HashMap<>();
-        
-        for(int a: arr) h.put(a,true);
-        
-        for(int a: arr)
-        {
-            if(h.containsKey(a-1)) h.put(a-1,false);
-        }
-        
-        int ans=1;
-        for(Integer k : h.keySet())
-        {
-            if(h.get(k))
-            {
-                ans= Math.max(ans,findLength(h,k-1));
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer, Boolean> hm = new HashMap<>();
+        for(int i =0; i <nums.length; i++)
+            hm.put(nums[i], true);
+      
+        for(int i =0; i <nums.length; i++){
+            if(hm.containsKey(nums[i] - 1)){
+                hm.put(nums[i], false);
             }
-            
         }
-        return ans;
         
+        int max = 0;
+        for(Integer key : hm.keySet()){
+            if(hm.get(key) == true){
+                max = Math.max(max, findLength(hm, key));
+             }
+       }
+        return max;        
     }
-    private int findLength(HashMap<Integer,Boolean>h,Integer k)
-    {
-        int ans=1;
-        while(h.containsKey(k)==true && h.get(k)==false)
-        {
-            k--;
+    
+    private static int findLength(HashMap<Integer, Boolean> hm, int k){
+        int ans  = 0;
+        while(hm.containsKey(k)){
             ans++;
+            k++;
         }
         return ans;
     }
