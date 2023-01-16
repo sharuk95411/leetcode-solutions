b68acf7beef69a7f8eedc56e9d048e3300ab242c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-16 19:16:00 +0000

    Sync LeetCode submission Runtime - 41 ms (10.37%), Memory - 61.1 MB (100.00%)

--- solutions/0128-longest-consecutive-sequence/solution.java
+++ solutions/0128-longest-consecutive-sequence/solution.java
@@ -1,30 +1,41 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        HashMap<Integer, Boolean> hm = new HashMap<>();
-        for(int i =0; i <nums.length; i++)
-            hm.put(nums[i], true);
-      
-        for(int i =0; i <nums.length; i++){
-            if(hm.containsKey(nums[i] - 1)){
-                hm.put(nums[i], false);
-            }
-        }
+    public int longestConsecutive(int[] arr) {
         
-        int max = 0;
-        for(Integer key : hm.keySet()){
-            if(hm.get(key) == true){
-                max = Math.max(max, findLength(hm, key));
-             }
+        HashMap<Integer,Boolean>h= new HashMap<>();
+       for(int a : arr)
+       {
+           h.put(a,true);
        }
-        return max;        
-    }
-    
-    private static int findLength(HashMap<Integer, Boolean> hm, int k){
-        int ans  = 0;
-        while(hm.containsKey(k)){
-            ans++;
-            k++;
-        }
-        return ans;
+
+         for (Map.Entry<Integer,Boolean> m : h.entrySet())
+        {
+         int key = m.getKey();
+            if(h.get(key-1)!=null)
+            {
+                h.put(key,false);
+            }
+          }
+          System.out.println("VALUE "+h.get(3));
+
+int ans=0;
+        for (Map.Entry<Integer,Boolean> m : h.entrySet())
+        {
+        
+         int key = m.getKey();
+         if(h.get(key)==true)
+         {
+             int temp=1;
+             key++;
+             while(h.get(key)!=null && h.get(key)==false)
+             {
+                 temp++;
+                 key++;
+             }
+             ans= Math.max(temp,ans);
+         }
+            
+          }
+          return ans;
+        
     }
 }
