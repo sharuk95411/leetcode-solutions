12ab06c16f63b026abe6a981db140d72f3586e0c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-18 14:29:26 +0000

    Sync LeetCode submission Runtime - 67 ms (6.05%), Memory - 45.7 MB (100.00%)

--- solutions/0056-merge-intervals/solution.java
+++ solutions/0056-merge-intervals/solution.java
@@ -1,46 +1,56 @@
-class Solution {
-    
-    class Pair{
-        int start,end;
-        Pair(int start,int end)
-        {
-            this.start= start;
-            this.end= end;
-        }
+class Pair {
+
+    int row,col;
+    Pair(int r,int c)
+    {
+        row=r;
+        col=c;
     }
+}
+
+class Solution {
     public int[][] merge(int[][] arr) {
-        
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        List<Pair>list= new ArrayList<>();
-        
-        int start= arr[0][0];
-    
-        int end= arr[0][1];
-        for(int i=1;i<arr.length;i++)
-        {
-                if(end>=arr[i][0])
-                {
-                    if(end>arr[i][1]) continue;
-                    else   end= arr[i][1];
-                    
-                }
-            else if(arr[i][0]>end)
-            {
-                list.add(new Pair(start,end));
-                start= arr[i][0];
-                end= arr[i][1];
-                
-            }
-            
-        }
-        list.add(new Pair(start,end));
-        int ans[][]= new int [list.size()][2];
-        for(int i=0;i<list.size();i++)
-        {
-            Pair p= list.get(i);
-            ans[i][0]=p.start;
-            ans[i][1]= p.end;
-        }
-        return ans;
+Arrays.sort(arr, (a, b) -> Integer.compare(a[0],b[0])); //increasing orde
+
+for(int i=0;i<arr.length;i++)
+{
+    System.out.println("COL IS "+arr[i][0]+" AND "+arr[i][1]);
+}
+Stack<Pair>s = new Stack<>();
+
+   s.push(new Pair(arr[0][0],arr[0][1]));
+   int start= arr[0][1];
+
+   for(int i=1;i<arr.length;i++)
+   {
+       if(start>=arr[i][0])
+       {
+           Pair p = s.pop();
+           s.push(new Pair(p.row,Math.max(arr[i][1],p.col)));
+           start= Math.max(p.col,arr[i][1]);
+       }
+       else
+       {
+           s.push(new Pair(arr[i][0],arr[i][1]));
+           start= arr[i][1];
+       }
+   }
+
+   int ans[][]= new int[s.size()][2];
+   int i= ans.length-1;
+   while(i>=0)
+   {
+       Pair p= s.pop();
+       ans[i][0]= p.row;
+       ans[i][1]=p.col;
+       i--;
+   }
+   return ans;
+
+
+ 
+      
+
+
     }
 }
