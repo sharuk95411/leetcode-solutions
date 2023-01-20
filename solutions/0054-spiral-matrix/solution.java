be63c1bed58cb549bdcac8e9210b9fb45df3aa8d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-20 20:18:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.6 MB (100.00%)

--- solutions/0054-spiral-matrix/solution.java
+++ solutions/0054-spiral-matrix/solution.java
@@ -1,49 +1,52 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] arr) {
-        List<Integer>list= new ArrayList<>();
-        int top=0;
+        List<Integer>l= new ArrayList<>();
+        if(arr.length==0)return l;
         int left=0;
-        int down= arr.length-1;
         int right= arr[0].length-1;
-        int dir =0;
-        while(top<=down && left<=right)
+        int top=0;
+        int down= arr.length-1;
+        int dir=0;
+
+        while(left<=right && top<=down)
         {
-            if(dir==0)
-            {
-                for(int i= left;i<=right;i++)
-                {
-                    list.add(arr[top][i]);
-                }
-                top++;
-            }
-            else if(dir==1)
-            {
-                for(int i=top;i<=down;i++)
-                {
-                    list.add(arr[i][right]);
-                }
-                right--;
-            }
-            else if(dir==2)
-            {
-                for(int i=right;i>=left;i--)
-                {
-                    list.add(arr[down][i]);
-                }
-                down--;
-            }
-            else if(dir==3)
-            {
-                for(int i=down;i>=top;i--)
-                {
-                    list.add(arr[i][left]);
-                    
-                }
-                left++;
-            }
-            dir= (dir+1)%4;
+
+          if(dir==0)
+          {
+              for(int j=left;j<=right;j++)
+              {
+                  l.add(arr[left][j]);
+              }
+              top++;
+          }
+          else if(dir==1)
+          {
+              for(int i=top;i<=down;i++)
+              {
+                  l.add(arr[i][right]);
+              }
+              right--;
+          }
+          else if(dir==2)
+          {
+              for(int j=right;j>=left;j--)
+              {
+                  l.add(arr[down][j]);
+              }
+              down--;
+          }
+          else if(dir==3)
+          {
+              for(int i=down;i>=top;i--)
+              {
+                  l.add(arr[i][left]);
+              }
+              left++;
+          }
+
+  dir= (dir+1)%4;
+
         }
-        return list;
-        
+        return l;
     }
 }
