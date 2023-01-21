1110abe6af30a7989bcf8a3241c3c8fb6156782e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-21 13:17:05 +0000

    Sync LeetCode submission Runtime - 5 ms (15.71%), Memory - 48.2 MB (64.42%)

--- solutions/0240-search-a-2d-matrix-ii/solution.java
+++ solutions/0240-search-a-2d-matrix-ii/solution.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
-        
-        int x= 0;
-        int y= arr[0].length-1;
-        while((x>=0 &&x<arr.length ) && (y>=0 && y<arr[0].length))
+        int row= 0;
+        int col= arr[0].length-1;
+
+        while(col>=0 && row<arr.length)
         {
-            if(arr[x][y]==target)return true;
-            else
-            {
-                if(arr[x][y]>target) y--;
-                else x++;
-            }
-                
+           if(target<arr[row][col])
+           {
+               col--;
+           }
+           else if(target>arr[row][col])
+           {
+               row++;
+           }
+           else if(arr[row][col]==target)
+           return true;
         }
         return false;
     }
