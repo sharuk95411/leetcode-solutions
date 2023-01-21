5d869c3f9f9e9de37cb769c973faa46ebfeab118
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-01-21 13:38:27 +0000

    Sync LeetCode submission Runtime - 2 ms (20.79%), Memory - 44.2 MB (100.00%)

--- solutions/0073-set-matrix-zeroes/solution.java
+++ solutions/0073-set-matrix-zeroes/solution.java
@@ -1,42 +1,68 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        boolean row=false;
-        boolean col=false;
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr[0].length;j++)
-            {
-                if(arr[i][j]==0)
-                {
-                    if(i==0) row=true;
-                    if(j==0) col=true;
-                    arr[0][j]=0;
-                    arr[i][0]=0;
-                }
-            }
-        }
+        
+         boolean row= false;
+         for(int j=0;j<arr[0].length;j++)
+         {
+             if(arr[0][j]==0)
+             {
+                 row=true;
+                 break;
+             }
+         }
+         boolean col= false;
+         for(int i=0;i<arr.length;i++)
+         {
+             if(arr[i][0]==0)
+             {
+                 col=true;
+                 break;
+             }
+         }
 
-        for(int i=1;i<arr.length;i++)
-        {
-            for(int j=1;j<arr[0].length;j++)
-            {
-                if(arr[0][j]==0 || arr[i][0]==0)
-                arr[i][j]=0;
-            }
-        }
-        if(row==true)
-        {
-            for(int j=0;j<arr[0].length;j++)
-            {
-               arr[0][j]=0;
-            }
-        }
-        if(col==true)
-        {
-            for(int i=0;i<arr.length;i++)
-            {
-                arr[i][0]=0;
-            }
-        }
+         for(int i=1;i<arr.length;i++)
+         {
+             for(int j=arr[0].length-1;j>=1;j--)
+             {
+                 if(arr[i][j]==0)
+                 {
+                     arr[0][j]=0;
+                     arr[i][0]=0;
+                 }
+             }
+         }
+
+       for(int i=1;i<arr.length;i++)
+         {
+             for(int j=arr[0].length-1;j>=1;j--)
+             {
+                 if(arr[0][j]==0 || arr[i][0]==0)
+                 {
+                     arr[i][j]=0;
+                 }
+             }
+         }
+         if(row)
+         {
+             for(int j=0;j<arr[0].length;j++)
+             {
+                 arr[0][j]=0;
+             }
+             System.out.println("OK1");
+         }
+         if(col)
+         {
+             for(int i=0;i<arr.length;i++)
+             {
+                 arr[i][0]=0;
+             }
+              System.out.println("OK2");
+         }
+
+
+         
+
+
+        
     }
 }
