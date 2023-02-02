7a9f01186b42b4513f8605a5fcef80288075a321
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-02 15:12:46 +0000

    Sync LeetCode submission Runtime - 8 ms (4.45%), Memory - 62.8 MB (99.99%)

--- solutions/0134-gas-station/solution.java
+++ solutions/0134-gas-station/solution.java
@@ -1,30 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-     
         
-        int total_gas= 0;
-        int total_cost=0;
+        int rem=0;
+        int store=0;
+        int ans=0;
+
         for(int i=0;i<gas.length;i++)
         {
-            total_gas= total_gas+gas[i];
-            total_cost= total_cost+cost[i];
+             rem= rem+gas[i]-cost[i];
+             if(rem<0)
+             {
+                 store= store+rem;
+                 rem=0;
+                 ans=i+1;
+             }
         }
-        if(total_gas-total_cost<0) return -1;
-        // Now ab confirm ho gya h Circle to bnega ab each possible index check kr lge.
-        
-        int start=0;
-        int total_fuel=0;
-        for(int i=0;i<gas.length;i++)
-        {
-          total_fuel = total_fuel+gas[i]-cost[i];
-            
-            if(total_fuel<0)
-            {
-                start=i+1;
-                total_fuel=0;
-            }
-        }
-        
-        return start;
+        System.out.println("rem "+rem);
+         System.out.println("store "+store);
+        if(rem>=Math.abs(store)) return ans;
+        else return -1;
     }
 }
