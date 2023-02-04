ce32e5d13fb3f7dcc3aa4ed79b2f099eab4ef137
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-04 07:24:18 +0000

    Sync LeetCode submission Runtime - 21 ms (5.33%), Memory - 42.7 MB (99.00%)

--- solutions/0014-longest-common-prefix/solution.java
+++ solutions/0014-longest-common-prefix/solution.java
@@ -1,29 +1,33 @@
 class Solution {
-    public String longestCommonPrefix(String[] s) {
+    public String longestCommonPrefix(String[] strs) {
         
-    if(s.length==0) return "";
-        
-        String ans=s[0];
-        String str="";
-        
-        
-        for(int i=1;i<s.length;i++)
+        if(strs.length==1) return strs[0];
+        String s1= strs[0];
+        String ans="";
+        boolean flag=false;
+        for(int i=1;i<strs.length;i++)
         {
-            String temp= s[i];
-            int m=0,n=0;
-            while(m<ans.length()&& n<temp.length())
+            String s2= strs[i];
+            String temp="";
+            flag=false;
+            for(int j=0;j<Math.min(s1.length(),s2.length());j++)
             {
-                if(ans.charAt(m)==temp.charAt(n))
-                {
-                   str= str+ans.charAt(m);
-                    m++;
-                    n++;
-                }
-                else break;
+                     if(s1.charAt(j)==s2.charAt(j))
+                     {
+                        temp=temp+s1.charAt(j);
+                        flag=true;
+                     }     
+                     else break;
             }
-            ans= str;
-            str="";
+                    if(ans.length()>temp.length())
+                     {
+                         ans=temp;
+                     }
+                     if(ans.length()==0) ans=temp;
+                     System.out.println("OK "+ans);
+                     if(flag==false) return "";
+          
         }
-         return ans;
+          return ans;
     }
 }
