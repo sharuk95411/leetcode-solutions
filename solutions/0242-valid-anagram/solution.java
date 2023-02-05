3c782611958e81bbce2b9689a8f8ddd2f4b0cf4a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-05 08:24:43 +0000

    Sync LeetCode submission Runtime - 4 ms (82.38%), Memory - 42.2 MB (100.00%)

--- solutions/0242-valid-anagram/solution.java
+++ solutions/0242-valid-anagram/solution.java
@@ -1,21 +1,16 @@
 class Solution {
-    // bht acha use kia gya freq ka with in array jiske advantage se hashmap ka use krne se bach gye
     public boolean isAnagram(String s, String t) {
-     
+        
+        if(s.length()!=t.length()) return false;
         int arr[]= new int[26];
-        for(int i=0;i<s.length();i++)
-        {
-            arr[s.charAt(i)-'a']++;
-            
-        }
         for(int i=0;i<t.length();i++)
         {
-            arr[t.charAt(i)-'a']--;
-            
+            arr[t.charAt(i)-'a']++;
         }
-        for(int a :arr)
+        for(int i=0;i<s.length();i++)
         {
-            if(a!=0) return false;
+            if(arr[s.charAt(i)-'a']==0) return false;
+            else arr[s.charAt(i)-'a']--;
         }
         return true;
     }
