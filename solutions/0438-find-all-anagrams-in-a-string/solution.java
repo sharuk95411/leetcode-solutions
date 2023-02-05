dcae08701284125ba6fd307d8150cb127672cb2c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-05 13:02:27 +0000

    Sync LeetCode submission Runtime - 6 ms (98.11%), Memory - 43 MB (100.00%)

--- solutions/0438-find-all-anagrams-in-a-string/solution.java
+++ solutions/0438-find-all-anagrams-in-a-string/solution.java
@@ -1,33 +1,29 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer>list= new ArrayList<>();
-        
         if(p.length()>s.length()) return list;
-        int pF[]= new int[26];
-        
+
+        int arr1[]= new int[26];
+        int arr2[]= new int[26];
+
         for(int i=0;i<p.length();i++)
         {
-            pF[p.charAt(i)-'a']++;
+            arr1[p.charAt(i)-'a']++;
         }
-        
-        int sF[]= new int[26];
-        
         for(int i=0;i<p.length();i++)
         {
-            sF[s.charAt(i)-'a']++;
+            arr2[s.charAt(i)-'a']++;
         }
-        System.out.println(sF[1]);
-        if(Arrays.equals(pF, sF))list.add(0);
-        int index=0;
+        if(Arrays.equals(arr1,arr2))list.add(0);
+        int j=0;
         for(int i=p.length();i<s.length();i++)
         {
-            sF[s.charAt(index)-'a']--;
-            sF[s.charAt(i)-'a']++;
-            index++;
-             if(Arrays.equals(pF, sF))list.add(index);
+            arr2[s.charAt(j)-'a']--;
+            arr2[s.charAt(i)-'a']++;
+            if(Arrays.equals(arr1,arr2))list.add(j+1);
+            j++;
         }
         return list;
-        
-        
+      
     }
 }
