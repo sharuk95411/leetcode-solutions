d429b7188afe2e6b2def71fecd89af4e97b0eab9
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-10 18:14:51 +0000

    Sync LeetCode submission Runtime - 5 ms (1.77%), Memory - 42.3 MB (99.95%)

--- solutions/0017-letter-combinations-of-a-phone-number/solution.java
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.java
@@ -1,33 +1,30 @@
 class Solution {
+     List<String>l= new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
-        if(digits.length() == 0)
-            return ans;
-        
-        HashMap<Character, String> hm = new HashMap<>();
-        hm.put('2', "abc");
-        hm.put('3', "def");
-        hm.put('4', "ghi");
-        hm.put('5', "jkl");
-        hm.put('6', "mno");
-        hm.put('7', "pqrs");
-        hm.put('8', "tuv");
-        hm.put('9', "wxyz");
-        
-        backtrack(digits, 0, hm, new StringBuilder(), ans);
-        return ans;
-        
+
+        if(digits.length()==0) return l;
+        String arr[]= {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        A("",arr,0,digits);
+        return l;
     }
-        public void backtrack(String digits, int i, HashMap<Character, String> hm, StringBuilder sb, List<String> ans){
-        if(i == digits.length()){
-            ans.add(sb.toString());
-            return;
+
+    public void A(String ans,String arr[],int index,String s)
+    {
+        if(index==s.length())
+        {
+           l.add(ans);
+           return ; 
         }
-        String curr =  hm.get(digits.charAt(i));
-        for(int k =0; k <curr.length(); k++){
-            sb.append(curr.charAt(k));
-            backtrack(digits, i+1, hm,  sb, ans);
-            sb.deleteCharAt(sb.length() -1);
+
+        int j= s.charAt(index)-'0';
+        String temp= arr[j-2];
+        for(int i=0;i<temp.length();i++)
+        {
+            ans=ans+temp.charAt(i);
+            A(ans,arr,index+1,s);
+            ans=ans.substring(0,ans.length()-1);
         }
-        }
+
+         
+    }
 }
