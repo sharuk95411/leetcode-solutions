805c8f4a5f2d75324c60646e633b0b1d9b3fef98
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-10 21:32:56 +0000

    Sync LeetCode submission Runtime - 27 ms (6.83%), Memory - 42.5 MB (99.96%)

--- solutions/0067-add-binary/solution.java
+++ solutions/0067-add-binary/solution.java
@@ -1,26 +1,35 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder ans= new StringBuilder();
-        int i=a.length()-1;
-        int j= b.length()-1;
-        int carry=0;
-        int sum=0;
-     int n1=0;
-     int n2=0;
-        while(i>=0 || j>=0)
-        {
-           if(i>=0)  n1= a.charAt(i)-'0';
-           else n1=0;
-           if(j>=0)   n2= b.charAt(j)-'0';
-           else n2=0;
-           sum= n1+n2+carry;
-           if(sum>1) carry =1;
-           else carry=0;
-           ans.append(sum%2);
-           i--;
-           j--;
-        }
-        if(carry>0)ans.append(carry);
-        return ans.reverse().toString();
+
+int i=a.length()-1;
+int j= b.length()-1;
+
+int sum=0;
+int carry =0;
+int l= Math.max(i,j);
+
+for(int k=l;l>=0;l--)
+{
+    int n= (i<0)? 0: Character.getNumericValue(a.charAt(i));
+    int m= (j<0)? 0: Character.getNumericValue(b.charAt(j));  
+     System.out.println("N "+n +" M "+m);
+    sum= carry+m+n;
+      if(sum>1)
+      {
+          ans.append(String.valueOf(sum%2));
+          carry=1;
+      }
+      else
+      {
+          ans.append(String.valueOf(sum));
+          carry=0;
+      }
+      i--;
+      j--;
+}
+
+if(carry>0) ans.append("1");
+return ans.reverse().toString();
     }
 }
