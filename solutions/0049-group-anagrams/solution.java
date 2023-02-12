69a8bb62065edbc8be451fad2f8b1dd98704a140
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-12 07:26:39 +0000

    Sync LeetCode submission Runtime - 22 ms (8.35%), Memory - 47 MB (100.00%)

--- solutions/0049-group-anagrams/solution.java
+++ solutions/0049-group-anagrams/solution.java
@@ -1,33 +1,13 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] arr) {
-        HashMap<String,List<String>>h= new HashMap<>();
-
-        for(int i=0;i<arr.length;i++)
-        {
-               char ch[]= arr[i].toCharArray();
-               Arrays.sort(ch);
-              String s = new String(ch);  
-              
-               if(h.containsKey(s))
-               {
-                   h.get(s).add(arr[i]);
-               }
-               else
-               {
-                   h.put(s,new ArrayList<>());
-                   h.get(s).add(arr[i]);
-               }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap();
+        for (String s : strs) {
+            int[] m = new int[26];
+            for (char c : s.toCharArray()) m[c-'a']++;
+            String key = Arrays.toString(m);
+            map.putIfAbsent(key, new ArrayList());
+            map.get(key).add(s);
         }
-
-        List<List<String>>outer= new ArrayList<>();
-     for (Map.Entry<String,List<String>> m : h.entrySet())
-{
-
-      List<String>inner = m.getValue();
-      System.out.println(inner);
-      outer.add(new ArrayList<>(inner));
-}
-return outer;
-
+        return new ArrayList(map.values());
     }
 }
