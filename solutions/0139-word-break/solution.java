0490cd704cbd0fe5ff07b9f3a0893f0be7421aff
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-02-25 19:03:37 +0000

    Sync LeetCode submission Runtime - 12 ms (5.21%), Memory - 42.5 MB (99.56%)

--- solutions/0139-word-break/solution.java
+++ solutions/0139-word-break/solution.java
@@ -1,24 +1,53 @@
-class Solution {
-    Map<String,Boolean> map= new HashMap<>();
-    public boolean wordBreak(String s, List<String> l) {
-         if(l.contains(s))
-        {
-            return true;
-        }
-        if(map.containsKey(s))
-            return map.get(s);
-         for(int i=1;i<=s.length();i++)
-        {
-        String prefix= s.substring(0,i);
-        if(l.contains(prefix) && wordBreak(s.substring(i),l))
-        {
-            map.put(s,true);
-        return true;
-        }
-        else map.put(s,false);
-  
-    }
-  return false;
+// class Solution {
+//     Map<String,Boolean>map= new HashMap<>();
+//     public boolean wordBreak(String s, List<String> wordDict) {
         
+//         if(wordDict.contains(s)) return true;
+//         return A(s,wordDict);
+        
+//     }
+//     public boolean A(String s, List<String>list)
+//     {
+//         if(s.length()==0)
+//         {
+//            return true;
+//         } 
+//         if(map.containsKey(s)) return true;
+//         int i=1;
+//         for(i=1;i<=s.length();i++)
+//         {
+//             if(list.contains(s.substring(0,i)))
+//             {
+//                 System.out.println("OK");
+//                 map.put(s.substring(0,i),true);
+//                 if(A(s.substring(i,s.length()),list)) return true;
+//             }
+           
+//         }
+       
+//         return false;
+//     }
+// }
+class Solution {
+ Map<String,Boolean>map= new HashMap<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        
+        if(wordDict.contains(s)) return true;
+        if(map.containsKey(s)) return map.get(s);
+        
+        for(int i=1;i<=s.length();i++)
+        {
+            String left= s.substring(0,i);
+    
+            if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict))
+            {
+                map.put(s,true);
+                        System.out.println("OK");
+                return true;
+            }
+            else map.put(s,false);
+        }
+        return false;
     }
 }
