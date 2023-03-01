48c2d5406aa36aae9f82e2152b79e0a343295e3f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-01 17:36:11 +0000

    Sync LeetCode submission Runtime - 7 ms (68.78%), Memory - 42.9 MB (99.96%)

--- solutions/0567-permutation-in-string/solution.java
+++ solutions/0567-permutation-in-string/solution.java
@@ -1,37 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
-        HashMap<Character,Integer>h1= new HashMap<>();
-        
         if(s1.length()>s2.length()) return false;
-        
+
+       int arr1[]= new int[26];
+
         for(int i=0;i<s1.length();i++)
         {
-            char c= s1.charAt(i);
-            h1.put(c,h1.getOrDefault(c,0)+1);
+            arr1[s1.charAt(i)-'a']++;
         }
-        
-        HashMap<Character,Integer>h2= new HashMap<>();
+
+        int arr2[]= new int[26];
+
         for(int i=0;i<s1.length();i++)
         {
-            char c= s2.charAt(i);
-            h2.put(c,h2.getOrDefault(c,0)+1);
+            arr2[s2.charAt(i)-'a']++;
         }
+
+        if(Arrays.equals(arr1,arr2)) return true;
         int j=0;
-        if(h1.equals(h2)) return true;
         for(int i=s1.length();i<s2.length();i++)
         {
-        
-              char c= s2.charAt(j);
-              int value= h2.get(c);
-              if(value==1)h2.remove(c);
-              else h2.put(c,value-1);
-              c= s2.charAt(i);          
-              h2.put(c,h2.getOrDefault(c,0)+1);
-              if(h1.equals(h2)) return true;
-              System.out.println(h2);
-              j++;   
-            
+            arr2[s2.charAt(j)-'a']--;
+            arr2[s2.charAt(i)-'a']++;
+            j++;
+             if(Arrays.equals(arr1,arr2)) return true;
         }
         return false;
     }
