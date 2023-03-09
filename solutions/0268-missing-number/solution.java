c70a10f03eb681ae33bc20ddbd6f239ad2e1d5ac
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-09 18:24:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.1 MB (100.00%)

--- solutions/0268-missing-number/solution.java
+++ solutions/0268-missing-number/solution.java
@@ -1,12 +1,14 @@
 class Solution {
- /*   From elementary school math, we have a popular math trick which is the sum of 1+2+...+n = n*(n+1)/2, it can be used here. Since we are finding the missing number, just get the sum of all the n number first using the formula, and the minus it to the sum of all the numbers in the array, we get the missing number. */
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int total = n * (n + 1) / 2;
-        int sum = 0;
-        for(int num : nums) {
-            sum += num;
+        
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
         }
-        return total - sum;
+        int n= nums.length;
+        int ans=n*(n+1)/2;
+        return ans-sum;
+
     }
 }
