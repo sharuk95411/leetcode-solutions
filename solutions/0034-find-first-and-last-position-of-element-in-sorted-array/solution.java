8b3d79390767df0120bc1005cd4cd4387ece41d2
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-09 18:06:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.1 MB (99.99%)

--- solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,53 +1,56 @@
-// simple logic h 2 binary serch mthd bnakr leftmost and rightmost element k liye chla do and ye sol 100 acceptance rate ay h iska.
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int a[]= new int[2];
-        
-        a[0]= binary_search(0,nums.length-1,nums,target);
-        if(a[0]==-1) return new int[]{-1,-1};
-         a[1]= binarysearch(0,nums.length-1,nums,target); 
-        
-        return a;
-       // return new int[]{1,2};
+        int ans[]= new int[2];
+         ans[0]= leftPosition(nums,target);
+         ans[1]= rightPosition(nums,target);
+        return ans;
     }
-    
-    private int binary_search(int left,int right,int arr[],int target)
+    public int leftPosition(int arr[],int target)
     {
+        int left=0;
+        int right=arr.length-1;
+        int ans=-1;
         while(left<=right)
         {
-            int mid = left+ (right-left)/2;
-            if(arr[mid]==target)
-            {
-                if(mid>0 && arr[mid-1]==target)
-                {
-                    right= mid-1;
-                }
-                else
-                    return mid;
-            }
-            else if(arr[mid]>target) right= mid-1;
-            else left=mid+1;
+            int mid= (left+right)/2;
+            if(target==arr[mid])
+        {
+             ans=mid;
+             right=mid-1;
         }
-        return -1;
+        else if(target>arr[mid])
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right= mid-1;
+        }
+        }
+        return ans;
     }
-    private int binarysearch(int left,int right,int arr[],int target)
+    public int rightPosition(int arr[],int target)
     {
+        int left=0;
+        int right=arr.length-1;
+        int ans=-1;
         while(left<=right)
         {
-            int mid= left+(right-left)/2;
-            if(arr[mid]==target)
-            {
-                if(mid<=arr.length-2 && arr[mid+1]==target)
-                {
-                    left= mid+1;
-                }
-                else
-                    return  mid;
-            }
-            if(arr[mid]>target) right= mid-1;
-            else left= mid+1;
+            int mid= (left+right)/2;
+            if(target==arr[mid])
+        {
+             ans=mid;
+             left=mid+1;
         }
-        return -1;
+        else if(target>arr[mid])
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right= mid-1;
+        }
+        }
+        return ans;
     }
 }
