a8123c4209042f28251e5c10d58c1ee6b2f0996d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-11 20:17:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.3 MB (100.00%)

--- solutions/0540-single-element-in-a-sorted-array/solution.java
+++ solutions/0540-single-element-in-a-sorted-array/solution.java
@@ -1,33 +1,22 @@
 class Solution {
-    int ans=0;
-    public int singleNonDuplicate(int[] nums) {
-     
-        if(nums.length==0) return 0;
-        if(nums.length==1) return 1;
-        
-        int left=0;
-        int right= nums.length-1;
-        int mid= left+(right-left)/2; 
-        check(nums,left,right);
-        return ans;
-        }
-        
-       private void check(int []nums,int left,int right)
+    public int singleNonDuplicate(int[] arr) {
+        int l=0;
+        int r=arr.length-1;
+        while(l<=r)
         {
-           while(left<right)
-           {
-           int mid=left+(right-left)/2;
-           if(mid%2==1 && nums[mid-1]==nums[mid] || (mid%2==0 && nums[mid]==nums[mid+1]))
-           {
-                 left=mid+1;
-           }
-               else 
-               {
-                   right=mid;
-               }  
-               
-           }
-          ans= nums[left];
+            if(l==r) return arr[l];
+            int mid= l+(r-l)/2;
+            if(mid%2==0)
+            {
+             if(arr[mid]==arr[mid+1]) l=mid+2;
+                else r=mid;
+            }
+            else
+            {
+                if(arr[mid]==arr[mid+1]) r=mid-1;
+                else l= mid+1;
+            }
         }
-    
+        return -1;
+    }
 }
