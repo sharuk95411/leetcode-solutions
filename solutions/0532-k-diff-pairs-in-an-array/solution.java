2cc309fe90021f78b6e15ef76d7e0021d42dec20
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-13 18:58:03 +0000

    Sync LeetCode submission Runtime - 9 ms (97.25%), Memory - 43.2 MB (100.00%)

--- solutions/0532-k-diff-pairs-in-an-array/solution.java
+++ solutions/0532-k-diff-pairs-in-an-array/solution.java
@@ -1,27 +1,29 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-     
-        HashMap<Integer,Integer>h= new HashMap<>();
+         HashMap<Integer,Integer>h= new HashMap<>();
         
         for(int i=0;i<nums.length;i++)
         {
             h.put(nums[i],i);
         }
         
-        List<Integer>list= new ArrayList<>();
-        
         int ans=0;
+        
         
         for(int i=0;i<nums.length;i++)
         {
-            int diff= k+nums[i];
+            int target= k+nums[i];
             
-           if(h.containsKey(diff)==true && h.get(diff)!=i)
+           if(h.containsKey(target)==true && h.get(target)!=i)
             {
                 ans++;
-                h.remove(diff);
+                h.remove(target);  // its remove bcz we find the distinct pair
             }
         }
         return ans;
+
+
+
+
     }
 }
