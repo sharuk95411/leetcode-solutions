afb0f2bfaaebc53faee90eb8b07271d341b54be0
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-14 18:57:19 +0000

    Sync LeetCode submission Runtime - 5 ms (55.94%), Memory - 44.8 MB (100.00%)

--- solutions/0658-find-k-closest-elements/solution.java
+++ solutions/0658-find-k-closest-elements/solution.java
@@ -1,46 +1,24 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> res = new ArrayList<>(k);
-            int index = Arrays.binarySearch(arr,x);
-            if(index < 0) index = -(index + 1);
-            boolean left = true, right = true;
-            int l = index-1, r = index;
-            
-            if(index == 0) left = false;
-            if(index == arr.length) right = false;
-
-            while (res.size() < k) {
-                if(left && right) {
-                    int tempLeft = arr[l], tempRight = arr[r];
-                    int a = Math.abs(tempLeft - x), b = Math.abs(tempRight - x);
-                    if(a < b) {
-                        res.add(tempLeft);
-                        l--;
-                    }
-                    else if(b < a) {
-                        res.add(tempRight);
-                        r++;
-                    }
-                    else {
-                        res.add(Math.min(tempLeft, tempRight));
-                        l--;
-                    }
-
-                    if(l < 0) left = false;
-                    if(r >= arr.length) right = false;
-                    continue;
-                }
-                if(left) {
-                    res.add(arr[l--]);
-                    if(l < 0) left = false;
-                    continue;
-                }
-                if(right) {
-                    res.add(arr[r++]);
-                    if(r >= arr.length) right = false;
-                }
-            }
-            Collections.sort(res); // wanting to reduce this O(K * log(k)) to any complexity O(k)
-            return res;
+     List<Integer>ans= new ArrayList<>();
+     int i=0;
+     int j=arr.length-1;
+     while(j-i>=k)
+     {
+           int left= Math.abs(arr[i]-x);
+           int right= Math.abs(arr[j]-x);
+           if(right>left || left==right)
+           {
+               j--;
+           }
+           else i++;
+        
+     }
+     while(i<=j)
+     {
+         ans.add(arr[i]);
+         i++;
+     }
+     return ans;   
     }
 }
