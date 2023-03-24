d4f5f9cb3f957aa21721e9cef6bb0eb0a187e75e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-03-24 22:01:19 +0000

    Sync LeetCode submission Runtime - 4 ms (16.91%), Memory - 43.1 MB (99.32%)

--- solutions/0088-merge-sorted-array/solution.java
+++ solutions/0088-merge-sorted-array/solution.java
@@ -1,41 +1,31 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        // if(m==0)
-        // {
-        // for(int i=0;i<n;i++)
-        // {
-        //     nums1[i]= nums2[i];
-        // }
-        //     return ;
-        // }
-        // if(n==0) return;
-        int j=m-1;
-        int i=n-1;
-        
-        int length= m+n-1;
-        while(i>=0 && j>=0)
-        {
-            if(nums2[i]>=nums1[j])
-            {
-                nums1[length]= nums2[i];
-                length--;
-                i--;
-            }
-            else
-            {
-                nums1[length]=nums1[j];
-                    length--;
-                j--;
-            
-            }
-        }
-        while(i>=0)
-        {
-            nums1[length]= nums2[i];
-            i--;
-            length--;
-        }
-        
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+        System.out.println("LENGTH IS "+m);
+      int i=m-1;
+      int j= m+n-1;
+      int k=n-1;
+
+      while(k>=0 && i>=0)
+      {
+          if(arr1[i]>arr2[k])
+          {
+              arr1[j]= arr1[i];
+              i--;
+              j--;
+          }
+          else 
+          {
+              arr1[j]= arr2[k];
+              j--;
+              k--;
+          }
+      }
+      while(k>=0)
+      {
+          arr1[j]=arr2[k];
+          j--;
+          k--;
+      }
+
     }
 }
