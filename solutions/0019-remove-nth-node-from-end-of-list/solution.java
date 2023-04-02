cab5cfbeae2274df80b2c83f982b3fcf64d10f15
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-04-02 20:09:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.2 MB (100.00%)

--- solutions/0019-remove-nth-node-from-end-of-list/solution.java
+++ solutions/0019-remove-nth-node-from-end-of-list/solution.java
@@ -1,6 +1,3 @@
-// logic self- Step 1- first find total no nodes.
-// step2 now find m jo ki delete krne wle node ka no dega from start
-// step3 -ab logic lga do node kis trh delete krna h jbki node no pta chl gya h start se 
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,41 +10,26 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head==null) return head;
-       int count=0;
-       ListNode temp= head;
-       while(temp!=null)
-       {
-           count++;
-           temp=temp.next;
-       }
-       
-int m= count-n+1;// yha node no pta chl gya h strt se ki kis no wle node ko delete krna h
-       if(m==1)
-       { 
-          temp=head;
-          head=head.next;
-          temp.next=null;
-          return head;
-           
-       }
-      else
-      { 
-          count=2;
-          ListNode slow=head;
-          ListNode fast=head.next;
-          
-          while(count!=m)
-          {
-              
-              slow=slow.next;
-              fast= fast.next;
-              count++;
-          }
-          slow.next= fast.next;
-          return head;
-          
-      }
-        
+        ListNode curr= head;
+        int count=0;
+        while(curr!=null)
+        {
+            count++;
+             curr= curr.next;
+        }
+        int remove=count-n;
+        if(remove==0) return head.next;
+        count=0;
+        ListNode prev=head;
+        curr= head.next;
+        while(count<remove-1)
+        {
+            prev=curr;
+            curr=curr.next;
+            count++;
+        }
+        prev.next= curr.next;
+        return head;
+
     }
 }
