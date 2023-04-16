02f236c8a6cb360a12c96deba6ada44a06dd9daa
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-04-16 11:01:50 +0000

    Sync LeetCode submission Runtime - 1 ms (99.94%), Memory - 42.5 MB (99.99%)

--- solutions/0090-subsets-ii/solution.java
+++ solutions/0090-subsets-ii/solution.java
@@ -1,18 +1,26 @@
 class Solution {
+    List<List<Integer>>outer= new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer>inner= new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        helper(res,new ArrayList<>(),nums,0);
-        return res;
+        // outer.add(new ArrayList<>(inner));
+        fun(0,nums,inner);
+        return outer;
     }
-    
-    public void helper(List<List<Integer>> res, List<Integer> ls, int[] nums, int pos) {
-        res.add(new ArrayList<>(ls));
-        for(int i=pos;i<nums.length;i++) {
-            if(i>pos&&nums[i]==nums[i-1]) continue;
-            ls.add(nums[i]);
-            helper(res,ls,nums,i+1);     
-            ls.remove(ls.size()-1);
+
+    public void fun(int i,int arr[],List<Integer>inner)
+    {
+        if(i==arr.length)
+        {
+            // if(!outer.contains(inner))
+            outer.add(new ArrayList<>(inner));
+            return ;
         }
+        inner.add(arr[i]);
+        fun(i+1,arr,inner);
+        inner.remove(inner.size()-1);
+  while(i+1<arr.length && arr[i]==arr[i+1]) i++;  // check for not add dublicates 
+        fun(i+1,arr,inner);
+    
     }
 }
