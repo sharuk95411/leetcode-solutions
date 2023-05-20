25fb790bcc32f0ca9c27086fd1a2b4b4cb27437b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-05-20 07:38:13 +0000

    Sync LeetCode submission Runtime - 235 ms (9.99%), Memory - 40.7 MB (99.98%)

--- solutions/0079-word-search/solution.java
+++ solutions/0079-word-search/solution.java
@@ -1,38 +1,44 @@
 class Solution {
     boolean ans= false;
     public boolean exist(char[][] board, String word) {
-     if(board.length==0)return ans;
-        else if(word.length()==0) return ans;
+        
+           boolean b[][]= new boolean[board.length][board[0].length];
+      
         for(int i=0;i<board.length;i++)
         {
             for(int j=0;j<board[0].length;j++)
             {
                 if(board[i][j]==word.charAt(0))
                 {
-                    dfs(board,word,i,j,0);
-                        if(ans==true) return ans;
+                    System.out.println("OK");
+                    A(board,word,b,i,j,0);
                 }
+                  
             }
         }
+     
         return ans;
     }
-    
-    private void dfs(char board[][],String word,int i,int j,int index)
-    {
-        if(index==word.length())
-        {
-            ans= true;
-            return ;
-        }
-    if(i<0 || j<0|| i>=board.length|| j>=board[0].length || board[i][j]!=word.charAt(index)) return;
-        
-        char temp = board[i][j];
-        board[i][j]= '*';// yha boolean array ki jgh is trike ko use kia gya h jisse hmra extra space save ho rha h 
-        dfs(board,word,i,j+1,index+1);
-        dfs(board,word,i+1,j,index+1);
-        dfs(board,word,i,j-1,index+1);
-        dfs(board,word,i-1,j,index+1);
-        board[i][j]= temp; // Backtraking
-        return ;
-    }
+
+public void A(char arr[][],String word,boolean b[][],int row,int col,int index)
+{
+if(index==word.length())
+{
+    ans=true;
+    return;
+}
+if(row<0 || row>=arr.length) return ;
+if(col<0 || col>=arr[0].length) return;
+if(arr[row][col]!=word.charAt(index)) return;
+if(arr[row][col]==word.charAt(index) && b[row][col]==false)
+{
+        b[row][col]=true;
+        A(arr,word,b,row,col+1,index+1);
+        A(arr,word,b,row+1,col,index+1);
+        A(arr,word,b,row,col-1,index+1);
+        A(arr,word,b,row-1,col,index+1);
+        b[row][col]=false;
+}
+
+}
 }
