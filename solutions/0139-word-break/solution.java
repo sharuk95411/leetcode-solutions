d4ce2df1e5f08cc7fd5350866b7b077f42862fc1
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-05-21 07:48:17 +0000

    Sync LeetCode submission Runtime - 9 ms (25.88%), Memory - 43.8 MB (76.64%)

--- solutions/0139-word-break/solution.java
+++ solutions/0139-word-break/solution.java
@@ -1,53 +1,20 @@
-// class Solution {
-//     Map<String,Boolean>map= new HashMap<>();
-//     public boolean wordBreak(String s, List<String> wordDict) {
-        
-//         if(wordDict.contains(s)) return true;
-//         return A(s,wordDict);
-        
-//     }
-//     public boolean A(String s, List<String>list)
-//     {
-//         if(s.length()==0)
-//         {
-//            return true;
-//         } 
-//         if(map.containsKey(s)) return true;
-//         int i=1;
-//         for(i=1;i<=s.length();i++)
-//         {
-//             if(list.contains(s.substring(0,i)))
-//             {
-//                 System.out.println("OK");
-//                 map.put(s.substring(0,i),true);
-//                 if(A(s.substring(i,s.length()),list)) return true;
-//             }
-           
-//         }
-       
-//         return false;
-//     }
-// }
 class Solution {
- Map<String,Boolean>map= new HashMap<>();
-    public boolean wordBreak(String s, List<String> wordDict) {
-        
-        
-        if(wordDict.contains(s)) return true;
+     Map<String,Boolean>map= new HashMap<>();
+    public boolean wordBreak(String s, List<String> word) {
+       
         if(map.containsKey(s)) return map.get(s);
+         if(word.contains(s)) return true;
         
         for(int i=1;i<=s.length();i++)
         {
             String left= s.substring(0,i);
-    
-            if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict))
+            if(word.contains(left) && wordBreak(s.substring(i),word))
             {
                 map.put(s,true);
-                        System.out.println("OK");
                 return true;
             }
-            else map.put(s,false);
         }
+        map.put(s,false);
         return false;
     }
 }
