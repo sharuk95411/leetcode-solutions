7b35da942f0b37a7401bbdcad349fe72696bd00c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-05-27 15:27:15 +0000

    Sync LeetCode submission Runtime - 4 ms (74.56%), Memory - 43.6 MB (99.98%)

--- solutions/0496-next-greater-element-i/solution.java
+++ solutions/0496-next-greater-element-i/solution.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int[] nextGreaterElement(int[] findNums, int[] nums) {
-        
-         Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
-        Stack<Integer> stack = new Stack<>();
-        for (int num : nums) {
-            while (!stack.isEmpty() && stack.peek() < num)
-            {   map.put(stack.pop(), num);
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int ans[]= new int[nums1.length];
+        HashMap<Integer,Integer>h= new HashMap<>();
+        Stack<Integer>stack= new Stack<>();
+        stack.push(nums2[0]);
+        for(int i=1;i<nums2.length;i++)
+        {
+                        while(!stack.isEmpty()&& nums2[i]>stack.peek())
+                          {
+                              h.put(stack.pop(),nums2[i]);
+                          }
+                          stack.push(nums2[i]);                     
+        }
+
+        for(int i=0;i<nums1.length;i++)
+        {
+            if(h.get(nums1[i])==null) ans[i]= -1;
+            else
+            {
+                ans[i]= h.get(nums1[i]);
             }
-            
-            stack.push(num);
-        }   
-        for (int i = 0; i < findNums.length; i++)
-            findNums[i] = map.getOrDefault(findNums[i], -1);
-        return findNums;
+        }
+
+        return ans;
     }
 }
