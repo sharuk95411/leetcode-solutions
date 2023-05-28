233bfc6fd27226d3628fbf92bb346d488a66dc78
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-05-28 08:19:07 +0000

    Sync LeetCode submission Runtime - 720 ms (4.57%), Memory - 45.1 MB (99.98%)

--- solutions/0042-trapping-rain-water/solution.java
+++ solutions/0042-trapping-rain-water/solution.java
@@ -1,57 +1,27 @@
 class Solution {
     public int trap(int[] arr) {
-       if(arr.length<3) return 0;
-   /*     int left_max=Integer.MIN_VALUE;
-        int right_max= Integer.MIN_VALUE;
-        int trapped_water=0;
-        for(int i=1;i<arr.length-1;i++)
-        {
-            int j=0;
-            while(j<i)
-            {
-                left_max= Math.max(left_max,arr[j]);
-                j++;
-                
-            }
-           
-            j++;
-            while(j<arr.length)
-            {
-                right_max= Math.max(right_max,arr[j]);
-                j++;
-            }
-            int a= Math.min(left_max,right_max);
-            if(a>arr[i])
-            {
-            trapped_water= trapped_water+(a-arr[i]);
-            }
-            left_max=-1;
-            right_max=-1;
-                
-        }
-        return trapped_water;*/
-        
-        int trapped_water=0;
-        int left_max= arr[0];
-        int right_max= arr[arr.length-1];
-        int left= 1;
-            int right= arr.length-2;
-        while(left<=right)
-        {
-            if(right_max>left_max)
-            {
-                if(left_max>arr[left])trapped_water= trapped_water+(left_max-arr[left]);
-                left_max=Math.max(left_max,arr[left]);
-                left++;
-                
-            }
-            else
-            {
-                if(right_max>arr[right]) trapped_water= trapped_water+(right_max-arr[right]);
-                right_max= Math.max(right_max,arr[right]);
-                    right--;
-            }
-        }
-        return trapped_water;
+
+      int ans=0;
+      for(int i=1;i<arr.length-1;i++)
+      {
+          int m1= Integer.MIN_VALUE;
+          int left=i-1;
+          while(left>=0)
+          {
+              m1=Math.max(m1,arr[left]);
+              left--;
+          }
+          int m2= Integer.MIN_VALUE;
+          int right=i+1;
+          while(right<arr.length)
+          {
+              m2= Math.max(m2,arr[right]);
+              right++;
+          }
+         int get= Math.min(m1,m2)-arr[i];
+         if(get>0) ans= ans+get;
+      }  
+
+      return ans;
     }
 }
