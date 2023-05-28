26e1f4e7e1557f276bcdc190e7aaed4685b74ee2
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-05-28 06:30:07 +0000

    Sync LeetCode submission Runtime - 23 ms (13.27%), Memory - 44.9 MB (100.00%)

--- solutions/0503-next-greater-element-ii/solution.java
+++ solutions/0503-next-greater-element-ii/solution.java
@@ -1,50 +1,28 @@
 class Solution {
-    // monotonic decreasing stack ka concept use hua h.
-// yha array kha se strt hga ye identyfity krna tmhra km h 
-    public int[] nextGreaterElements(int[] arr) {
+    public int[] nextGreaterElements(int[] nums) {
         
-        int n= arr.length;
         Stack<Integer>stack= new Stack<>();
-        stack.push(arr[n-1]);
-        
-    int index,i,temp;
-        for(index=n-1;index>=0;index--)
+        int ans[]= new int[nums.length];
+        for(int i=0;i<nums.length;i++)
         {
-            if(arr[index]>=stack.peek())
-            {    
-                temp= arr[index];
-                while(stack.size()>0 && temp>=stack.peek())
-           {
-               stack.pop();
-               if(stack.size()==0) // element  right side ni h an left me check krna h
-               {
-                   for(i=0;i<index;i++)
-                   {
-                       if(arr[i]>temp)
-                       {
-                           arr[index]=arr[i];
-                           break;
-                       }
-                       
-                   }
-                   if(i==index) arr[index]=-1;
-               } // closed the if bloack which traverse the left
-               
-           }  // closed while loop
-            
-          if(stack.size()>0) arr[index]=stack.peek(); // mtlb element right side me mil gya h
-                stack.push(temp);
-                
-            } // closed if bloack
-            
-            else
+            while(!stack.isEmpty() && nums[i]>nums[stack.peek()])
             {
-                temp=arr[index];
-                arr[index]= stack.peek();
-                stack.push(temp);
+                ans[stack.pop()]=nums[i];
             }
-            
+            stack.push(i);
         }
-        return arr;
+
+        for(int i=0;i<nums.length;i++)
+        {
+              while(!stack.isEmpty()&& nums[i]>nums[stack.peek()])
+              {
+                     ans[stack.pop()]=nums[i]; 
+              }
+        }
+        while(stack.size()>0)
+        {
+            ans[stack.pop()]=-1;
+        }
+        return ans;
     }
 }
