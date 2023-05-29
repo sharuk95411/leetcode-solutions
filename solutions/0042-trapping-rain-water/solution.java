1fbb5a7b5f49e426085b9cda35430b00a752876e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-05-29 18:51:57 +0000

    Sync LeetCode submission Runtime - 1 ms (60.10%), Memory - 44.6 MB (100.00%)

--- solutions/0042-trapping-rain-water/solution.java
+++ solutions/0042-trapping-rain-water/solution.java
@@ -1,27 +1,30 @@
 class Solution {
     public int trap(int[] arr) {
+        int n= arr.length;
+        if(n<3) return 0;
+        int ans=0;
+        int leftMax[]= new int[n];
+        int rightMax[]= new int[n];
+          int maxL=arr[0];
+          int maxR= arr[n-1];
+    //    rightMax[]= arr[n-1];
+        for(int i=1;i<n;i++)
+        {
+            leftMax[i]=maxL;
+            maxL= Math.max(maxL,arr[i]);
+            rightMax[n-i-1]= maxR;
+            maxR= Math.max(maxR,arr[n-i-1]);
+        }
 
-      int ans=0;
-      for(int i=1;i<arr.length-1;i++)
-      {
-          int m1= Integer.MIN_VALUE;
-          int left=i-1;
-          while(left>=0)
-          {
-              m1=Math.max(m1,arr[left]);
-              left--;
-          }
-          int m2= Integer.MIN_VALUE;
-          int right=i+1;
-          while(right<arr.length)
-          {
-              m2= Math.max(m2,arr[right]);
-              right++;
-          }
-         int get= Math.min(m1,m2)-arr[i];
-         if(get>0) ans= ans+get;
-      }  
-
-      return ans;
+        for(int i=0;i<n;i++)
+        {
+             int temp= Math.min(leftMax[i],rightMax[i])-arr[i];
+             if(temp>0)
+             {
+                  ans=ans+temp;
+             }
+        }
+        return ans;
+        
     }
 }
