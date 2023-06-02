437924a75ce888d14b1c43d5fd0fb194b12a2660
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-06-02 20:29:07 +0000

    Sync LeetCode submission Runtime - 58 ms (8.37%), Memory - 55.4 MB (14.23%)

--- solutions/0937-online-stock-span/solution.java
+++ solutions/0937-online-stock-span/solution.java
@@ -1,39 +1,35 @@
-// probelm is easy based on Monotonic Decreasing Stack
-// Isme Stack node type me use kia gya h ye intresting h ek hi stack me km ho gya.
-// Logic- codeBix
 class StockSpanner {
-
-    Stack<Node>s= new Stack<>();
-     class Node
+ 
+ 
+class Node
+{
+    int val;
+    int del;
+    
+    public Node(int val,int del)
     {
-        int span;
-        int price;
-        public Node(int span,int price)
-        {
-            this.span=span;
-            this.price= price;
-        }
+        this.val = val;
+        this.del = del;
     }
-    public StockSpanner() {
-     //   s= new Stack<>();
-    }
-    
-    public int next(int price) {
-        int count=0;
-        while(s.size()>0 && price>=s.peek().price)
-        {
-            count= count+s.peek().span;
-            s.pop();
-        }
-    count++;
-        s.push(new Node(count,price));
-        return count;
-    }
-    
+        
 }
 
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner obj = new StockSpanner();
- * int param_1 = obj.next(price);
- */
+Stack<Node>st= new Stack<>();
+
+public StockSpanner() {
+
+}
+
+public int next(int price) {
+    int count = 0;
+    while(!st.isEmpty() && st.peek().val<=price)
+    {
+         count+=st.peek().del;
+         st.pop();
+    }
+    count++;
+    st.push(new Node(price,count));
+    return count;
+}
+
+}
