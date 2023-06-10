9bfbac362738574e0d4aaeb189005eaafc5528fd
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-06-10 20:07:43 +0000

    Sync LeetCode submission Runtime - 56 ms (8.37%), Memory - 55.2 MB (14.31%)

--- solutions/0937-online-stock-span/solution.java
+++ solutions/0937-online-stock-span/solution.java
@@ -1,35 +1,35 @@
 class StockSpanner {
- 
- 
-class Node
+
+class Node 
 {
-    int val;
-    int del;
+    int value,count;
+    Node(int a,int b)
+    {
+        value=a;
+        count=b;
+    }
+}
+Stack<Node>stack= new Stack<>();
+    public StockSpanner() {
+       
+    }
     
-    public Node(int val,int del)
-    {
-        this.val = val;
-        this.del = del;
-    }
+    public int next(int price) {
         
-}
+        int ans=1;
+         while(stack.size()>0 && price>=stack.peek().value)
+         {
+             ans=ans+stack.peek().count;
+             stack.pop();
+         }
+          stack.push(new Node(price,ans));
+          return ans;
 
-Stack<Node>st= new Stack<>();
-
-public StockSpanner() {
-
-}
-
-public int next(int price) {
-    int count = 0;
-    while(!st.isEmpty() && st.peek().val<=price)
-    {
-         count+=st.peek().del;
-         st.pop();
     }
-    count++;
-    st.push(new Node(price,count));
-    return count;
 }
 
-}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
