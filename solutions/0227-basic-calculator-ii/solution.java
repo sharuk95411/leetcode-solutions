42d6e4d8e4c8b8f21112ded1b00c3e82d8707c39
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-01 08:46:27 +0000

    Sync LeetCode submission Runtime - 79 ms (5.09%), Memory - 46.4 MB (89.17%)

--- solutions/0227-basic-calculator-ii/solution.java
+++ solutions/0227-basic-calculator-ii/solution.java
@@ -1,39 +1,57 @@
 class Solution {
     public int calculate(String s) {
-        
-        int ans=0;
-        char opr= '+';
-        int num=0;
-        Stack<Integer>stack= new Stack<>();
+         s = s.replaceAll("\\s", "");
+         System.out.println("LENNGTH IS "+s.length());
+         Stack<Integer>stack= new Stack<>();
+         boolean opr=false;
+         int n1=0;
+         int i=0;
+         while(i<s.length())
+         {
+             n1=0;
+             char c= s.charAt(i);
+             if(c=='-')
+             {
+             opr=true;
+             }
+             else if(Character.isDigit(c))
+             {
+              while(i<s.length()&&Character.isDigit(s.charAt(i)))
+             {
+                n1=n1*10+Character.getNumericValue(s.charAt(i)); 
+                i++; 
+             }
+             if(opr)
+             {
+                 stack.push(-n1);
+                 opr=false;
+             } else 
+             {
+                   stack.push(n1);
+             }
+            i--;
+             }
+            else if(c=='*' || c=='/')
+             {
+             i++;
+             while(i<s.length()&&Character.isDigit(s.charAt(i)))
+             {
+                n1=n1*10+Character.getNumericValue(s.charAt(i)); 
+                i++; 
+             }
+               if(c=='/') stack.push(stack.pop()/n1);
+               else stack.push(stack.pop()*n1);
+               i--;
+             }
+             i++;
 
-        for(char chr : (s + "+").toCharArray())
-        {
-            if(chr==' ') continue;
-            if(Character.isDigit(chr))
-            {
-                num= num*10+chr-'0';
-            }
-            else
-            {
-                    switch(opr){  
-    
-    case '+': stack.push(num) ;
-    break;  
-    case '-': stack.push(-num)  ;
-    break;  
-    case '/':  stack.push(stack.pop()/num); 
-    break;  
-     case '*': stack.push(stack.pop()*num);
-    }  
-    num=0;
-    opr =chr;
-            }
-        }
+         }
+         while(stack.size()>1)
+         {
+             stack.push(stack.pop()+stack.pop());
+         }
+         return stack.peek();
 
-        while(!stack.isEmpty())
-        {
-            ans= ans+stack.pop();
-        }
-        return ans;
+         
     }
 }
