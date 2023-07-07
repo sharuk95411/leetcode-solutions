bb5cd327f48c077c6ad24b252eb06ce5e743cee2
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-07 04:39:36 +0000

    Sync LeetCode submission Runtime - 1 ms (99.48%), Memory - 40.4 MB (99.99%)

--- solutions/1127-last-stone-weight/solution.java
+++ solutions/1127-last-stone-weight/solution.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for(int v : stones){
-            maxHeap.offer(v);
-        }
-        int x;
-        int y;
-        while(maxHeap.size() > 1){
-            y = maxHeap.poll();
-            x = maxHeap.poll();
-            if(y > x){
-                maxHeap.offer(y - x);
-            }
-        }
-        if(maxHeap.size() == 0) return 0;
-        return maxHeap.poll();
+    public int lastStoneWeight(int[] arr) {
+PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+ for(int i=0;i<arr.length;i++)
+ {
+     pq.add(arr[i]);
+ }
+ while(pq.size()>1)
+ {
+     int a = pq.poll();
+     int b= pq.poll();
+     if(a>b)
+     pq.add(a-b);
+     
+
+ }
+ if(pq.isEmpty())return 0;
+ else return pq.peek();
+
     }
 }
