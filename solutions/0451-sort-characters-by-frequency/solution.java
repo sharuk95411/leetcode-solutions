0e469a43f13a08a04d8b40d7cb61bba03734a196
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-12 18:35:15 +0000

    Sync LeetCode submission Runtime - 928 ms (5.20%), Memory - 44.8 MB (99.99%)

--- solutions/0451-sort-characters-by-frequency/solution.java
+++ solutions/0451-sort-characters-by-frequency/solution.java
@@ -1,45 +1,33 @@
 class Solution {
-    public String frequencySort(String S) {
+    public String frequencySort(String s) {
         
- HashMap<Character,Integer>h= new HashMap<>();
-        
-        for(int i=0;i<S.length();i++)
+        HashMap<Character,Integer>h= new HashMap<>();
+        for(int i=0;i<s.length();i++)
         {
-            char c= S.charAt(i);
+            char c= s.charAt(i);
             h.put(c,h.getOrDefault(c,0)+1);
         }
 
-PriorityQueue<Map.Entry<Character,Integer>>pq= new PriorityQueue<>( (a,b) ->b.getValue()-a.getValue());
-for(Map.Entry<Character, Integer> entry: h.entrySet())// h is Object of HashMap
+     PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> h.get(b) - h.get(a));
+
+    for (Map.Entry<Character,Integer> m : h.entrySet())
         {
-            pq.offer(entry);
-            
-        }
-        String ans="";
-        
-        while(pq.size()>0)
-        {
-        
-           String temp= Character.toString(pq.peek().getKey());
-        //    System.out.println(temp);
-            int no= pq.peek().getValue();
-        //    System.out.println(no);
-            
-            while(no>1)
-            {
-                temp=temp+pq.peek().getKey();
-                no--;
-            }
-            //System.out.println(temp);
-            ans= ans+temp;
-            temp="";
-            pq.poll();
-                
-        }
-//System.out.println(pq.peek().getKey());
-//System.out.println(pq.peek().getValue());
-        return ans;
-         
+             pq.add(m.getKey());
+          }
+          
+          String ans="";
+          while(pq.size()>0)
+          {
+              int freq= h.get(pq.peek());
+              Character c= pq.poll();
+              while(freq>0)
+              {
+                  ans=ans+c;
+                  freq--;
+              }
+
+          }
+
+return ans;
     }
-  
 }
