2f8abf317edd3f5ca093070315371eb0f6c94892
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-14 19:24:09 +0000

    Sync LeetCode submission Runtime - 36 ms (21.65%), Memory - 51.9 MB (90.83%)

--- solutions/1014-k-closest-points-to-origin/solution.java
+++ solutions/1014-k-closest-points-to-origin/solution.java
@@ -1,78 +1,57 @@
- /*  Sol 1- TC is NlogN 
-    Arrays.sort(p,(p1,p2)-> p1[0]*p1[0]+p1[1]*p1[1] -p2[0]*p2[0]-p2[1]*p2[1]);
-        
-        int ans[][]= new int[k][2];
-        for(int i=0;i<k;i++)
-        {
-            ans[i][0]= p[i][0];
-            ans[i][1]= p[i][1];
-        }
-        return ans; */
-//======================================================================================
-/*Soln 2- TC is Nlog(k)         
-PriorityQueue<int[]>pq= new PriorityQueue<>((p1,p2)->p2[0]*p2[0]+p2[1]*p2[1] -p1[0]*p1[0]-p1[1]*p1[1]);
-            
-            for(int arr[]: p)
-            {
-                pq.offer(arr);
-                if(pq.size()>k)
-                {
-                    pq.poll();
-                }
-            }
-        
-        int ans[][]= new int[k][2];
-        int i=0;
-        while(i<k)
-        {
-            ans[i]= pq.poll();
-            i++;
-        }
-        return ans;*/
-//==========================================================================================
-
-// question easy h but isme bht kuch seekhne ko mila h 
-
 class Solution {
-    // Ye mmne khud se kia h and isme float type k based pr kse sort krte h PQ me wo seekha h and sol-1 and sol-2 are both are awesomee so un dno ko zruru ache se smjh lena 
-    class pair
+
+    class A
     {
-        int x,y;
-        Double distance;
-        pair(int x,int y, Double distance)
+        int x,y,d;
+        A(int m,int n,int distance)
         {
-            this.x=x;
-            this.y= y;
-            this.distance=distance;
+            x=m;
+            y=n;
+            d=distance;
         }
     }
-    public int[][] kClosest(int[][] points, int k) {
-        
-        int no_of_points= points.length;
-        
-    int ans[][]= new int[k][2];
-        
-PriorityQueue<pair>pq= new PriorityQueue<pair>((p1,p2)->(p1.distance.compareTo(p2.distance)));
-   // yha compare to krne se hi double me sort krege withouut iske PQ integer k base pr sort krta h so ye new chz seekhi h      
-        
-        
-        for(int i=0;i<points.length;i++)
+    public class C implements Comparator<A>
+    {
+        public int compare(A a1, A a2)
         {
-            int x2=points[i][0];
-            int y2= points[i][1];
-           //System.out.println(x2+" "+y2);
-                double distance= Math.sqrt((x2*x2)+(y2*y2));
-           System.out.println(distance);
-            pq.add(new pair(x2,y2,distance));
+            if(a1.d>a2.d) return -1;
+            else if(a1.d<a2.d) return 1;
+            else return 0;
         }
-        
-        for(int i=0;i<k;i++)
-        {
-            pair p= pq.poll();
-            ans[i][0]= p.x;
-            ans[i][1]=p.y;
-        }
-        return ans;
-        
+    }
+
+         public int[][] kClosest(int[][] p, int k) {
+        PriorityQueue<A>pq= new PriorityQueue<>(new C());
+         for(int i=0;i<p.length;i++)
+         {
+             int x= p[i][0];
+             int y= p[i][1];
+            int d= x*x +y*y;
+             if(pq.size()==k)
+             {
+                A a= pq.peek();
+                if(d<a.d) 
+                {
+                    pq.poll();
+                    pq.add(new A(x,y,d));
+                    
+                }
+             }
+             else
+             {
+                 pq.add(new A(x,y,d));
+             }
+         }
+int ans[][]= new int[k][2];
+int i=0;
+while(pq.size()>0)
+{
+        A a =pq.poll();
+        ans[i][0]=a.x;
+        ans[i][1]=a.y;
+        i++;
+}
+return ans;
+
     }
 }
