6aed26e93d9ab6abc90f825f77f2144b0bcfb17f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-22 06:14:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.7 MB (99.99%)

--- solutions/0094-binary-tree-inorder-traversal/solution.java
+++ solutions/0094-binary-tree-inorder-traversal/solution.java
@@ -14,15 +14,19 @@
  * }
  */
 class Solution {
-    List<Integer>l= new ArrayList<>();
+    List<Integer>ans= new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root==null) return l;
-            
-            inorderTraversal(root.left);
-        l.add(root.val);
-        inorderTraversal(root.right);
-    
-        return l;
         
+        A(root);
+        return ans;
     }
+    public void A(TreeNode root)
+{
+    if(root!=null)
+    {
+        A(root.left);
+        ans.add(root.val);
+        A(root.right);
+    }
+}
 }
