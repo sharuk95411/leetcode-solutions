c1a118ca07ab5c11af302c8fa33cc9241b660433
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-30 06:02:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.3 MB (99.99%)

--- solutions/0124-binary-tree-maximum-path-sum/solution.java
+++ solutions/0124-binary-tree-maximum-path-sum/solution.java
@@ -13,23 +13,29 @@
  *     }
  * }
  */
+
+// Take all the path sum and choose between them 
+// Cases - All +ve, All -ve, some +ve some -ve
 class Solution {
+    int maxAll=Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        int ans[]= new int[1];
-        ans[0]= Integer.MIN_VALUE;
-        A(root,ans);
-        return ans[0];
+
+        A(root);
+        return maxAll;        
+
     }
-    
-    private int A(TreeNode root,int ans[])
+
+    public int A(TreeNode root)
     {
         if(root==null) return 0;
+        int left= A(root.left);
+        int right = A(root.right);
         
-        int left_path_sum= Math.max(0,A(root.left,ans));
-        int right_path_sum= Math.max(0,A(root.right,ans));
-        
-        ans[0]= Math.max(ans[0],root.val+left_path_sum+right_path_sum);
-        
-        return root.val+Math.max(left_path_sum,right_path_sum);
+        int lpath= root.val+left;
+        int rpath= root.val +right;
+        int cpath= left+right+root.val;
+        int max= Math.max(Math.max(lpath,rpath),Math.max(root.val,cpath));
+         maxAll= Math.max(maxAll,max);
+        return Math.max(Math.max(lpath,rpath),root.val);
     }
 }
