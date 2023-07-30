fa85e8adee1450d8cf9209af0cb41f2ce79ace3f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-07-30 11:45:50 +0000

    Sync LeetCode submission Runtime - 1 ms (80.17%), Memory - 41 MB (99.99%)

--- solutions/0103-binary-tree-zigzag-level-order-traversal/solution.java
+++ solutions/0103-binary-tree-zigzag-level-order-traversal/solution.java
@@ -4,60 +4,41 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
-      List<List<Integer>>l= new ArrayList<>();
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-       
-        
-        if(root==null) return l ;
-        B(root,1);
-        return (l);
-        
-        
-    }
-     public void B(TreeNode root,int level)
-    {
-        Stack<TreeNode>s1= new Stack<>();
-        Stack<TreeNode>s2= new Stack<>();
-        s1.add(root);
-        while(s1.size()>0)
-        {
-        List<Integer>l1= new ArrayList<>();
-        int size= s1.size();
-        while(size>0)
-        {
-        TreeNode n= s1.pop();
-        l1.add(n.val);
-            if(level%2!=0 ) // odd level 
-            {
-                if(n.left!=null)
-                s2.add(n.left);
-                if(n.right!=null)
-                s2.add(n.right);
-            }
-            else // evem level
-            {
-                if(n.right!=null)
-                s2.add(n.right);
-                if(n.left!=null)
-                s2.add(n.left);
-            }
-            size--;
+        List<List<Integer>> op = new LinkedList<>();
+        if(root==null){
+            return op;
         }
-        l.add(l1);
-        level++;
-        s1= s2;
-        s2= new Stack<>();
-        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean odd=true;
+        while(!queue.isEmpty()) {
+           int size = queue.size();
+           LinkedList<Integer> res = new LinkedList<>();
+           for(int i=1;i<=size;i++)
+           {
+               TreeNode node = queue.poll();
+               if(odd){
+                   res.add(node.val);   
+               } else {
+                   res.addFirst(node.val);
+               }
+               if(node.left!=null) {
+                       queue.add(node.left);
+               }
+            if(node.right!=null){
+                        queue.add(node.right);
+                   }
+               
+           }
+            op.add(res);
+            odd=!odd; 
+
         }
+        return op;
     }
 }
