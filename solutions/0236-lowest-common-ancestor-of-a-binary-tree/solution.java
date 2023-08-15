7bfa31cbc4bfc51bc61e1b42fd5aa7e18690bafe
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-08-15 03:55:02 +0000

    Sync LeetCode submission Runtime - 7 ms (99.54%), Memory - 43.1 MB (100.00%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
@@ -8,57 +8,19 @@
  * }
  */
 class Solution {
-    List<TreeNode>l1= new ArrayList<>();
-    List<TreeNode>l2= new ArrayList<>();
-    boolean b=true;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        A(root,p);
-        b=true;
-        B(root,q);   
-        // System.out.println("L! "+l1);
-        // System.out.println("L2 "+l2);
-        int a=Math.min(l1.size(),l2.size());
         
-        for(int i=0;i<a;i++)
-        {
-             if(l1.get(i).val==l2.get(i).val)
-             {
-                 p=l1.get(i);
-             }
-             else break;
-        }
-        return p;
+        return A(root,p.val,q.val);
     }
+    public TreeNode A(TreeNode root, int a,int b)
+    {
+        if(root==null || root.val==a || root.val==b) return root;
 
-    public void A(TreeNode root, TreeNode p)
-    {
-       if(root==null)return ;
-        l1.add(root);
-        if(root.val==p.val)
-        {
-            b=false;
-        }
-        if(b)
-        A(root.left,p);
-        if(b)
-        A(root.right,p);
-        if(b)
-        l1.remove(l1.size()-1);
+        TreeNode left= A(root.left,a,b);
+        TreeNode right= A(root.right,a,b);
+
+        if(left==null)return right;
+        else if(right==null)return left;
+        else return root;
     }
-     public void B(TreeNode root, TreeNode q)
-    {
-        if(root==null)return ;
-        l2.add(root);
-        if(root.val==q.val)
-        {
-            b=false;
-        }
-        if(b)
-        B(root.left,q);
-        if(b)
-        B(root.right,q);
-        if(b)
-        l2.remove(l2.size()-1);
-        }
-    
 }
