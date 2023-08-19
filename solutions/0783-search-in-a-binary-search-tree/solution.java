32b398f2b5edd99a84869f41480049ebe994375a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-08-19 07:42:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (100.00%)

--- solutions/0783-search-in-a-binary-search-tree/solution.java
+++ solutions/0783-search-in-a-binary-search-tree/solution.java
@@ -15,15 +15,14 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        
-        if(root==null) return root;
+          if(root==null) return root;
         
         if(val==root.val) return root;
         
         if(val<root.val && root.left!=null) return searchBST(root.left,val);
         else if (val>root.val && root.right!=null) 
             
-            return searchBST(root.right,val);
+        return searchBST(root.right,val);
         
         return null;
     }
