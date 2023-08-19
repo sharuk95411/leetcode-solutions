9bf86fa94472d6267fd68e8ea660779a09966407
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-08-19 09:23:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.6 MB (99.92%)

--- solutions/0975-range-sum-of-bst/solution.java
+++ solutions/0975-range-sum-of-bst/solution.java
@@ -14,14 +14,15 @@
  * }
  */
 class Solution {
-    int ans=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
 
-     if(root==null) return 0 ;
-    if(root.val>=low && root.val<=high) ans= ans+root.val;
-     rangeSumBST(root.left,low,high);
-     rangeSumBST(root.right,low,high);
-     return ans;  
+      if (root == null) return 0;
+        int sum = 0;
+        if (root.val >= low && root.val <= high) sum += root.val;
+        if (root.val > low) sum += rangeSumBST(root.left, low, high);
+        if (root.val < high) sum += rangeSumBST(root.right, low, high);
+
+        return sum;  
      
     }
 }
