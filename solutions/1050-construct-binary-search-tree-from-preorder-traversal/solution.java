bbba07be4324ee1f33f62c1aced2bf2c16dd3f4f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-08-24 02:53:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.9 MB (100.00%)

--- solutions/1050-construct-binary-search-tree-from-preorder-traversal/solution.java
+++ solutions/1050-construct-binary-search-tree-from-preorder-traversal/solution.java
@@ -13,21 +13,24 @@
  *     }
  * }
  */
-// Solution by https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/discuss/252232/JavaC%2B%2BPython-O(N)-Solution
-//ye lee215 ka solution h and iske sre solution awesome hte h
-
 class Solution {
-    int i=0;
     public TreeNode bstFromPreorder(int[] preorder) {
-        return bstFromPreorder(preorder, Integer.MAX_VALUE);
+     return helper(preorder, 0, preorder.length - 1);   
     }
     
-    public TreeNode bstFromPreorder(int[] A, int bound) {
-        if (i == A.length || A[i] > bound) return null;
-        TreeNode root = new TreeNode(A[i++]);
-        root.left = bstFromPreorder(A, root.val);
-        root.right = bstFromPreorder(A,bound);
-        return root;
+    private TreeNode helper(int[] preorder, int start, int end) {
+        if(start > end) return null;
+        
+        TreeNode node = new TreeNode(preorder[start]);
+        int i;
+        for(i=start;i<=end;i++) {
+        if(preorder[i] > node.val)
+            break;
+        }
+        
+        node.left = helper(preorder, start+1, i-1);
+        node.right = helper(preorder, i, end);
+        return node;
+        
     }
-
 }
