8ba74d6420f785ac382b5a0bf1a8f76dd80eff6f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-08-27 05:28:45 +0000

    Sync LeetCode submission Runtime - 1 ms (80.17%), Memory - 41.4 MB (99.99%)

--- solutions/0103-binary-tree-zigzag-level-order-traversal/solution.java
+++ solutions/0103-binary-tree-zigzag-level-order-traversal/solution.java
@@ -15,31 +15,39 @@
  */
 class Solution {
 
+    
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode>q= new LinkedList<>();
-        List<List<Integer>>outer= new ArrayList<>();
-        List<Integer>inner=new ArrayList<>();
-        if(root==null)return outer;
-        int level=0;
-        
+
+List<List<Integer>>outer= new ArrayList<>();
+    Queue<TreeNode>q= new LinkedList<>();
+     if(root==null)return outer;
         q.add(root);
+        int i=0;
         while(q.size()>0)
         {
             int size=q.size();
-            inner=new ArrayList<>();
+            List<Integer>inner= new ArrayList<>();
             while(size>0)
             {
-                TreeNode n= q.poll();
-                if(level%2==0) inner.add(n.val);
-                else inner.add(0,n.val);
-                 if(n.left!=null)  q.add(n.left);
-                  if(n.right!=null)q.add(n.right);
+                TreeNode n=q.poll();
+                
+                if(i%2==0)
+                {
+                    inner.add(n.val);
+                    if(n.left!=null)q.add(n.left);
+                    if(n.right!=null)q.add(n.right);
+                }
+                else
+                {
+                         inner.add(0,n.val);
+                         if(n.left!=null)q.add(n.left);
+                         if(n.right!=null)q.add(n.right);
+                }
                 size--;
             }
             outer.add(new ArrayList<>(inner));
-            level++;
+            i++;
         }
-        return outer;
-
+     return outer;   
     }
 }
