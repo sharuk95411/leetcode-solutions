3a923ee262aabd272cc72e50883de4dcbbb2f1da
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-09-10 09:06:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.7 MB (100.00%)

--- solutions/0198-house-robber/solution.java
+++ solutions/0198-house-robber/solution.java
@@ -1,20 +1,20 @@
 class Solution {
-    Integer t[];
-    public int rob(int[] arr) {
-        t= new Integer [arr.length];
-        return A(arr,0);
-        
+    public int rob(int []arr) {
+    
+         int dp[]= new int[arr.length+1];
+         Arrays.fill(dp,-1);
+         return A(0,arr,dp);
     }
-    public int A(int arr[], int index)
+
+    public int A(int index, int arr[],int dp[])
     {
-        if(index>=arr.length)
-            return 0;
-        
-        if(t[index]!=null) return t[index];
-        
-        int stealCurrent= arr[index]+ A(arr,index+2);
-        int skipCurrent= A(arr,index+1);
-        
-        return t[index] =Math.max(stealCurrent,skipCurrent);
+        if(index>=arr.length)return 0;
+        if(dp[index]!=-1)return dp[index];
+        int sum1= 0;
+        sum1= arr[index]+A(index+2,arr,dp);
+        int sum2=A(index+1,arr,dp);
+        dp[index] =Math.max(sum1,sum2);
+        return dp[index];
+
     }
 }
