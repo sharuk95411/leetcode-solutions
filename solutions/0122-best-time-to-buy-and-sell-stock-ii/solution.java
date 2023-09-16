c01d89c5cfac3b19804b97b40022a9878853ae0a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-09-16 12:14:43 +0000

    Sync LeetCode submission Runtime - 3 ms (11.27%), Memory - 47 MB (10.75%)

--- solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,18 +1,33 @@
 class Solution {
-    // hint- iski simple problem dekh lo whi se idea mil jyga
-    // code- Anuj Bhaiya
-    
     public int maxProfit(int[] arr) {
         
-        if(arr.length==0) return 0;
-        int profit=0;
-        
-        for(int i=1;i<arr.length;i++)
-        {
-            if(arr[i]>arr[i-1]) profit+= arr[i]-arr[i-1];
-        }
-        
-        return profit;
-        
+        int dp[][]=new int[arr.length+1][2];
+         for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return A(0,0,arr,dp);
     }
+    
+    public int A(int i,int check,int arr[],int dp[][])
+    {
+         if(i>=arr.length)return 0;
+           if(dp[i][check]!=-1)return dp[i][check];
+        
+        int profit=0;
+        if(check==0)
+        {
+            int buy= A(i+1,1,arr,dp)-arr[i];
+            int nbuy= A(i+1,0,arr,dp);
+            
+            profit= Math.max(buy,nbuy);
+            
+        }
+        else
+        {
+            int sell= arr[i]+A(i+1,0,arr,dp);
+            int nsell= A(i+1,1,arr,dp);
+            profit= Math.max(sell,nsell);
+        }
+        return dp[i][check]=profit;
+
+}
 }
