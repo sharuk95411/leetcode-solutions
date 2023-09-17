7b02c6d4663c38ebca0c3a54a59f185c79a9d89b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-09-17 03:38:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.1 MB (100.00%)

--- solutions/0213-house-robber-ii/solution.java
+++ solutions/0213-house-robber-ii/solution.java
@@ -1,38 +1,27 @@
 class Solution {
     
-    Integer dp[];
-    public int rob(int[] arr) {
-        
-        if(arr.length<=1) return arr[0];
-        dp= new Integer[arr.length];
-        
-    
-        int a= A(arr,0,true);
-        Arrays.fill(dp,null);
-        int b= A(arr,1,false);
-        
-        return Math.max(a,b);
+    public int rob(int[] nums) {
+        int dp[]= new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        int start_with_zero= A(0,nums,nums.length-1,dp);
+        Arrays.fill(dp,-1);
+        int start_with_one= A(1,nums,nums.length,dp);
+        if(nums.length==1)return nums[0];
+        return Math.max(start_with_zero,start_with_one);
     }
     
-    private int A (int arr[],int index,boolean b)
+    public int A(int i,int arr[],int n,int dp[])
+        
     {
-        if(b==true)
-        {
-             if(index>=arr.length-1) return 0;
-        }
-        if(b==false)
-        {
-            if(index>=arr.length) return 0;
-        }
-       
-        if(dp[index]!=null) return dp[index];
-         
-        int even= arr[index]+A(arr,index+2,b);
-       
+        if(i>=n)return 0;
+        if(dp[i]!=-1) return dp[i];
         
-        int odd= A(arr,index+1,b);
+        int ans=0;
         
-        return dp[index]= Math.max(even,odd);
+        int pick= arr[i]+A(i+2,arr,n,dp);
+        int notpick= A(i+1,arr,n,dp);
         
+        ans= Math.max(pick,notpick);
+        return dp[i]=ans;
     }
 }
