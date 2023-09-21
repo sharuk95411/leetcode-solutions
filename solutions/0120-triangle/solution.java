6441b28082f9c9013309187b5e4217f2f469acae
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-09-21 04:11:40 +0000

    Sync LeetCode submission Runtime - 1 ms (99.80%), Memory - 43.6 MB (99.99%)

--- solutions/0120-triangle/solution.java
+++ solutions/0120-triangle/solution.java
@@ -1,22 +1,23 @@
 class Solution {
     
-    // Logic BY Naresh gupta and isme Each Case me TC b btai h to dekh lena ek bar TC and its very easy and TYN
-    Integer dp[][];
-    
-    public int minimumTotal(List<List<Integer>> list) {
+    public int minimumTotal(List<List<Integer>> outer) {
         
-       dp = new Integer[list.size()][list.size()];
-        return A(list,0,0);
-        
+        int n=outer.size();
+        Integer dp[][]=new Integer[n][n];
+        return A(outer,0,0,dp);
     }
-    private int A(List<List<Integer>>list,int r,int c)
+    
+    public int A(List<List<Integer>>outer,int i,int j,Integer dp[][])
     {
-        if(r>=list.size()) 
+        if(i==outer.size())
         {
             return 0;
         }
-        if(dp[r][c]!=null) return dp[r][c];
-        return dp[r][c]= Math.min(A(list,r+1,c),A(list,r+1,c+1))+list.get(r).get(c);
+        if(dp[i][j]!=null)return dp[i][j];
         
+        int a= outer.get(i).get(j)+A(outer,i+1,j,dp);
+        int b= outer.get(i).get(j)+A(outer,i+1,j+1,dp);
+        
+        return dp[i][j]=Math.min(a,b);
     }
 }
