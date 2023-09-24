9103d8f5bbd26614a60ee341a355fc64144c59c9
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-09-24 05:26:09 +0000

    Sync LeetCode submission Runtime - 1 ms (99.96%), Memory - 60.3 MB (99.93%)

--- solutions/0053-maximum-subarray/solution.java
+++ solutions/0053-maximum-subarray/solution.java
@@ -1,21 +1,17 @@
 class Solution {
 
-    int ans=Integer.MIN_VALUE;
-    public int maxSubArray(int[] arr) {
-        int sum=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            sum= sum+arr[i];
-            if(sum>ans)
-            {
-                ans=sum;
-            }
-            if(arr[i]>sum)
-            {
-                ans= Math.max(arr[i],ans);
-                sum= Math.max(sum,arr[i]);
-            }
-        }
-        return ans;
-    }
+      public int maxSubArray(int[] nums) {
+       int sum = 0;
+       int maxSum = nums[0];
+       for(int  i =0;i<nums.length;i++){
+           
+           sum=sum+nums[i];
+           maxSum =  Math.max(maxSum,sum);
+           if(sum<0)
+           {
+               sum = 0;
+           }
+       }
+           return maxSum;
+   }
 }
