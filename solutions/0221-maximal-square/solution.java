e15ba1d815bf129006ee5d7ac8d8af1689e19d8b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-10-08 15:19:37 +0000

    Sync LeetCode submission Runtime - 9 ms (34.65%), Memory - 53.7 MB (100.00%)

--- solutions/0221-maximal-square/solution.java
+++ solutions/0221-maximal-square/solution.java
@@ -1,34 +1,40 @@
 class Solution {
-    Integer b[][];
+    int dp[][] = null;
     public int maximalSquare(char[][] matrix) {
-        
-        if(matrix.length==0) return 0;
-        int ans=0;
-        
-        b= new Integer[matrix.length][matrix[0].length];
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<matrix[0].length;j++)
-            {
-                if(matrix[i][j]=='1')
-                {
-                    int temp= call_the_neg(matrix,i,j);
-                    ans= Math.max(temp,ans);
-                }
-            }
-        }
-        return ans*ans;
-        
-    }
-    
-    private int call_the_neg(char arr[][],int i,int j)
-    {
-        if(i>=arr.length || j>=arr[0].length || arr[i][j]=='0')
-        {
-            return 0;
-        }
-        if(b[i][j]!=null) return b[i][j];
+		if (matrix.length == 0) {
+			return 0;
+		}
+		dp = new int[matrix.length][matrix[0].length];
+		for (int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		int h = matrix.length;
+		int l = matrix[0].length;
+		int max = 0;
 
-        return b[i][j]= 1+Math.min(call_the_neg(arr,i+1,j+1),Math.min(call_the_neg(arr,i+1,j),call_the_neg(arr,i,j+1)));
-    }
+		int curMax = 0;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < l; j++) {
+				curMax = findSquare(matrix, i, j);
+				if (max < curMax) {
+					max = curMax;
+				}
+			}
+		}
+		return max * max;
+	}
+
+	private int findSquare(char[][] matrix, int i, int j) {
+		if (i >= matrix.length || j >= matrix[0].length) {
+			return 0;
+		}
+		if (matrix[i][j] == '0') {
+			return dp[i][j] = 0;
+		}
+		if (dp[i][j] != -1) {
+			return dp[i][j];
+		}
+		int sum = 1 + Math.min(findSquare(matrix, i + 1, j + 1), Math.min(findSquare(matrix, i + 1, j), findSquare(matrix, i, j + 1)));
+		return dp[i][j] = sum;
+	}
 }
