9d8867b825a9f3302aa8fbc0677e3a1d566f54b6
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-10-08 07:31:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.3 MB (100.00%)

--- solutions/0063-unique-paths-ii/solution.java
+++ solutions/0063-unique-paths-ii/solution.java
@@ -1,16 +1,29 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int width = obstacleGrid[0].length;
-    int[] dp = new int[width];
-    dp[0] = 1;
-    for (int[] row : obstacleGrid) {
-        for (int j = 0; j < width; j++) {
-            if (row[j] == 1)
-                dp[j] = 0;
-            else if (j > 0)
-                dp[j] += dp[j - 1];
+
+    public int uniquePathsWithObstacles(int[][]arr) {
+        
+        int dp[][]=new int [arr.length+1][arr[0].length+1];
+         for (int[] row : dp)
+        {
+          Arrays.fill(row, -1);
         }
+        return A(0,0,arr,dp);
+        
     }
-    return dp[width - 1];
+    public int A(int i,int j,int arr[][],int dp[][])
+    {
+         if(i>=arr.length || j>=arr[0].length) return 0;
+        if(arr[i][j]==1)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(i==arr.length-1 && j==arr[0].length-1)
+        {
+            
+            return 1;
+        }
+        
+        
+       int way1=A(i,j+1,arr,dp);
+       int way2= A(i+1,j,arr,dp);
+        return dp[i][j]= way1+way2;
     }
 }
