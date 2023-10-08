db3d3361a10f797d02929d50efb3caa41b6e809e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-10-08 07:01:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.6 MB (100.00%)

--- solutions/0062-unique-paths/solution.java
+++ solutions/0062-unique-paths/solution.java
@@ -1,36 +1,27 @@
 class Solution {
     int ans=0;
-    Integer b[][];
     public int uniquePaths(int m, int n) {
-        int arr[][]= new int[m][n];
         
-        b=new Integer[m][n];
-        int row= m;
-        int col=n;
-        
-        
-        
-        return A(0,0,m,n,arr);
+int dp[][]=new int [m+1][n+1];
+ for (int[] row : dp)
+{
+  Arrays.fill(row, -1);
+}
+        return A(0,0,m,n,dp);
         
     }
-    
-    private int A(int i,int j,int row,int col,int arr[][])
+    public int A(int i,int j,int row,int col,int dp[][])
     {
+         if(i>=row || j>=col) return 0;
         if(i==row-1 && j==col-1)
         {
+            
             return 1;
-            
         }
+        if(dp[i][j]!=-1)return dp[i][j];
         
-        if(i>=row ||j>=col) return 0;
-        if(b[i][j]!=null) return b[i][j];
-    int way1=0;
-        way1= A(i,j+1,row,col,arr);
-        b[i][j]=way1;
-        int way2=0;
-        way2=A(i+1,j,row,col,arr);
-        b[i][j]= way2;
-        return b[i][j]= way1+way2;
-            
+       int way1=A(i,j+1,row,col,dp);
+       int way2= A(i+1,j,row,col,dp);
+        return dp[i][j]=way1+way2;
     }
 }
