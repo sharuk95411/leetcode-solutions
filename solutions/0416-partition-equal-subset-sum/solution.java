ba8ad719c08ecd2fe00cb2aa9bf2dd979524e6d4
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-10-13 12:23:03 +0000

    Sync LeetCode submission Runtime - 87 ms (25.98%), Memory - 57.4 MB (38.99%)

--- solutions/0416-partition-equal-subset-sum/solution.java
+++ solutions/0416-partition-equal-subset-sum/solution.java
@@ -1,21 +1,32 @@
-class Solution {
-    public boolean canPartition(int[] nums) {
+class Solution 
+{
+    public boolean canPartition(int[] arr) {
+        
         int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+            for(int i=0;i<arr.length;i++)
+        {
+            sum= sum+arr[i];
         }
-        if(sum%2==1)
-            return false;
-        Boolean dp[][]=new Boolean[nums.length+1][(sum/2)+1];
-        return partition(nums,sum/2,0,dp);
+        if(sum%2!=0)return false;
+        int dp[][]=new int[arr.length+1][sum/2+1];
+             for (int[] row : dp)
+            {
+              Arrays.fill(row, -1);
+            }
+        if(A(arr,0,sum/2,dp)>0)return true;
+        return false;
+        
     }
-     public boolean partition(int a[],int sum,int i,Boolean dp[][]){
-        if(sum==0)
-            return true;
-		if(i>=a.length || sum<0)
-            return false;
-        if(dp[i][sum]!=null)
-            return dp[i][sum];
-        return dp[i][sum]=(partition(a,sum-a[i],i+1,dp) || partition(a,sum,i+1,dp));
+    
+    public int A(int arr[],int i, int sum,int dp[][])
+    {
+        if(i>=arr.length)return 0;
+        
+        if(sum==0)return 1;
+        if(sum<0)return 0;
+        if(dp[i][sum]!=-1)return dp[i][sum];
+        dp[i][sum]= A(arr,i+1,sum-arr[i],dp)+A(arr,i+1,sum,dp);
+        if(dp[i][sum]>0)return 1;
+        return dp[i][sum];
     }
 }
