26a80b0dee8ea5e5061a9e5f328a683644c3b2b7
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-10-15 08:35:25 +0000

    Sync LeetCode submission Runtime - 1 ms (27.38%), Memory - 41.3 MB (100.00%)

--- solutions/0678-valid-parenthesis-string/solution.java
+++ solutions/0678-valid-parenthesis-string/solution.java
@@ -1,51 +1,41 @@
-// Use two Integer type stack.
-// Logic-codebix but TC is not good when its under O(n)
-// https://leetcode.com/problems/valid-parenthesis-string/discuss/107566/Java-12-lines-solution-backtracking isne recusrion se kia h jo ki approach achi h but TL exceed a ri h 
-/* This is Optimal Solution Hint- One Stack used and its Char Type
-Stack<Character> stk = new Stack<>();
-        int scount = 0;
-        for(char c : s.toCharArray()) {
-            if(c == '(') stk.push(c);
-            else {
-                if(c == ')' && !stk.isEmpty()) stk.pop();
-                else if(c == ')' && scount > 0) scount--;
-                else if(c  == ')') return false;
-                else {
-                    scount++;
-                    if(!stk.isEmpty()) {
-                        stk.pop();
-                        scount++;
-                    }
-                }
-            }
-        }
-        return stk.isEmpty();
-*/
 class Solution {
+    Boolean dp[][];
     public boolean checkValidString(String s) {
-     if(s.length()==0) return true;
-        Stack<Integer>s1= new Stack<>();
-       Stack<Integer>s2= new Stack<>();
-       char c[]= s.toCharArray();
-       for(int i=0;i<c.length;i++)
-       {
-          if(c[i]=='(') s1.push(i);
-          else if(c[i]==')')
-          {
-              if(s1.size()>0) s1.pop();
-              else if(s2.size()>0) s2.pop();
-              else return false;
-          }
-          else
-          s2.push(i);
-       }
-       while(s1.size()>0 && s2.size()>0)
-       {
-           Integer a= s2.pop();
-           if(a>s1.peek()) s1.pop();
-           
-       }
-       if(s1.size()>0) return false;
-       else return true;
+         dp=new Boolean[s.length()+1][s.length()+1];
+        return A(0,0,0,s);
+    }
+    public boolean A(int open,int close,int i,String s)
+        
+    {
+        if(open <0 || i>=s.length())
+        {
+            if(open!=0)return false;
+            else return true;
+        }
+       
+        boolean a,b,d;
+        a=false;
+        b=false;
+        d=false;
+        
+        char c=s.charAt(i);
+        if(dp[open][i]!=null)return dp[open][i];
+        
+        if(c=='(')
+         a= A(open+1,close,i+1,s);
+        
+        else if(c==')')
+        {
+            if(open<=0) return false;
+            b= A(open-1,close,i+1,s);
+        }
+        else
+        {
+            a=A(open+1,close,i+1,s);  // Traet as open bracket
+            b= A(open-1,close,i+1,s); // Treat as close braket
+            d=A(open,close,i+1,s);    // Treat as none
+        }
+        
+        return dp[open][i] =(a|| (b||d)); 
     }
 }
