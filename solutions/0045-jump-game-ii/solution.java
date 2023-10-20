17a70ff51879b7d9bf79acc67b1b818974e08365
sharuk <78816538+sharuk95411@users.noreply.github.com>
2023-10-20 04:20:20 +0000

    Sync LeetCode submission Runtime - 111 ms (8.66%), Memory - 44.9 MB (99.95%)

--- solutions/0045-jump-game-ii/solution.java
+++ solutions/0045-jump-game-ii/solution.java
@@ -1,18 +1,23 @@
 class Solution {
+    int dp[];
     public int jump(int[] arr) {
-     
-    int curr_reach=0,max_jump=0,jump=0;
-        for(int i=0;i<arr.length-1;i++)
+        if(arr.length==1)return 0;
+        
+        dp=new int[arr.length+1];
+        return A(0,arr);
+    }
+    
+    public int A(int i ,int arr[])
+    {
+        if(i>=arr.length-1)return 0;
+        if(arr[i]==0)return arr.length;
+        if(dp[i]!=0)return dp[i];
+        int ans=arr.length;        
+        for(int j=1;j<=arr[i];j++)
         {
-            
-            max_jump= Math.max(max_jump,i+arr[i]);
-            if(i==curr_reach)
-            {
-                jump++;
-                curr_reach= max_jump;
-                
-            }
+          int temp_ans= 1+A(i+j,arr);
+          ans= Math.min(temp_ans,ans);
         }
-        return jump;
+        return dp[i]= ans;
     }
 }
