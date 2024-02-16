300b7d101411754ea4a3f7c89738ac8a3ec5bc45
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-02-16 02:14:26 +0000

    Sync LeetCode submission Runtime - 2 ms (10.64%), Memory - 41.8 MB (99.99%)

--- solutions/0118-pascals-triangle/solution.java
+++ solutions/0118-pascals-triangle/solution.java
@@ -1,34 +1,27 @@
 class Solution {
-    public List<List<Integer>> generate(int num) {
         List<Integer>inner= new ArrayList<>();
         List<List<Integer>>outer= new ArrayList<>();
-        
-                inner.add(1);
-                outer.add(inner);
-        for(int i=1;i<num;i++)
-        {
-            inner= new ArrayList<>();
-            inner.add(1);
-         //   System.out.println(outer);
-         
-            for(int j=0;j<outer.get(i-1).size();j++)
-            {
-                if(j==outer.get(i-1).size()-1)
-                {
-                    inner.add(1);
-                    outer.add(inner);
-                    // System.out.println("OK");
-                    break;
-                } 
-                else
-                {
-                    // int no= outer.get(i-1).get(j);
-                    int no= outer.get(i-1).get(j)+outer.get(i-1).get(j+1);
-                    inner.add(no);
-                }
-            }
-        }
+    public List<List<Integer>> generate(int num) {
+        inner.add(1);
+        outer.add(new ArrayList<>(inner));
+        A(num-1);
         return outer;
-        
+    }
+    
+    public void A(int num)
+    {
+       if(num==0)return ;
+         inner.removeAll(inner); 
+        inner.add(1);
+        List<Integer>l= new ArrayList<>();
+        l=outer.get(outer.size()-1);
+        for(int i=1;i<l.size();i=i+1)
+        {
+            int sum= l.get(i-1)+l.get(i); 
+            inner.add(sum);
+        }
+        inner.add(1);
+        outer.add(new ArrayList<>(inner));
+        A(num-1);
     }
 }
