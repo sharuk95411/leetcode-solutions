1048fc080158d603d879d4ac805f5768e540bba0
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-02-16 14:52:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (99.94%)

--- solutions/0048-rotate-image/solution.java
+++ solutions/0048-rotate-image/solution.java
@@ -1,29 +1,36 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-    int start=0;
-    int end = matrix.length-1;
-    while(start<=end)
-    {
-        int temp[]= new int[matrix.length];
-        temp=matrix[start];
-        matrix[start]= matrix[end];
-        matrix[end]= temp;
-        start++;
-        end--;
-    }
-
-      for(int i=0;i<matrix[0].length;i++)
-      {
-          for(int j=0;j<matrix[0].length;j++)
-          {
-              if(i!=j && j>i)
-              {
-                  int temp= matrix[i][j];
-                  matrix[i][j]= matrix[j][i];
-                  matrix[j][i]= temp;
-              }
-          }
-      }
-
+    public void rotate(int[][] arr) {
+        
+        int i=0;
+        int j=0;
+        int n= arr[0].length;
+        
+        for(int a=0;a<n-1;a++)
+        {
+            int k=j+1;
+            while(k<n)
+            {
+                int temp=arr[i][k];
+                arr[i][k]=arr[k][i];
+                arr[k][i]=temp;
+                k++;
+            }
+            i++;
+            j++;
+        }
+        int l=0;
+        int r=n-1;
+        while(l<r)
+        {
+            for(i=0;i<n;i++)
+            {
+                int temp= arr[i][l];
+                arr[i][l]= arr[i][r];
+                arr[i][r]=temp;
+            }
+            l++;
+            r--;
+        }
+        
     }
 }
