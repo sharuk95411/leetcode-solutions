bb6fa59faf6d4d04a3b0992a8f796ca9dd305bd6
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-02-28 03:18:04 +0000

    Sync LeetCode submission Runtime - 9 ms (35.76%), Memory - 46.6 MB (99.99%)

--- solutions/0056-merge-intervals/solution.java
+++ solutions/0056-merge-intervals/solution.java
@@ -1,56 +1,44 @@
-class Pair {
-
-    int row,col;
-    Pair(int r,int c)
-    {
-        row=r;
-        col=c;
-    }
-}
-
 class Solution {
+    
+    class Pair{
+        int i,j;
+            Pair(int a,int b)
+        {
+            i=a;
+            j=b;
+        }
+    }
+    Queue<Pair>q= new LinkedList<>();
     public int[][] merge(int[][] arr) {
-Arrays.sort(arr, (a, b) -> Integer.compare(a[0],b[0])); //increasing orde
-
-for(int i=0;i<arr.length;i++)
-{
-    System.out.println("COL IS "+arr[i][0]+" AND "+arr[i][1]);
-}
-Stack<Pair>s = new Stack<>();
-
-   s.push(new Pair(arr[0][0],arr[0][1]));
-   int start= arr[0][1];
-
-   for(int i=1;i<arr.length;i++)
-   {
-       if(start>=arr[i][0])
-       {
-           Pair p = s.pop();
-           s.push(new Pair(p.row,Math.max(arr[i][1],p.col)));
-           start= Math.max(p.col,arr[i][1]);
-       }
-       else
-       {
-           s.push(new Pair(arr[i][0],arr[i][1]));
-           start= arr[i][1];
-       }
-   }
-
-   int ans[][]= new int[s.size()][2];
-   int i= ans.length-1;
-   while(i>=0)
-   {
-       Pair p= s.pop();
-       ans[i][0]= p.row;
-       ans[i][1]=p.col;
-       i--;
-   }
-   return ans;
-
-
- 
-      
-
-
+        
+        Arrays.sort(arr, (a, b) ->  Integer.compare(a[0],b[0]));
+        
+        int a= arr[0][0];
+        int b= arr[0][1];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(b>=arr[i][0])
+            {
+                   b=Math.max(b,arr[i][1]);
+            }
+            else
+            {
+                 q.add(new Pair(a,b));
+                a=arr[i][0];
+                b=arr[i][1];
+            }
+        }
+        
+        q.add(new Pair(a,b));
+        int ans[][]=new int[q.size()][2];
+        
+        for(int i=0;i<ans.length;i++)
+        {
+            Pair p=q.poll();
+            ans[i][0]=p.i;
+            ans[i][1]=p.j;
+        }
+        
+            return ans;
     }
 }
