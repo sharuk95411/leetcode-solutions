79c92e3aad21c995e07b9dd050c94940aaa3b52b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-06-16 15:17:32 +0000

    Sync LeetCode submission Runtime - 27 ms (3.03%), Memory - 57.1 MB (99.93%)

--- solutions/0238-product-of-array-except-self/solution.java
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -1,24 +1,33 @@
 class Solution {
     public int[] productExceptSelf(int[] arr) {
         
-        int left[]= new int[arr.length];
-
-         left[0]=1;
-        int product=1;
-        for(int i=1;i<arr.length;i++)
-        {
-         product= product*arr[i-1];
-            left[i]= product;
-        }
-
-        int temp=1;
-        for(int i=arr.length-2;i>=0;i--)
-        {
-            temp=temp*arr[i+1];
-            left[i]= left[i]*temp;
-        }
-      
-       return left;
+        int ans[]= new int[arr.length];
+        int n= arr.length;
         
+        int multiplyFromLeft[]= new int[n];
+        int multiplyFromRight[]= new int[n];
+        int prev=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            multiplyFromLeft[i]= arr[i-1]*prev;
+            prev= multiplyFromLeft[i];
+        }
+        prev=1;
+         System.out.println(Arrays.toString(multiplyFromLeft));
+        for(int i=n-2;i>=0;i--)
+        {
+            multiplyFromRight[i]= prev*arr[i+1];
+            prev= multiplyFromRight[i];
+        }
+         System.out.println(Arrays.toString(multiplyFromRight));
+        ans[0]= multiplyFromRight[0];
+        ans[n-1]= multiplyFromLeft[n-1];
+        for(int i=1;i<n-1;i++)
+        {
+            ans[i]= multiplyFromLeft[i]*multiplyFromRight[i];
+        }
+        
+        return ans;
     }
 }
