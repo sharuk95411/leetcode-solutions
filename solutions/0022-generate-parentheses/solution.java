a7b9494dd6ea9e482f3c68759bb8eea774b6151d
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-05 03:02:32 +0000

    Sync LeetCode submission Runtime - 2 ms (71.45%), Memory - 43.2 MB (99.97%)

--- solutions/0022-generate-parentheses/solution.java
+++ solutions/0022-generate-parentheses/solution.java
@@ -1,29 +1,55 @@
 class Solution {
-    List<String>ans=new ArrayList<>();
+    List<String>ans= new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        StringBuilder str= new StringBuilder();
-         A(n,0,0,str);
-         return ans;
+        
+    fun(0,0,"",n);
+    return ans;
     }
-
-    public void A(int n,int open,int close,StringBuilder str)
+    public void fun(int open,int close,String temp,int n)
     {
-        if(str.length()==2*n)
+        if(temp.length()==2*n)
         {
-            ans.add(str.toString());
-            return ;
+            ans.add(temp);
+            return;
         }
         if(open<n)
         {
-            str.append("(");
-            A(n,open+1,close,str);
-            str.delete(str.length()-1,str.length());
+            fun(open+1,close,temp+"(",n);
         }
         if(close<open)
         {
-            str.append(")");
-            A(n,open,close+1,str);
-              str.delete(str.length()-1,str.length());
+            fun(open,close+1,temp+")",n);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
