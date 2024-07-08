7b670cebd0ba1518ad1c1a8ad27ed75a9c9d20c3
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-08 03:11:22 +0000

    Sync LeetCode submission Runtime - 5 ms (97.55%), Memory - 42.1 MB (99.98%)

--- solutions/0567-permutation-in-string/solution.java
+++ solutions/0567-permutation-in-string/solution.java
@@ -1,32 +1,40 @@
 class Solution {
-    public boolean checkInclusion(String p, String s) {
- int freq1[] = new int[26];
-   int freq2[] = new int[26];
-    if(s.length()<p.length())
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if(s1.length()>s2.length())return false;
+
+
+        int freq1[]=new int[26];
+
+        for(int i=0;i<s1.length();i++)
+        {
+            freq1[s1.charAt(i)-'a']++;
+        }
+
+        int freq2[]= new int[26];
+        for(int i=0;i<s1.length();i++)
+        {
+            freq2[s2.charAt(i)-'a']++;
+        }
+
+        if(Arrays.equals(freq1,freq2))return true;
+
+        int i=0;
+        int j=s1.length();
+
+        while(j<s2.length())
+        {
+            freq2[s2.charAt(i)-'a']--;
+            freq2[s2.charAt(j)-'a']++;
+             if(Arrays.equals(freq1,freq2))return true;
+             i++;
+             j++;
+
+        }
+
         return false;
-    
-    for(int i=0; i<p.length(); i++){
-        freq1[s.charAt(i)-'a']++;
-        freq2[p.charAt(i)-'a']++;
-    }
- 
-    int start=0;
-    int end=p.length();
-    
-    if(Arrays.equals(freq1,freq2))
-        return true;
-    
-    while(end<s.length()){
+
         
-        freq1[s.charAt(start)-'a']--;
-        freq1[s.charAt(end)-'a']++;
         
-        if(Arrays.equals(freq1,freq2))
-        return true;
-        
-        start++;
-        end++;
-    }
-  return false;
     }
 }
