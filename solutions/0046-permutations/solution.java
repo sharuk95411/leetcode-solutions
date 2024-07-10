d8cfbc387f70c48ec06349a1a366bea9d02f5b88
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-10 09:28:35 +0000

    Sync LeetCode submission Runtime - 1 ms (87.38%), Memory - 44.7 MB (99.82%)

--- solutions/0046-permutations/solution.java
+++ solutions/0046-permutations/solution.java
@@ -1,33 +1,33 @@
 class Solution {
-    boolean visited[];
+
     List<List<Integer>>outer= new ArrayList<>();
-    List<Integer>inner= new ArrayList<>();
-    public List<List<Integer>> permute(int[] nums) {
-     visited= new boolean[nums.length];
-     A(nums);
-return outer ;
+    List<Integer>inner=new ArrayList<>();
+        
+    public List<List<Integer>> permute(int[] arr) {
+         boolean b[]=new boolean[arr.length];
+        A(arr,b);
+        return outer;
     }
 
-    public void A(int arr[])
+      public  void A(int arr[],boolean b[])
     {
         if(inner.size()==arr.length)
         {
-            outer.add(new ArrayList<>(inner));
-            return;
-        }
         
-        for(int i=0;i<arr.length;i++)
-        {
-         if(visited[i]==false)
-        {
-            inner.add(arr[i]);
-            visited[i]=true;
-            A(arr);
-            inner.remove(inner.size()-1);
-            visited[i]=false;
-            
+           outer.add(new ArrayList<>(inner));
+            return ;
         }
+
+        for(int j=0;j<arr.length;j++)
+        {
+            if(b[j]==false)
+            {
+                b[j]=true;
+                inner.add(arr[j]);
+                A(arr,b);
+                inner.remove(inner.size()-1);
+                b[j]=false;
+            }
         }
-      
     }
 }
