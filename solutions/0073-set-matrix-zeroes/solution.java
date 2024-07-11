a3e01ffaa97af0fdf063d2d78c7f0e019e70f06a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-11 03:21:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.7 MB (99.98%)

--- solutions/0073-set-matrix-zeroes/solution.java
+++ solutions/0073-set-matrix-zeroes/solution.java
@@ -1,68 +1,72 @@
 class Solution {
     public void setZeroes(int[][] arr) {
         
-         boolean row= false;
-         for(int j=0;j<arr[0].length;j++)
-         {
-             if(arr[0][j]==0)
-             {
-                 row=true;
-                 break;
-             }
-         }
-         boolean col= false;
-         for(int i=0;i<arr.length;i++)
-         {
-             if(arr[i][0]==0)
-             {
-                 col=true;
-                 break;
-             }
-         }
+        boolean firstRow=false;
+        boolean firstCol= false;
+        for(int j=0;j<arr[0].length;j++)
+        {
+            if(arr[0][j]==0)
+            {
+                firstRow=true;
+                break;
+            }
 
-         for(int i=1;i<arr.length;i++)
-         {
-             for(int j=arr[0].length-1;j>=1;j--)
-             {
-                 if(arr[i][j]==0)
-                 {
-                     arr[0][j]=0;
-                     arr[i][0]=0;
-                 }
-             }
-         }
+        }
 
-       for(int i=1;i<arr.length;i++)
-         {
-             for(int j=arr[0].length-1;j>=1;j--)
-             {
-                 if(arr[0][j]==0 || arr[i][0]==0)
-                 {
-                     arr[i][j]=0;
-                 }
-             }
-         }
-         if(row)
-         {
-             for(int j=0;j<arr[0].length;j++)
-             {
-                 arr[0][j]=0;
-             }
-             System.out.println("OK1");
-         }
-         if(col)
-         {
-             for(int i=0;i<arr.length;i++)
-             {
-                 arr[i][0]=0;
-             }
-              System.out.println("OK2");
-         }
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i][0]==0)
+            {
+                firstCol=true;
+                break;
+            }
+        }
 
+        for(int i=1;i<arr.length;i++)
+        {
+            for(int j=1;j<arr[0].length;j++)
+            {
+                if(arr[i][j]==0)
+                {
+                    arr[0][j]=0;
+                    arr[i][0]=0;
+                }
+            }
+        }
 
-         
-
-
-        
+        for(int j=1;j<arr[0].length;j++)
+        {
+            if(arr[0][j]==0)
+            {
+                for(int i=0;i<arr.length;i++)
+                {
+                    arr[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i][0]==0)
+            {
+                for(int j=0;j<arr[0].length;j++)
+                {
+                    arr[i][j]=0;
+                }
+            }
+        }
+        if(firstRow)
+        {
+            for(int j=0;j<arr[0].length;j++)
+            {
+                arr[0][j]=0;
+            }
+        }
+        if(firstCol)
+        {
+            for(int i=0;i<arr.length;i++)
+            {
+                arr[i][0]=0;
+            }
+        }
     }
 }
