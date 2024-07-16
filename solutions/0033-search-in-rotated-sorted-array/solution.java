d5640b253e41ed3af1c6967ad103bbcb566eb79c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-16 02:42:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42 MB (99.99%)

--- solutions/0033-search-in-rotated-sorted-array/solution.java
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,41 +1,26 @@
 class Solution {
-    // Isme hme rotated array dia jyga and ye rotation kse hga wo bataya gya h quesion me so hme rotated array me element index find krna h 
-// Isme Simple Binray Search lgaya jyga.
     public int search(int[] arr, int target) {
-           int start=0;
-        int end= arr.length-1;
-        while(start<=end)
+        
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right)
         {
-            int mid= start+(end-start)/2;
-            if(arr[mid]==target)
+            int mid = left+(right-left)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<arr[right]) // Sorted 
             {
-                return mid;
+                    if(target>arr[mid] && arr[right]>=target) left=mid+1;
+                    else right=mid-1;
             }
-            if(arr[start]<=arr[mid])  // yha hm check kr rhe h ki jis range me h hum h wo given array k left side me h (4 to 7) k beech me 
+            else if(arr[mid]>arr[right])
             {
-                if(target>=arr[start] && target<=arr[mid]) 
-                {
-                    end= mid-1;
-                }
-                else
-                {
-                    start=mid+1;
-                }
+                  if(target>=arr[left]&& target<=arr[mid])right=mid-1;
+                  else left=mid+1;
             }
-            else  // Yha hum right side me h (0 to 2) k beech me h 
-            {
-                if(target>=arr[mid] && target<=arr[end])
-                {
-                    start=mid+1;
-                }
-                else
-                {
-                end= mid-1;;
-                }
-            }
-            
+            else right=mid-1;
+
+
         }
         return -1;
-        
     }
 }
