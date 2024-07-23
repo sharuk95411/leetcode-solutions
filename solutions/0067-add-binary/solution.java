6e852ab0afce88437c10bfd9a5cc0614c9e50a00
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-23 03:28:39 +0000

    Sync LeetCode submission Runtime - 12 ms (6.83%), Memory - 45 MB (8.23%)

--- solutions/0067-add-binary/solution.java
+++ solutions/0067-add-binary/solution.java
@@ -1,35 +1,53 @@
 class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder ans= new StringBuilder();
+    public String addBinary(String a1, String b1) {
+        StringBuilder str= new StringBuilder();
+        String ans="";
 
-int i=a.length()-1;
-int j= b.length()-1;
+        int i= a1.length()-1;
+        int j= b1.length()-1;
+        int carry=0;
+          int sum=0;
 
-int sum=0;
-int carry =0;
-int l= Math.max(i,j);
+        while(i>=0 && j>=0)
+        {
+            int a= Character.getNumericValue(a1.charAt(i));
+            int b= Character.getNumericValue(b1.charAt(j));
 
-for(int k=l;l>=0;l--)
-{
-    int n= (i<0)? 0: Character.getNumericValue(a.charAt(i));
-    int m= (j<0)? 0: Character.getNumericValue(b.charAt(j));  
-     System.out.println("N "+n +" M "+m);
-    sum= carry+m+n;
-      if(sum>1)
-      {
-          ans.append(String.valueOf(sum%2));
-          carry=1;
-      }
-      else
-      {
-          ans.append(String.valueOf(sum));
-          carry=0;
-      }
-      i--;
-      j--;
-}
+            sum=a+b;
+            sum=(sum+carry)%2;
+            char c=(char)(sum+'0');    
+            carry=(a+b+carry)/2;
+            ans=c+ans;
+            i--;
+            j--;
+        }
 
-if(carry>0) ans.append("1");
-return ans.reverse().toString();
+        System.out.println("ANS "+ans);
+        
+        while(i>=0)
+        {
+            int a= Character.getNumericValue(a1.charAt(i));
+            sum=(a+carry)%2;
+            carry=(a+carry)/2;
+        char c=(char)(sum+'0');
+        ans=c+ans;
+            i--;
+        }
+         while(j>=0)
+        {
+            int b= Character.getNumericValue(b1.charAt(j));
+            sum=(b+carry)%2;
+            carry=(b+carry)/2;
+             char c=(char)(sum+'0');
+        ans=c+ans;
+            j--;
+        }
+
+        System.out.println("ANS "+ans);
+
+        if(carry>0) ans='1'+ans;
+        return ans;
+
+
     }
 }
