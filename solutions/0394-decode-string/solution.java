2c22f0314221974ed418859169df99190fb93c10
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-26 04:37:13 +0000

    Sync LeetCode submission Runtime - 4 ms (5.51%), Memory - 41.6 MB (99.99%)

--- solutions/0394-decode-string/solution.java
+++ solutions/0394-decode-string/solution.java
@@ -1,59 +1,67 @@
 class Solution {
     public String decodeString(String s) {
-        if(s.length()==1)
-        {
-            if(Character.isDigit(s.charAt(0))) return "";
-        }
-         Stack<Character> stack = new Stack<>();
-        
-        for(char c : s.toCharArray())
-        {
-            if(c != ']') 
-                stack.push(c); //push everything but ]
-            
-            else 
-            {
-                //step 1: 
-                    //if you find a closing ] then 
-                   //retrieve the string it encapsulates
-                
-                StringBuilder sb = new StringBuilder();
-                while(!stack.isEmpty() && Character.isLetter(stack.peek()))
-                    sb.insert(0, stack.pop());
-                
-                String sub = sb.toString(); //this is the string contained in [ ]
-                stack.pop(); //Discard the '[';
-                
-                
-                //step 2: 
-                    //after that get the number of
-                  // times it should repeat from stack
-                    
-                sb = new StringBuilder();
-                while(!stack.isEmpty() && Character.isDigit(stack.peek()))
-                    sb.insert(0, stack.pop());
-                    
-                int count = Integer.valueOf(sb.toString()); //this is the number
-                
-                
-                //step 3: 
-                    //repeat the string within the [ ] count 
-                  //number of times and push it back into stack
-                
-                while(count > 0)
-                {
-                    for(char ch : sub.toCharArray())  
-                        stack.push(ch);
-                    count--;
-                }
-            }
-        }
-        
-      //final fetching and returning the value in stack 
-        StringBuilder retv = new StringBuilder();
-        while(!stack.isEmpty())
-            retv.insert(0, stack.pop());
 
-        return retv.toString();
+        Stack<Integer>s1= new Stack<>();
+        Stack<String>s2= new Stack<>();
+         
+         int i=0;
+
+         String ans="";
+         while(i<s.length())
+         {
+            char c= s.charAt(i);
+               
+             if(Character.isDigit(c))
+             {
+                int temp=0;
+                int num=0;
+                   while(s.charAt(i)!='[')
+                   {
+                        int getNo= s.charAt(i)-'0';
+                        num= num*10+getNo;
+                        temp++;
+                        i++;
+                   }
+
+                System.out.println("NUM "+num);
+                s1.push(num);
+                i--;
+             }
+             else if(c==']')
+             {
+                   int num= s1.pop();
+                   String temp=s2.pop();
+                   while(!s2.isEmpty()&& !s2.peek().equals("["))
+                   {
+                    //   System.out.println("C "+temp);
+                      temp=s2.pop()+temp; 
+
+                   }
+                //    System.out.println("NOT  "+temp);
+                   if(!s2.isEmpty())s2.pop();
+                   String app=temp;
+                   while(num>1)
+                   {
+                        temp=app+temp;
+                        num--;
+                   }
+                   s2.push(temp);
+                //    System.out.println("C "+temp);
+             }
+             else
+             {
+                String pp=c+"";
+                // System.out.println("pp "+pp);
+                s2.push(pp);
+             }
+            i++;
+         }
+
+       while(!s2.isEmpty())
+       {
+       ans= s2.pop()+ans;
+       }
+
+        return ans;
     }
 }
