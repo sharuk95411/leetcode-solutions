9cecae696010721552551507797fb0fe6bdf68bb
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-07-30 02:27:19 +0000

    Sync LeetCode submission Runtime - 3 ms (2.59%), Memory - 44.3 MB (99.93%)

--- solutions/0002-add-two-numbers/solution.java
+++ solutions/0002-add-two-numbers/solution.java
@@ -9,27 +9,33 @@
  * }
  */
 class Solution {
-   
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
         ListNode dummy= new ListNode(0);
-        ListNode temp1=l1,temp2=l2,curr=dummy;
+        ListNode ans= dummy;
         int carry=0;
-        while(temp1!=null || temp2!=null)
+        int sum=0;
+
+        while(l1!=null || l2!=null)
         {
-            int a= temp1!=null ? temp1.val: 0;
-            int b= temp2!=null ? temp2.val: 0;
-            int sum= carry+a+b;
+            // res=(num1>num2) ? (num1+num2):(num1-num2)
+            int a= l1==null ? 0:l1.val;
+            int b= l2==null ? 0:l2.val;
+            sum=a+b+carry;
             carry= sum/10;
-            curr.next= new ListNode(sum%10);
-            curr= curr.next;
-            if(temp1!=null) temp1= temp1.next;
-            if(temp2!=null) temp2= temp2.next;
+            ListNode a1= new ListNode(sum%10);
+            dummy.next=a1;
+            dummy=a1;
+            if(l1!=null) l1=l1.next;
+            if(l2!=null) l2=l2.next;
+
         }
         if(carry>0)
         {
-            curr.next= new ListNode(carry);
+            System.out.println("working");
+            ListNode b1= new ListNode(carry);
+            dummy.next=b1;
+
         }
-        return dummy.next;
+        return ans.next;
     }
 }
