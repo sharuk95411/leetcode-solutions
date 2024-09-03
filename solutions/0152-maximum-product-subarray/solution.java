18c6f820570bc0cf35dcd8a2c5d70c3d03cbe412
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-09-03 11:19:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.9 MB (99.93%)

--- solutions/0152-maximum-product-subarray/solution.java
+++ solutions/0152-maximum-product-subarray/solution.java
@@ -1,42 +1,36 @@
 class Solution {
     public int maxProduct(int[] arr) {
-        
-        int ans=arr[0];
-        int min=arr[0];
-        int max=arr[0];
-        
-        for(int i=1;i<arr.length;i++)
+        if(arr.length==1) return arr[0];
+        int ans=Integer.MIN_VALUE;
+        int product=1;
+        for(int i=0;i<arr.length;i++)
         {
-            
-            
-//             if(arr[i]==0)
-//             {
-//                 if(i==arr.length-1)return Math.max(ans,arr[i]);
-//                 else
-//                 {
-//                 ans=Math.max(arr[i],ans);
-//                 max=0;
-//                 min=0;
-//                 i++;
-//                 }
-               
-//             }
-            
-            if(arr[i]<0)
-            {
-                int temp= max;
-                max=min;
-                min=temp;
-            }
-            
-            max= Math.max(arr[i]*max,arr[i]);
-            min= Math.min(arr[i]*min,arr[i]);
-            
-            ans=Math.max(max,ans);
-           
+           if(arr[i]==0)
+           {
+             ans= Math.max(ans,arr[i]);
+             product=1;
+           }
+           else
+           {
+            product=product*arr[i];
+            ans= Math.max(ans,product);
+           }
         }
-        
+         product=1;
+        for(int i=arr.length-1;i>=0;i--)
+        {
+           if(arr[i]==0)
+           {
+             ans= Math.max(ans,arr[i]);
+             product=1;
+           }
+           else
+           {
+            product=product*arr[i];
+            ans= Math.max(ans,product);
+           }
+        }
         return ans;
-        
+
     }
 }
