3929426d6b873602b08ef4c72574b801651f6634
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-09-12 02:51:10 +0000

    Sync LeetCode submission Runtime - 2 ms (99.45%), Memory - 43.7 MB (99.97%)

--- solutions/0496-next-greater-element-i/solution.java
+++ solutions/0496-next-greater-element-i/solution.java
@@ -1,35 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        int ans[]= new int[nums1.length];
-        HashMap<Integer,Integer>h = new HashMap<>();
-         Stack<Integer>stack= new Stack<>();
+
+        HashMap<Integer,Integer>h= new HashMap<>();
         for(int i=0;i<nums2.length;i++)
         {
             h.put(nums2[i],i);
         }
-       
-        int n= nums2.length;
-        stack.push(nums2[n-1]);
-        nums2[n-1]=-1;
-        int i=n-2;
-        while(i>=0)
-        {
-            int temp= nums2[i];
-            while(stack.size()>0 && nums2[i]>stack.peek())
+
+          for(int i=0;i<nums1.length;i++)
+          {
+            int index= h.get(nums1[i])+1;
+            while(index<nums2.length)
             {
-                stack.pop();
+                if(nums2[index]>nums1[i])
+                {
+                    nums1[i]=nums2[index];
+                    break;
+                }
+                index++;
             }
-            if(stack.isEmpty()) nums2[i]=-1;
-            else nums2[i]=stack.peek();
-            stack.push(temp);
-            i--;
-        }
-        System.out.println("NEW ARRAY IS "+Arrays.toString(nums2));
-        for(i=0;i<ans.length;i++)
-        {
-            ans[i]=nums2[h.get(nums1[i])];
-        }
-        return ans;
+            if(index>=nums2.length) nums1[i]=-1;
+
+          }
+          return nums1;
     }
 }
