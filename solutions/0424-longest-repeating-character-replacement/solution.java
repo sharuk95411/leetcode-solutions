2b198aa52dec1a8864ff5e8e970485b716aae569
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-10-02 03:32:20 +0000

    Sync LeetCode submission Runtime - 10 ms (56.79%), Memory - 42.3 MB (100.00%)

--- solutions/0424-longest-repeating-character-replacement/solution.java
+++ solutions/0424-longest-repeating-character-replacement/solution.java
@@ -1,41 +1,23 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-       int[] arr = new int[26];  //Will be used to store the number of each char in current window
+        int[] freq = new int[26];
+        int mostFreqLetter = 0;
+        int left = 0;
+        int max = 0;
         
-        int windowStartIndex = 0;       //Starting index of the window
-        int windowEndIndex = 0;         //Ending index of the window
-        
-        int maxNumOfSameCharsInCurrentWindow = 0;           //Max num of same chars in current window
-        
-        int ans = 0;                            //Max length of possible repetitive subsequence
-        
-        for(windowEndIndex = 0; windowEndIndex<s.length(); windowEndIndex++){  //Window will keep expanding to right in each step
-            char newChar = s.charAt(windowEndIndex);               //The newly added char to right because of window expansion
+        for(int right = 0; right < s.length(); right++){
+            freq[s.charAt(right) - 'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
             
-            arr[newChar-'A']++;                                         //Increment the new char count
-            
-            //Check and update max number of same chars in current window
-            maxNumOfSameCharsInCurrentWindow = Math.max(maxNumOfSameCharsInCurrentWindow, arr[newChar-'A']);
-            
-            //Number of other (non-max char) in current window = Window size - num of max chars
-            int numOfOtherChars = (windowEndIndex - windowStartIndex + 1) - maxNumOfSameCharsInCurrentWindow;
-            
-            if(numOfOtherChars<=k){             //Valid window, where the num of max-chars can be maximized by filling gaps
-                
-                //Since we can fill the gaps, the whole window can be filled with max-char. Check and update maxRepSubseqLen
-                ans = Math.max(ans, windowEndIndex - windowStartIndex + 1);    
+            int lettersToChange = (right - left + 1) - mostFreqLetter;
+            if(lettersToChange > k){
+                freq[s.charAt(left) - 'A']--;
+                left++;
             }
-            else {       //Invalid window. Slide one step to right
-                
-                //Decrease count of left char, since we are excluding it from window
-                arr[s.charAt(windowStartIndex)-'A']--;    
-                
-                //Move start of window to one step right
-                windowStartIndex++;
-                
-            }
+            
+            max = Math.max(max, right - left + 1);
         }
         
-        return ans;
+        return max;
     }
 }
