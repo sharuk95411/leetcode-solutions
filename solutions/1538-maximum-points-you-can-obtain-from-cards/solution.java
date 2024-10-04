6785b27d3fa8c84feb5fdf4789a33853fa2d47ac
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-10-04 04:02:24 +0000

    Sync LeetCode submission Runtime - 2 ms (99.73%), Memory - 55.2 MB (99.95%)

--- solutions/1538-maximum-points-you-can-obtain-from-cards/solution.java
+++ solutions/1538-maximum-points-you-can-obtain-from-cards/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public int maxScore(int[] arr, int k) {
+        
+        int n=arr.length;
+        int sum= 0;
         int ans=0;
-        int sum=0;
-        int left=k-1;
-        int right= arr.length-1;
         for(int i=0;i<k;i++)
         {
             sum=sum+arr[i];
         }
-        if(k==arr.length) return sum;
-        ans=sum;
-        while(left>=0)
+        ans= sum;
+         
+        int j=k-1;
+        for(int i=n-1;i>=n-k;i--)
         {
-            sum=sum-arr[left]+arr[right];
-            ans= Math.max(sum,ans);
-            left--;
-            right--;
+            sum=sum-arr[j]+arr[i];
+            ans=Math.max(sum,ans);
+            j--;
         }
         return ans;
     }
