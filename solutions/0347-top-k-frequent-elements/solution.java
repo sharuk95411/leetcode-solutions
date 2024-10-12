5a0988d17b00f7689112d12a6e2e5487b8117c47
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-10-12 08:33:36 +0000

    Sync LeetCode submission Runtime - 16 ms (39.73%), Memory - 45.4 MB (99.99%)

--- solutions/0347-top-k-frequent-elements/solution.java
+++ solutions/0347-top-k-frequent-elements/solution.java
@@ -1,47 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] arr, int k) {
         
-        List<Integer>[ ] freq = new ArrayList[arr.length+1];
-
         HashMap<Integer,Integer>h= new HashMap<>();
-        for(int a :arr)
+        for(int i=0;i<arr.length;i++)
         {
-            h.put(a,h.getOrDefault(a,0)+1);
+            h.put(arr[i],h.getOrDefault(arr[i],0)+1);
         }
 
-        for (Map.Entry<Integer,Integer> m : h.entrySet())
-        {
-         int key = m.getKey();
-         int value = m.getValue();
-        if(freq[value]==null)
-        {
-            List<Integer>l= new ArrayList<>();
-            l.add(key);
-            freq[value]=l;
-        }
-        else
-        {
-                List<Integer>l= freq[value];
-                l.add(key);
-        }
-          }
+   PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> h.get(a) - h.get(b));
 
-             int ans[]= new int[k];
-             for(int i=freq.length-1;i>=0;i--)
-             {
-                 if(freq[i]!=null)
-                 {
-                     List<Integer>l= freq[i];
-                    int j=0;
-                    while(k>0 && j<l.size())
-                    {
-                        ans[k-1]=l.get(j);
-                        j++;
-                        k--;
-                    }
-                 }
-             }
-        return ans;
+                
+for (Map.Entry<Integer,Integer> m : h.entrySet())
+{
+     int key = m.getKey();
+     minHeap.add(key);
+      while(minHeap.size()>k)
+            minHeap.poll();
+
+}
+int ans[]= new int[k];
+  for(int i=0;i<k;i++)
+  {
+      ans[i]= minHeap.poll();
+  }
+  return ans;
 
     }
 }
