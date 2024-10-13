5ef91355be909f65623958a909b021710fd84880
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-10-13 05:40:13 +0000

    Sync LeetCode submission Runtime - 396 ms (5.20%), Memory - 46.5 MB (65.03%)

--- solutions/0451-sort-characters-by-frequency/solution.java
+++ solutions/0451-sort-characters-by-frequency/solution.java
@@ -1,33 +1,32 @@
 class Solution {
     public String frequencySort(String s) {
-        
         HashMap<Character,Integer>h= new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
             char c= s.charAt(i);
             h.put(c,h.getOrDefault(c,0)+1);
         }
+     //  PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> h.get(b) - h.get(a));
+         PriorityQueue<Character>pq= new PriorityQueue<>((a,b)-> h.get(b)-h.get(a));
 
-     PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> h.get(b) - h.get(a));
-
-    for (Map.Entry<Character,Integer> m : h.entrySet())
-        {
-             pq.add(m.getKey());
-          }
-          
-          String ans="";
-          while(pq.size()>0)
+          for (Map.Entry<Character,Integer> m : h.entrySet())
           {
-              int freq= h.get(pq.peek());
-              Character c= pq.poll();
-              while(freq>0)
-              {
-                  ans=ans+c;
-                  freq--;
-              }
-
+            pq.add(m.getKey());
           }
+          String ans="";
+        
+        while(pq.size()>0)
+        {
+            char c= pq.poll();
+            int freq= h.get(c);
+            while(freq>0)
+            {
+                ans= ans+c;
+                freq--;
+            }
+        }
+        return ans;
 
-return ans;
+
     }
 }
