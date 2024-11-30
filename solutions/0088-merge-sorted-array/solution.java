069a6bf1d0d1b45a96e61b2da6924ad01665138c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-11-30 04:49:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (99.98%)

--- solutions/0088-merge-sorted-array/solution.java
+++ solutions/0088-merge-sorted-array/solution.java
@@ -1,37 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int temp[]= new int[nums1.length];
+     int k= nums1.length-1;
+     int i=m-1;
+     int j= nums2.length-1;
 
-            int i=0;
-            int j=0;
-            int k=0;
-            while(j<n && i<m)
-            {
-                if(nums1[i]<=nums2[j])
-                {
-                    temp[k]=nums1[i];
-                    i++;
-                }
-                else
-                {
-                    temp[k]=nums2[j];
-                    j++;
-                }
-                k++;
-            }
-            while(i<m)
-            {
-                temp[k]=nums1[i];
-                k++;
-                i++;
-            }
-            while(j<n)
-            {
-                temp[k]=nums2[j];
-                k++;
-                j++;
-            }
-            System.arraycopy(temp, 0, nums1, 0, nums1.length); 
+     while(i>=0 && j>=0)
+     {
+         if(nums1[i]<=nums2[j])
+         {
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+         }
+         else
+         {
+            nums1[k]=nums1[i];
+            nums1[i]=0;
+             k--;
+            i--;
+         }
+
+     }
+    while(j>=0)
+    {
+      nums1[k]=nums2[j];
+      j--;
+      k--;
+    }
     }
 }
