bb17629b18a51a29b6b4bb35630bc282f97aa94b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-12-02 18:24:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 50 MB (99.21%)

--- solutions/0378-kth-smallest-element-in-a-sorted-matrix/solution.java
+++ solutions/0378-kth-smallest-element-in-a-sorted-matrix/solution.java
@@ -1,15 +1,37 @@
-class Solution {
-    public int kthSmallest(int[][] arr, int k) {
-        List<Integer>list= new ArrayList<>();
-        for(int i=0;i<arr.length;i++)
-{
-    for(int j=0;j<arr.length;j++)
-    {
-        list.add(arr[i][j]);
-    }
-}
-
-Collections.sort(list);
-return list.get(k-1);
+	
+     class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n-1][n-1];
+        while(left < right){
+            int mid = left + (right - left)/2;
+    int count = seachMatrix(matrix,mid);  //find how many numbers are smaller than or equal to  mid element
+            if(count < k){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return left;
+    }  
+  
+  public int seachMatrix(int[][] matrix, int target) {
+        int count = 0;
+        int row = 0;
+        int col = matrix[0].length-1;
+    
+        while(row < matrix.length && col >= 0){
+    if(target>=matrix[row][col])  // thats mean ab wo niche jyega because is row ka last element target se chota or equal h 
+            {
+              count = count + col + 1;
+              row++;
+            }
+            else {
+               col--;
+            }
+        }
+        return count;
     }
 }
