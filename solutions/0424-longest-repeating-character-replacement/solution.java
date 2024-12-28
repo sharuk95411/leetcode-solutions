1e8ba29aebf26f957be6820e7b04f272c9c2b310
sharuk <78816538+sharuk95411@users.noreply.github.com>
2024-12-28 09:53:07 +0000

    Sync LeetCode submission Runtime - 8 ms (67.63%), Memory - 44.6 MB (99.93%)

--- solutions/0424-longest-repeating-character-replacement/solution.java
+++ solutions/0424-longest-repeating-character-replacement/solution.java
@@ -8,11 +8,11 @@ class Solution {
         for(int right = 0; right < s.length(); right++){
             freq[s.charAt(right) - 'A']++;
             mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
-            
             int lettersToChange = (right - left + 1) - mostFreqLetter;
-            if(lettersToChange > k){
+            while(lettersToChange > k){
                 freq[s.charAt(left) - 'A']--;
                 left++;
+              lettersToChange = (right - left + 1) - mostFreqLetter;
             }
             
             max = Math.max(max, right - left + 1);
