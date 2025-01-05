78dcb3ebcf58619a2e05b524036a8247c2968b70
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-05 13:07:20 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 44.5 MB (100.00%)

--- solutions/1184-car-pooling/solution.java
+++ solutions/1184-car-pooling/solution.java
@@ -1,33 +1,20 @@
 class Solution {
-    public boolean carPooling(int[][] arr, int capacity) {
+    public boolean carPooling(int[][] trips, int capacity) {
+        // Because from and to is between 0 and 1000. Idea is to store counts in an array of size 1001.
+        int lengthOfTrip[] = new int[1001];
+        for (int trip[] : trips){
+    lengthOfTrip[trip[1]] = lengthOfTrip[trip[1]]+ trip[0]; // Increment when passenger is on board
+    lengthOfTrip[trip[2]] = lengthOfTrip[trip[2]]-trip[0]; // decrement when they depart
+        }
+		// Count total passenger for each bus top
+        int carLoad = 0;
+        // we have the count array, we perform a line sweep from 0 to 1000 and track its total
+        for (int i = 0; i < 1001; i++){
+            carLoad =carLoad+ lengthOfTrip[i];
+			// Reject when the car is overloaded somewhere
+            if(carLoad > capacity) return false;
+        }
 
-     Arrays.sort(arr,new Comparator<int []>()
-     {
-         public int compare(int[]a,int []b)
-         {
-             return a[1]-b[1];
-         }
-     } );
-     PriorityQueue<int[]>pq= new PriorityQueue<int[]>(new Comparator <int []>()
-     {
-         public int compare(int a[],int b[])
-         {
-             return a[2]-b[2];
-         }
-     }  ); 
-     for(int i=0;i<arr.length;i++)
-     {
-         while(!pq.isEmpty() && pq.peek()[2]<=arr[i][1])
-         {
-             capacity=capacity+pq.peek()[0];
-             pq.poll();
-         }
-         
-         capacity=capacity-arr[i][0];
-         pq.add(arr[i]);
-         if(capacity<0)return false;
-         
-     }  
-return true;
+        return true; // Accept only if all trip is safe
     }
 }
