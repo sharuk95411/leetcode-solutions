a4e88e0f4a883319f040af7774dfeaf125f972c6
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-11 08:10:52 +0000

    Sync LeetCode submission Runtime - 3 ms (30.53%), Memory - 44 MB (99.99%)

--- solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
+++ solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
@@ -13,25 +13,32 @@
  *     }
  * }
  */
-class Solution {
-    int p=0;//preorder array pointer
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper( 0, inorder.length-1, preorder, inorder);
-    }
-    public TreeNode helper( int start, int end, int[] preorder, int[] inorder){
-        if(start>end) return null;
-int j = find(preorder[p], inorder, start, end); //find the root of the present subtree in inorder array
-        TreeNode node = new TreeNode(preorder[p]);//create new node of the sub tree
-        p++;
-        node.left = helper( start, j-1, preorder, inorder);//build left sub tree
-        node.right = helper( j+1, end, preorder, inorder);//build right sub tree
-        return node;
+ class Solution {
+    int index=0;
+    public TreeNode buildTree(int[] pre, int[] in) {
+        
+        return A(pre,in,0,in.length-1);
     }
 
-    public int find(int val, int[] inorder,int start, int end){
-         for (int i = start; i <= end; i++) {
-            if (inorder[i] == val) return i;
+    public TreeNode A (int pre[],int in[],int left,int right)
+    {
+               if(left>right) return null;
+               TreeNode n= new TreeNode(pre[index]);
+               int position= findRootPosn(in,pre[index]);
+               index++;
+               n.left= A(pre,in,left,position-1);
+               n.right=A(pre,in,position+1,right);
+               return n;
+
+    }
+
+    public int findRootPosn(int in[],int element)
+    {
+        for(int i=0;i<in.length;i++)
+        {
+            if(in[i]==element) return i;
         }
-        return -1;
+    return -1;
     }
 }
+
