10d3b26280b72fb40e368f2de85e43558d71c093
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-11 13:19:53 +0000

    Sync LeetCode submission Runtime - 7 ms (99.54%), Memory - 44.9 MB (99.97%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
@@ -9,24 +9,23 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //base case
-        if (root == null || root == p || root == q) {
-            return root;
-        }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        return A(root,p,q);
 
-        //thats mean p and q are lies are different sides
-        if(left!=null && right!= null) {
-            return root;
-        }
-        // thats mean same branch me same path me h 
-        else if(left == null) {
-            return right;
-        }
-        // thats mean same branch me same path me h  
-        else {
-            return left;
-        }
+    }
+    public TreeNode A (TreeNode root, TreeNode p, TreeNode q)
+    {
+        if(root==null) return null;
+        
+        if(root.val==p.val) return p;
+        else if(root.val==q.val) return q;
+
+        root.left= A(root.left,p,q);
+        root.right= A(root.right,p,q);
+
+        if(root.left!=null && root.right!=null)
+        return root;
+        else if(root.left==null) return root.right;
+        else return root.left;
     }
 }
