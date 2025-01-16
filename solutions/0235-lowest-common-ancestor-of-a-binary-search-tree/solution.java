bbfd9b4e6fa49a1b87f9add473ff3db606a7b8a1
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-16 08:10:26 +0000

    Sync LeetCode submission Runtime - 6 ms (97.19%), Memory - 44.8 MB (99.99%)

--- solutions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
+++ solutions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
@@ -9,57 +9,21 @@
  */
 
 class Solution {
-    List<TreeNode>l1= new ArrayList<>();
-    List<TreeNode>l2= new ArrayList<>();
-    boolean b=true;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        A(root,p);
-        b=true;
-        B(root,q);   
-        // System.out.println("L! "+l1);
-        // System.out.println("L2 "+l2);
-        int a=Math.min(l1.size(),l2.size());
         
-        for(int i=0;i<a;i++)
-        {
-             if(l1.get(i).val==l2.get(i).val)
-             {
-                 p=l1.get(i);
-             }
-             else break;
-        }
-        return p;
+        return A(root,p,q);
     }
 
-    public void A(TreeNode root, TreeNode p)
+    public TreeNode A (TreeNode root, TreeNode p, TreeNode q )
     {
-       if(root==null)return ;
-        l1.add(root);
-        if(root.val==p.val)
-        {
-            b=false;
-        }
-        if(b)
-        A(root.left,p);
-        if(b)
-        A(root.right,p);
-        if(b)
-        l1.remove(l1.size()-1);
+        if(root==null) return null;
+        if(root.val==p.val) return p;
+        if(root.val==q.val) return q;
+        root.left= A(root.left,p,q);
+        root.right= A(root.right,p,q);
+        if(root.left!=null && root.right!=null) return root;
+        else if(root.left==null && root.right==null) return null;
+    else if(root.left==null) return root.right;
+        else return root.left;
     }
-     public void B(TreeNode root, TreeNode q)
-    {
-        if(root==null)return ;
-        l2.add(root);
-        if(root.val==q.val)
-        {
-            b=false;
-        }
-        if(b)
-        B(root.left,q);
-        if(b)
-        B(root.right,q);
-        if(b)
-        l2.remove(l2.size()-1);
-        }
-    
 }
