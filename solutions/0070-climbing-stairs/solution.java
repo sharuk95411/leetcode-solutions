f1ededcf0c77743271dd47ddd3626aea04bef5fc
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-17 11:56:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.3 MB (99.98%)

--- solutions/0070-climbing-stairs/solution.java
+++ solutions/0070-climbing-stairs/solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int climbStairs(int n) {
-        
-        int arr[]= new int[n+1];
-        Arrays.fill(arr, -1);
-        return A(0,n,arr);
-    }
-    public int A(int start,int des,int arr[])
-    {
-        if(start>des)return 0;
-        if(start==des) return 1;
-        if(arr[start]!=-1)return arr[start];
 
-        arr[start]= A(start+1,des,arr)+A(start+2,des,arr);
-        return arr[start];
+    public int climbStairs(int n) {
+        int arr[]=new int[n+1];
+        Arrays.fill(arr,-1);
+        return A(n,arr);
+    }
+    public int A(int n,int arr[])
+    {
+        if(n==0) return 1;
+        if(n<0) return 0;
+         if(arr[n]!=-1) return arr[n];
+         int way1= A(n-1,arr);
+         int way2= A(n-2,arr);
+        return  arr[n]=way1+way2;
     }
 }
