4aaa4754c434c6f4e55f9c455ed0bf108945729f
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-17 13:16:35 +0000

    Sync LeetCode submission Runtime - 1 ms (40.14%), Memory - 43.5 MB (99.97%)

--- solutions/0747-min-cost-climbing-stairs/solution.java
+++ solutions/0747-min-cost-climbing-stairs/solution.java
@@ -1,15 +1,21 @@
 class Solution {
+    int ans=Integer.MAX_VALUE;
+    int arr[];
     public int minCostClimbingStairs(int[] cost) {
-        int arr[]= new int[cost.length+1];
+        arr=new int[cost.length+1];
         Arrays.fill(arr,-1);
-        return Math.min(rec(cost, 0,arr), rec(cost, 1,arr));
+       
+        return Math.min(A(cost,0),A(cost,1));
     }
-    
-    int rec(int[] cost, int index,int arr[]) {
-       if(index >= cost.length)
-           return 0;
-        if(arr[index]!=-1)return arr[index];
-        arr[index]=Math.min(cost[index]+rec(cost, index + 1,arr), (cost[index]+rec(cost, index + 2,arr)));
-return arr[index];
+    public int A(int cost[],int index)
+    {
+        if(index>=cost.length)
+        {
+            return 0;
+        }
+        if(arr[index]!=-1) return arr[index];
+         int a= cost[index]+A(cost,index+1);
+         int b= cost[index] + A(cost,index+2);
+         return arr[index]= Math.min(ans,Math.min(a,b));
     }
 }
