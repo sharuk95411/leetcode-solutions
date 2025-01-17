3bfbb9090ef6603406e2976f965cbdbbb0c0e009
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-17 11:27:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.2 MB (100.00%)

--- solutions/1013-fibonacci-number/solution.java
+++ solutions/1013-fibonacci-number/solution.java
@@ -1,9 +1,17 @@
 class Solution {
     public int fib(int n) {
-        
-        if(n==0) return 0;
-        if(n==2 || n==1) return 1;
+        int arr[]= new int[n+1];
+        Arrays.fill(arr,-1);
 
-        return fib(n-1)+fib(n-2);
+        return A(n ,arr);
+    }
+
+    public int A (int n,int arr[])
+    {
+        if(n<=1) return n;
+
+        if(arr[n]!=-1) return arr[n];
+
+        return arr[n]= A(n-1,arr)+A(n-2,arr);
     }
 }
