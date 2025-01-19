ae080b6393b97ac2f6978d863a53e7492d358781
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-19 08:29:14 +0000

    Sync LeetCode submission Runtime - 107 ms (38.59%), Memory - 45.9 MB (32.73%)

--- solutions/0494-target-sum/solution.java
+++ solutions/0494-target-sum/solution.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int findTargetSumWays(int[] arr, int target) {
-        
-        return A(0,arr,target,0);
+    public int findTargetSumWays(int[] nums, int target) {
+        HashMap<String, Integer> memo = new HashMap<>();
+        return solve(nums, target, 0, 0, memo);
     }
-    
-    public int A(int i,int arr[],int target,int sum)
-    {
-        if(i>=arr.length)
-        {
-            if(sum==target) return 1;
-            else return 0;
+
+    private int solve(int[] nums, int target, int i, int sum, HashMap<String, Integer> memo) {
+        if (i == nums.length) {
+            return sum == target ? 1 : 0;
         }
-        
-        int take_plus= A(i+1,arr,target,sum+arr[i]);
-        int take_minus= A(i+1,arr,target,sum-arr[i]);
-        
-        return take_plus+take_minus;
+
+        // Create a unique key for the current state
+        String key = i + "," + sum;  // you can put comma or anyother character
+
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int plus = solve(nums, target, i + 1, sum + nums[i], memo);
+        int minus = solve(nums, target, i + 1, sum - nums[i], memo);
+
+        memo.put(key, plus + minus);
+
+        return memo.get(key);
     }
 }
+
+
