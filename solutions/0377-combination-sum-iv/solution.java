d69a98312494041b55695e1cd8d88893aafc6c92
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-20 19:40:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41 MB (100.00%)

--- solutions/0377-combination-sum-iv/solution.java
+++ solutions/0377-combination-sum-iv/solution.java
@@ -1,29 +1,32 @@
 class Solution {
-    // int ans;
-    List<Integer>l= new ArrayList<>();
-    public int combinationSum4(int[] arr, int target) {
-        int dp[]=new int[target+1];
-        Arrays.fill(dp,-1);
-        Arrays.sort(arr);
-        return A(0,target,arr,dp);
-        
-    }
     
-    public int A(int sum,int target,int arr[],int dp[])
+    int[] t;
+    public int combinationSum4(int[] arr, int target) {
+          t = new int[target+1];
+        Arrays.fill(t,-1);
+        Arrays.sort(arr);
+        return A(arr,target);
+             
+    }
+    public int A(int arr[],int target)
     {
-         if(sum==target)
-         {
-            return 1;
-         }
-        if(sum>target) return 0;
-        if(dp[sum]!=-1)return dp[sum];
-        int ans=0;
-        
+        if(target==0) 
+        {
+            // System.out.println("WORKING");
+           return 1;
+        }
+       
+        if(target<0) return 0;
+         if(t[target] != -1) {
+            return t[target];
+        }
+         int ans=0;
         for(int i=0;i<arr.length;i++)
         {
-            ans=ans+A(sum+arr[i],target,arr,dp);        
+            ans=ans+A(arr,target-arr[i]);
+            
+            
         }
-        return dp[sum]=ans;
-        
+         return t[target] = ans;
     }
 }
