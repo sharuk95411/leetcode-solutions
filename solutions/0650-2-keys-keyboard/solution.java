396c5b9ec2c244ba62e16332b91f10c708742004
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-20 18:47:22 +0000

    Sync LeetCode submission Runtime - 17 ms (25.00%), Memory - 40.9 MB (99.86%)

--- solutions/0650-2-keys-keyboard/solution.java
+++ solutions/0650-2-keys-keyboard/solution.java
@@ -1,19 +1,19 @@
 class Solution {
+
     public int minSteps(int n) {
-        int dp[][]= new int[n+1][n+1];
-        return solve(n, 1, 0,dp);
+        if(n==1) return 0;
+
+        return 1+ A(1,1,n);
     }
-    public int solve(int n, int length, int clipboard,int dp[][]){
-        if(length == n)
-            return 0;
-        if(length > n)
-            return Integer.MAX_VALUE/2 ;
-        if(dp[length][clipboard]!=0) return dp[length][clipboard];
-        int copyAndPaste = Integer.MAX_VALUE , paste = Integer.MAX_VALUE ;
-        if(clipboard > 0)
-          paste = 1 + solve(n, length + clipboard, clipboard,dp);
-        copyAndPaste = 2 + solve(n, length + length, length,dp);
-        
-        return dp[length][clipboard]=Math.min(copyAndPaste, paste);
+
+    public int A (int currA,int clipA, int n)
+    {
+        if(currA>n) return 10000;
+        if(currA==n) return 0;
+
+         int paste= 1+A(currA+clipA,clipA,n);
+        int copyAndPaste= 1+1+A(currA+currA,currA,n);
+       
+        return Math.min(copyAndPaste,paste);
     }
 }
