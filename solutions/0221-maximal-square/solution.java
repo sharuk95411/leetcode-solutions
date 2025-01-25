4301af96a05abcaf0adb9e4ba95372157bd83b11
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-25 09:00:34 +0000

    Sync LeetCode submission Runtime - 12 ms (13.51%), Memory - 60.5 MB (100.00%)

--- solutions/0221-maximal-square/solution.java
+++ solutions/0221-maximal-square/solution.java
@@ -1,40 +1,49 @@
+
 class Solution {
-    int dp[][] = null;
-    public int maximalSquare(char[][] matrix) {
-		if (matrix.length == 0) {
-			return 0;
-		}
-		dp = new int[matrix.length][matrix[0].length];
-		for (int i = 0; i < dp.length; i++) {
-			Arrays.fill(dp[i], -1);
-		}
-		int h = matrix.length;
-		int l = matrix[0].length;
-		int max = 0;
+int memo[][];
+   public int maximalSquare(char[][] arr)
+    {
+        
+        memo= new int[arr.length+1][arr[0].length];
+          for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                memo[i][j] = -1; 
+            }
+        }
+             int ans=0;
 
-		int curMax = 0;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < l; j++) {
-				curMax = findSquare(matrix, i, j);
-				if (max < curMax) {
-					max = curMax;
-				}
-			}
-		}
-		return max * max;
-	}
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[0].length;j++)
+            {
+                if(arr[i][j]=='1')
+                {
+                    ans=Math.max(ans, A(arr,i,j));
+                }
+            }
+        }
+        return ans*ans;
+    }
+    public int A(char arr[][],int i,int j)
+    {
+        if(i>=arr.length || j>=arr[0].length) return 0;
+       if(memo[i][j]!=-1) return memo[i][j]; 
+        int right= A(arr,i,j+1);
+        int diagonal=A(arr,i+1,j+1);
+         int down= A(arr,i+1,j);
+        
+        if(arr[i][j]=='1')
+        {
+               
+                 return memo[i][j]= 1+Math.min(right,Math.min(down,diagonal));
+        }
+       
+        else return memo[i][j]= 0;
 
-	private int findSquare(char[][] matrix, int i, int j) {
-		if (i >= matrix.length || j >= matrix[0].length) {
-			return 0;
-		}
-		if (matrix[i][j] == '0') {
-			return dp[i][j] = 0;
-		}
-		if (dp[i][j] != -1) {
-			return dp[i][j];
-		}
-		int sum = 1 + Math.min(findSquare(matrix, i + 1, j + 1), Math.min(findSquare(matrix, i + 1, j), findSquare(matrix, i, j + 1)));
-		return dp[i][j] = sum;
-	}
+        // return memo[i][j];
+         
+
+    }
 }
+
+
