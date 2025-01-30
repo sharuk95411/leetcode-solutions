d23815078d90ddf360a0cdf30e4cc1d6f73c7aac
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-01-30 19:28:09 +0000

    Sync LeetCode submission Runtime - 5 ms (55.94%), Memory - 46.8 MB (99.86%)

--- solutions/0658-find-k-closest-elements/solution.java
+++ solutions/0658-find-k-closest-elements/solution.java
@@ -1,33 +1,23 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
-        
-        List<Integer>list= new ArrayList<>();
+        List<Integer>ans= new ArrayList<>();
         int i=0;
-        int j= arr.length-1;
+        int j=arr.length-1;
         while(j-i>=k)
         {
-            int a= Math.abs(arr[i]-x);
-            int b = Math.abs(arr[j]-x);
-
+            int a =Math.abs(arr[i]-x);
+            int b =Math.abs(arr[j]-x);
             if(a==b)
-            {
-                  if(arr[i]<arr[j])
-                  j--;
-                  else i++;
-            }
-            else if (a>b)
-            {
-                i++;
-            }
+                j--;
+            else if(a>b)i++;
             else j--;
         }
         while(i<=j)
         {
-        list.add(arr[i]);
-        i++;
+            ans.add(arr[i]);
+            i++;
         }
-        return list;
-        
+        return ans;
     }
 }
