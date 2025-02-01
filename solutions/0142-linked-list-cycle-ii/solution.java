e6a0b27c9b49d0753dde01334acd1c98b2287f4c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-02-01 06:19:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.4 MB (99.98%)

--- solutions/0142-linked-list-cycle-ii/solution.java
+++ solutions/0142-linked-list-cycle-ii/solution.java
@@ -11,18 +11,30 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head,fast=head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast){
-                slow = head;
-                while(slow != fast){
-                    slow = slow.next;
-                    fast = fast.next;
-                }
-                return slow;
+        if(head==null ||head.next==null)return null;
+        ListNode slow=head,fast=head;
+        slow=slow.next;
+        fast=fast.next.next;
+        boolean b=false;
+        while(fast!=null && fast.next!=null)
+        {
+            if(slow==fast)
+            {
+                b=true;
+                slow=head;
+                break;
             }
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(b)
+        {
+            while(slow!=fast)
+            {
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
         }
         return null;
     }
