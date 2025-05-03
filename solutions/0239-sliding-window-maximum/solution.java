75690772caa518293d03d88456fd9167fc218cae
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-03 15:20:29 +0000

    Sync LeetCode submission Runtime - 28 ms (90.29%), Memory - 58.6 MB (100.00%)

--- solutions/0239-sliding-window-maximum/solution.java
+++ solutions/0239-sliding-window-maximum/solution.java
@@ -1,33 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
-       ArrayDeque<Integer> q = new ArrayDeque<>(); 
-        int i=0, j=0, ptr=0; 
-        int n = nums.length; 
-        int[] res = new int[n-k+1]; 
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
 
-        while(j<n){
-            while(!q.isEmpty() && q.peekLast()<nums[j]){
-                q.pollLast(); 
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // Step 1: Remove elements from back smaller than current value
+            while (!dq.isEmpty() && dq.peekLast() < nums[i]) {
+                dq.pollLast();
             }
-        
-            q.add(nums[j]);
 
-            if(j-i+1<k){
-                j++; 
-            }else if(j-i+1==k){
+            // Step 2: Add current value
+            dq.offerLast(nums[i]);
 
-                res[ptr++] = q.peek();
-                
-                if(nums[i]==q.peek()){
-                    q.pollFirst(); 
-                }
-                i++; 
-                j++;
+            // Step 3: Remove element that’s now out of window from the front
+            // ONLY if it's equal to nums[i - k]
+            if (i >= k && dq.peekFirst() == nums[i - k]) {
+                dq.pollFirst();
+            }
+
+            // Step 4: Store the max in result (front of deque)
+            if (i >= k - 1) {
+                res[i - k + 1] = dq.peekFirst();
             }
         }
 
-        return res; 
-
+        return res;
     }
 }
+
