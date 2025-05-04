91c579972280763dd8851d61ed51836fa205a4f7
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-04 15:14:30 +0000

    Sync LeetCode submission Runtime - 7 ms (32.00%), Memory - 44 MB (100.00%)

--- solutions/1184-car-pooling/solution.java
+++ solutions/1184-car-pooling/solution.java
@@ -1,20 +1,45 @@
-class Solution {
-    public boolean carPooling(int[][] trips, int capacity) {
-        // Because from and to is between 0 and 1000. Idea is to store counts in an array of size 1001.
-        int lengthOfTrip[] = new int[1001];
-        for (int trip[] : trips){
-    lengthOfTrip[trip[1]] = lengthOfTrip[trip[1]]+ trip[0]; // Increment when passenger is on board
-    lengthOfTrip[trip[2]] = lengthOfTrip[trip[2]]-trip[0]; // decrement when they depart
-        }
-		// Count total passenger for each bus top
-        int carLoad = 0;
-        // we have the count array, we perform a line sweep from 0 to 1000 and track its total
-        for (int i = 0; i < 1001; i++){
-            carLoad =carLoad+ lengthOfTrip[i];
-			// Reject when the car is overloaded somewhere
-            if(carLoad > capacity) return false;
-        }
+class A
+{
+    int pass ,drop;
 
-        return true; // Accept only if all trip is safe
+    A(int a, int b)
+    {
+        pass=a;
+        drop=b;
+    }
+}
+
+class Solution {
+    public boolean carPooling(int[][] arr, int capacity) {
+        
+         PriorityQueue<A> minHeap = new PriorityQueue<>(new Comparator<A>() {
+            public int compare(A x, A y) {
+                return Integer.compare(x.drop, y.drop); // ascending order of b
+            }
+        });
+
+        for(int i=0;i<arr.length;i++)
+        {
+            minHeap.add(new A(arr[i][0],arr[i][2]));
+        }
+         
+         Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));
+        int cap= arr[0][0];
+        if(cap>capacity) return false;
+
+        for(int i=1;i<arr.length;i++)
+        {
+            int pick = arr[i][1];
+            A obj= minHeap.peek();
+            while(!minHeap.isEmpty()&&pick>=obj.drop)
+            {
+                  cap= cap-obj.pass;
+                  minHeap.poll();
+                  obj= minHeap.peek();
+            }
+            cap= cap+arr[i][0];
+            if(cap>capacity) return false;
+        }
+        return true;
     }
 }
