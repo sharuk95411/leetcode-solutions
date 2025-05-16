b9a5e195abfc95111cab9eefa52292fb94d57b90
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-16 16:00:50 +0000

    Sync LeetCode submission Runtime - 140 ms (48.92%), Memory - 41.3 MB (99.97%)

--- solutions/0079-word-search/solution.java
+++ solutions/0079-word-search/solution.java
@@ -1,43 +1,42 @@
 class Solution {
-    public boolean exist(char[][] arr, String s) {
+    boolean b= false;
+    boolean check[][];
+    public boolean exist(char[][] arr, String word) {
         
-        boolean bu[][]= new boolean [arr.length][arr[0].length];
-        for(int i=0;i<arr.length;i++)
+        int row=arr.length;
+        int col= arr[0].length;
+           check=new boolean[row][col];
+        for(int i=0;i<row;i++)
         {
-            for(int j=0;j<arr[0].length;j++)
+            for(int j=0;j<col;j++)
             {
-                   char c= arr[i][j];
-                   if(c==s.charAt(0))
-                   {
-                        boolean b= A(arr,s,i,j,0,bu);
-                        if(b)return true;
-                   }
+                if(arr[i][j]==word.charAt(0))
+                {
+                     A(arr,i,j,word,0);
+                     if(b) return true;
+                }
             }
         }
+       
         return false;
     }
 
-    private boolean A(char arr[][],String s,int i,int j,int index,boolean isVisit[][])
+    public void A(char arr[][],int i,int j, String word,int count)
     {
-        if (i<0 || i>=arr.length || j<0 || j>=arr[0].length)
-        return false;
-        if(isVisit[i][j]==true) return false;
-
-        char c= s.charAt(index);
-        if(c==arr[i][j] && index==s.length()-1) return true;
-        else if(c==arr[i][j])
-        {
-             isVisit[i][j]=true;
-            Boolean a = A(arr,s,i,j+1,index+1,isVisit);
-          Boolean b=  A(arr,s,i+1,j,index+1,isVisit);
-           Boolean m = A(arr,s,i,j-1,index+1,isVisit);
-           Boolean n= A(arr,s,i-1,j,index+1,isVisit);
-           isVisit[i][j]=false;
-           return((a||b)||(m||n));
-        }
-        else 
-        return false;
         
-
+         if(count==word.length())
+        {
+            b=true;
+            return ;
+        }
+      
+       else if(i>=arr.length || j>=arr[0].length ||i<0 || j<0) return;
+        else if(arr[i][j]!=word.charAt(count)|| check[i][j]==true) return;
+        check[i][j]=true;
+        A(arr,i,j+1,word,count+1);
+        A(arr,i,j-1,word,count+1);
+        A(arr,i+1,j,word,count+1);
+        A(arr,i-1,j,word,count+1);
+         check[i][j]=false;
     }
 }
