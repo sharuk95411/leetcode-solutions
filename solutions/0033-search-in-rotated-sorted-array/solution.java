fe41e6b1283c79892fe93a2739198cc232b95496
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-17 08:30:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (99.95%)

--- solutions/0033-search-in-rotated-sorted-array/solution.java
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,26 +1,32 @@
 class Solution {
+    int ans=-1;
     public int search(int[] arr, int target) {
-        
-        int left=0;
-        int right=arr.length-1;
-        while(left<=right)
+         
+         A(arr,0,arr.length-1,target);
+         return ans;
+    }
+
+    public void A(int arr[],int left,int right,int target)
+    {
+        if(left>right) return;
+        int mid = left+(right-left)/2;
+        if(arr[mid]==target)
         {
-            int mid = left+(right-left)/2;
-            if(arr[mid]==target) return mid;
-            else if(arr[mid]<arr[right]) // Sorted 
-            {
-                    if(target>arr[mid] && arr[right]>=target) left=mid+1;
-                    else right=mid-1;
-            }
-            else if(arr[mid]>arr[right])
-            {
-                  if(target>=arr[left]&& target<=arr[mid])right=mid-1;
-                  else left=mid+1;
-            }
-            else right=mid-1;
-
-
+            ans=mid;
+            return;
         }
-        return -1;
+
+        if(arr[mid]>=arr[left])
+        {
+            if(arr[left]<=target && arr[mid]>=target)
+            A(arr,left,mid-1,target);
+            else A(arr,mid+1,right,target);
+        }
+        else
+        {
+            if(arr[mid]<=target && arr[right]>=target)
+            A(arr,mid+1,right,target);
+            else A(arr,left,mid-1,target);
+        }
     }
 }
