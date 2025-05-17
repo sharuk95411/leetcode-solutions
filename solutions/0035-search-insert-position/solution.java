2b8172584c3d16b8b49df2c9ee0df0883fd8856a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-17 17:13:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (99.97%)

--- solutions/0035-search-insert-position/solution.java
+++ solutions/0035-search-insert-position/solution.java
@@ -1,17 +1,31 @@
 class Solution {
+       
+       int ans=0;
+
     public int searchInsert(int[] arr, int target) {
         
-        
-        int left=0;
-        int right= arr.length-1;
-        
-        while(left<=right)
+        // if(target>arr[arr.length-1]) return arr.length;
+        // if(arr[0]>target) return 0;
+          A(arr,0,arr.length-1,target);
+          return ans;
+    }
+    public void A(int arr[],int left,int right, int target)
+    {
+        if(left>right)
         {
-            int mid= left+(right-left)/2;
-            if(arr[mid]==target)return mid;
-            if(target>arr[mid])left=mid+1;
-            else right=mid-1;
+            ans= left;
+            return ;
         }
-        return left;
+
+        int mid= left+(right-left)/2;
+
+        if(arr[mid]==target)
+        {
+            ans= mid;
+            return ;
+        }
+        else if(target>arr[mid]) A(arr,mid+1,right,target);
+        else A(arr,left,mid-1,target);
+        
     }
 }
