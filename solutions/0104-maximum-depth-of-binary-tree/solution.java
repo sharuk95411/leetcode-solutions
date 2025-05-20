1c0bb9e29a9576d023c92192814616ad9b120c6e
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-20 17:43:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (99.99%)

--- solutions/0104-maximum-depth-of-binary-tree/solution.java
+++ solutions/0104-maximum-depth-of-binary-tree/solution.java
@@ -14,23 +14,11 @@
  * }
  */
 class Solution {
-    int ans=Integer.MIN_VALUE;
     public int maxDepth(TreeNode root) {
-         
-         A(root,0);
-         return ans;
-    }
+        if(root==null) return 0;
+         int left=maxDepth(root.left);
+         int right= maxDepth(root.right);
 
-    public void A(TreeNode root,int h)
-    {
-
-      if(root==null)
-      {
-        ans=Math.max(h,ans);
-        return ;
-      }
-      A(root.left,h+1);
-      A(root.right,h+1);
-
+         return 1+Math.max(left,right);
     }
 }
