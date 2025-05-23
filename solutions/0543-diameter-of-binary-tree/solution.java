fe70ff3157829d6e8a2a2678a67c7bdf4e498965
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-23 13:05:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.8 MB (99.99%)

--- solutions/0543-diameter-of-binary-tree/solution.java
+++ solutions/0543-diameter-of-binary-tree/solution.java
@@ -14,30 +14,20 @@
  * }
  */
 class Solution {
-    int ans=0;
-    
 
+int ans=0;
     public int diameterOfBinaryTree(TreeNode root) {
         
-         A(root);
-         return ans;
-    }
+        A(root);
+        return ans;
 
-    public int A(TreeNode root)
+    }
+    public int A (TreeNode root)
     {
-        if(root.left==null && root.right==null) return 1;
-          int left=0;
-          int right=0;
-        if(root.left!=null)
-        {
-            left= A(root.left);
-        }
-        if(root.right!=null)
-        {
-             right =A(root.right);
-        }
+        if(root==null) return 0;
+        int left= A(root.left);
+        int right= A(root.right);
         ans= Math.max(ans,left+right);
-        return 1+Math.max(left,right);
-         
+        return  1+Math.max(left,right);
     }
 }
