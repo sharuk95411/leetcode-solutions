b5af27d2a434ec8badf87b64b761ab9fbb8b0852
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-24 17:24:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.1 MB (99.95%)

--- solutions/0062-unique-paths/solution.java
+++ solutions/0062-unique-paths/solution.java
@@ -1,27 +1,27 @@
 class Solution {
-    int ans=0;
+
+
     public int uniquePaths(int m, int n) {
         
-int dp[][]=new int [m+1][n+1];
- for (int[] row : dp)
-{
-  Arrays.fill(row, -1);
-}
-        return A(0,0,m,n,dp);
-        
+       Integer[][] arr = new Integer[m+1][n+1];
+
+          return A(m-1,n-1,arr);
+          
     }
-    public int A(int i,int j,int row,int col,int dp[][])
+
+    public int A(int row, int col ,Integer arr[][])
     {
-         if(i>=row || j>=col) return 0;
-        if(i==row-1 && j==col-1)
+        if(row<0 || col<0) return 0  ;
+        if(arr[row][col]!=null) return arr[row][col];
+
+        if(row==0 && col ==0) 
         {
-            
-            return 1;
+            return 1 ;
         }
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-       int way1=A(i,j+1,row,col,dp);
-       int way2= A(i+1,j,row,col,dp);
-        return dp[i][j]=way1+way2;
+
+       int a= A(row-1,col,arr);
+        int b= A(row,col-1,arr);
+
+        return arr[row][col]=a+b;
     }
 }
