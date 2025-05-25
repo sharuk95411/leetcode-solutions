fd159dffea9bce5db3b1ca36ebaa8ef208dc8d87
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-25 07:05:03 +0000

    Sync LeetCode submission Runtime - 5 ms (68.96%), Memory - 45 MB (97.31%)

--- solutions/0072-edit-distance/solution.java
+++ solutions/0072-edit-distance/solution.java
@@ -1,31 +1,33 @@
-class Solution {
-    public int minDistance(String s1, String s2) {
-        
-        int dp[][]=new int[s1.length()+1][s2.length()+1];
-        for (int[] row : dp)
-{
-  Arrays.fill(row, -1);
-}
-        if(s1.length()==0)return s2.length();
-        if(s2.length()==0)return s1.length();
-        return A(s1,s2,0,0,dp);
+
+   class Solution {
+
+      Integer arr[][];
+
+    public int minDistance(String w1, String w2) {
+         arr= new Integer[w1.length()][w2.length()];
+        return A(w1,w2,0,0);
     }
-    public int A(String s1,String s2,int i,int j,int dp[][])
-        
+
+    public int A(String w1,String w2,int i ,int j)
     {
+        if(j>=w2.length())  return w1.length()-i;
+        else if (i>=w1.length())  return w2.length()-j;
+        else if (arr[i][j]!=null) return arr[i][j];
         
-        if(j==s2.length()) return s1.length()-i;
-        if(i>=s1.length())return s2.length()-j;
-        
-        if(s1.charAt(i)==s2.charAt(j))
+   int way1=Integer.MAX_VALUE,way2=Integer.MAX_VALUE,way3=Integer.MAX_VALUE;
+        if(w1.charAt(i)==w2.charAt(j))
         {
-          return dp[i][j] = A(s1,s2,i+1,j+1,dp);
+              return A(w1,w2,i+1,j+1);
         }
-        if(dp[i][j]!=-1)return dp[i][j];
-        int e= 1+A(s1,s2,i,j+1,dp);
-        int d= 1+A(s1,s2,i+1,j,dp);
-        int rep= 1+A(s1,s2,i+1,j+1,dp);
+        else
+        {
+        way1 = 1+A(w1,w2,i,j+1);  // insert 
+        way2 = 1+A(w1,w2,i+1,j);  // delete
+        way3= 1+A(w1,w2,i+1,j+1);  // replace
+        }
+       
         
-        return dp[i][j]= Math.min(e,Math.min(d,rep));
+    
+        return arr[i][j]= Math.min(way1, Math.min(way2,way3));
     }
 }
