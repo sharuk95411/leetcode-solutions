e2b30b59b5f794847eb8a94f833a396bc155320c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-05-28 08:38:35 +0000

    Sync LeetCode submission Runtime - 1 ms (93.11%), Memory - 45.7 MB (99.90%)

--- solutions/0152-maximum-product-subarray/solution.java
+++ solutions/0152-maximum-product-subarray/solution.java
@@ -1,36 +1,19 @@
 class Solution {
-    public int maxProduct(int[] arr) {
-        if(arr.length==1) return arr[0];
-        int ans=Integer.MIN_VALUE;
-        int product=1;
-        for(int i=0;i<arr.length;i++)
-        {
-           if(arr[i]==0)
-           {
-             ans= Math.max(ans,arr[i]);
-             product=1;
-           }
-           else
-           {
-            product=product*arr[i];
-            ans= Math.max(ans,product);
-           }
-        }
-         product=1;
-        for(int i=arr.length-1;i>=0;i--)
-        {
-           if(arr[i]==0)
-           {
-             ans= Math.max(ans,arr[i]);
-             product=1;
-           }
-           else
-           {
-            product=product*arr[i];
-            ans= Math.max(ans,product);
-           }
-        }
-        return ans;
+    public int maxProduct(int[] nums) {
+        return helper(nums, 1, nums[0], nums[0], nums[0]);
+    }
 
+     private static int helper(int[] nums, int index, int currentMax, int currentMin, int result) {
+        if (index == nums.length) {
+            return result;
+        }
+
+        int num = nums[index];
+
+        int tempMax = Math.max(num, Math.max(currentMax * num, currentMin * num));
+        int tempMin = Math.min(num, Math.min(currentMax * num, currentMin * num));
+        int tempResult = Math.max(result, tempMax);
+
+        return helper(nums, index + 1, tempMax, tempMin, tempResult);
     }
 }
