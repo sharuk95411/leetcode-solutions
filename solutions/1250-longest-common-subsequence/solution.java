89d43413727fad7b35ac15c3de85ba050da9f075
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-06-01 05:55:18 +0000

    Sync LeetCode submission Runtime - 60 ms (5.47%), Memory - 76.2 MB (5.05%)

--- solutions/1250-longest-common-subsequence/solution.java
+++ solutions/1250-longest-common-subsequence/solution.java
@@ -1,30 +1,28 @@
 class Solution {
     
+    Integer memo[][];
     public int longestCommonSubsequence(String s1, String s2) {
         
-        int dp[][]=new int[s1.length()+1][s2.length()+1];
-        
- for (int[] row : dp)
-{
-  Arrays.fill(row, -1);
-}
-        return A(s1,s2,0,0,dp);
-        
+        memo= new Integer[s1.length()][s2.length()];
+        return A(0,0,s1,s2);
+      
     }
-    
-    public int A(String s1,String s2, int i,int j,int dp[][])
+
+    public int A(int i,int j,String s1,String s2)
     {
-        if(i>=s1.length() || j>=s2.length())return 0;
-        if(dp[i][j]!=-1)return dp[i][j]; 
-        int ans=0;
+        if(i>=s1.length()|| j>=s2.length())
+        {
+            return 0 ;
+        }
+        if(memo[i][j]!=null) return memo[i][j];
+        int a=0,b=0,c=0;
         if(s1.charAt(i)==s2.charAt(j))
         {
-            ans=1+A(s1,s2,i+1,j+1,dp);
+           a=1+ A(i+1,j+1,s1,s2);
         }
-         else
-            ans=Math.max(A(s1,s2,i+1,j,dp),A(s1,s2,i,j+1,dp));
-         
-        return dp[i][j]=ans ;
-        
+        b= A(i+1,j,s1,s2);
+        c=A(i,j+1,s1,s2);
+
+        return memo[i][j]= Math.max(a,Math.max(b,c));
     }
 }
