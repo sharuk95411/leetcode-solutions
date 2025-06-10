be6ac6fa5665a2bfe087815838703e579d720d53
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-06-10 18:43:50 +0000

    Sync LeetCode submission Runtime - 14 ms (70.75%), Memory - 49.4 MB (7.87%)

--- solutions/0347-top-k-frequent-elements/solution.java
+++ solutions/0347-top-k-frequent-elements/solution.java
@@ -1,42 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer,Integer>h= new HashMap<>();
-        for(int a:nums)
-        {
-            h.put(a,h.getOrDefault(a,0)+1);
-          
-        }
+       HashMap<Integer,Integer>h= new HashMap<>();
+       
+       for(int a :nums)
+       {
+        h.put(a,h.getOrDefault(a,0)+1);
+       }
 
-        List<Integer>[] arr = new ArrayList[nums.length+1];
-         int ans[]=new int[k];
-         for(Map.Entry<Integer,Integer>map: h.entrySet())
-         {
-            int index= map.getValue();
-            if(arr[index]==null)
-            {
-                arr[index]= new ArrayList<>();
-            }
-                arr[index].add(map.getKey());
-         }
+    
 
-         int j = arr.length-1;
-         int i=0;
-         while(i<ans.length && j>=0)
-         {
-            if(arr[j]!=null)
-            {
-                List<Integer>l= arr[j];
-                for(int a:l)
-                {
-                    ans[i]=a;
-                    i++;
-                }
-            }
-            j--;
-         }
+     PriorityQueue<Map.Entry<Integer, Integer>> pq =
+            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
 
-       return ans;
-        
+      pq.addAll(h.entrySet());
+
+        int ans[]= new int[k];
+
+          int i=0;
+          while(k>0)
+          {
+             Map.Entry<Integer, Integer> entry = pq.poll();
+            ans[i++]=entry.getKey();
+            k--;
+          }
+          return ans;
+       
     }
 }
