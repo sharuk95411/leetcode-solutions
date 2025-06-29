52f68aef3edef82213a44abab53a45fc452f34ff
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-06-29 06:47:57 +0000

    Sync LeetCode submission Runtime - 12 ms (51.64%), Memory - 45.1 MB (99.93%)

--- solutions/0438-find-all-anagrams-in-a-string/solution.java
+++ solutions/0438-find-all-anagrams-in-a-string/solution.java
@@ -1,35 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-    int freq1[] = new int[26];
-   int freq2[] = new int[26];
-    List<Integer> list = new ArrayList<>();
-    
-    if(s.length()<p.length())
-        return list;
-    
-    for(int i=0; i<p.length(); i++){
-        freq1[s.charAt(i)-'a']++;
-        freq2[p.charAt(i)-'a']++;
-    }
- 
-    int start=0;
-    int end=p.length();
-    
-    if(Arrays.equals(freq1,freq2))
-        list.add(start);
-    
-    while(end<s.length()){
         
-        freq1[s.charAt(start)-'a']--;
-        freq1[s.charAt(end)-'a']++;
-        
-        if(Arrays.equals(freq1,freq2))
-        list.add(start+1);
-        
-        start++;
-        end++;
-    }
-  return list;  
-}
-    }
+        int arr[]= new int[26];
 
+        for(int i=0;i<p.length();i++)
+        {
+            char c= p.charAt(i);
+            arr[c-'a']++;
+        }
+        int i=0;
+        int j=0;
+        int count =0;
+        List<Integer>list= new ArrayList<>();
+        while(j<s.length())
+        {
+            char c= s.charAt(j);
+            arr[c-'a']--;
+            if(arr[c-'a']>=0) count++;
+        
+                  if(count==p.length()) list.add(i);
+                  if(j-i+1==p.length())
+                  {
+                    c= s.charAt(i);
+                    arr[c-'a']++;
+                    if(arr[c-'a']>0) count--;
+                    i++;
+                  }       
+            j++;
+        }
+        return list;
+    }
+}
