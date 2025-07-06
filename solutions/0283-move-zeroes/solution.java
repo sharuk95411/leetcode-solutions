ba94b13c58b19a07d9d2aa629ef14d298625c3ea
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-07-06 12:22:02 +0000

    Sync LeetCode submission Runtime - 2 ms (92.17%), Memory - 46.2 MB (99.92%)

--- solutions/0283-move-zeroes/solution.java
+++ solutions/0283-move-zeroes/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public void moveZeroes(int[] arr) {
         
-        int i=0;
-        while(i<arr.length && arr[i]!=0)
-        {
-            i++;
-        }
-        
-        for(int j=i+1;j<arr.length;j++)
+        int i =0;
+        int j=0;
+        while(j<arr.length)
         {
             if(arr[j]!=0)
             {
-                arr[i]=arr[j];
-                arr[j]=0;
+                if(arr[i]==0)
+                {
+                   arr[i]=arr[j];
+                    arr[j]=0;
+                }
                 i++;
+               
             }
+            j++;
         }
-        
     }
 }
