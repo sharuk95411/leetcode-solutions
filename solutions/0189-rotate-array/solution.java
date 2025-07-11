3c5f863241b22c6aab26fb4f3fbd8973ddd29f3b
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-07-11 17:52:36 +0000

    Sync LeetCode submission Runtime - 2 ms (19.88%), Memory - 57.6 MB (99.88%)

--- solutions/0189-rotate-array/solution.java
+++ solutions/0189-rotate-array/solution.java
@@ -1,49 +1,40 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        
-    k= k%nums.length;
-        
-        // Rotate he whole Array
-        
-        rotate_whole(nums);
-        rotate_left(nums,0,k-1);
-        rotate_right(nums,k,nums.length-1);
-        
-    }
-    private void rotate_whole(int arr[])
-    {
-        int i=0;
-        int j= arr.length-1;
-        while(i<j)
+    public void rotate(int[] arr, int k) {
+         k= k%arr.length;
+
+        int left=0;
+        int right= arr.length-1;
+
+        while(left<right)
         {
-            int temp =arr[i];
-            arr[i]= arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
+            swap(left,right,arr);
+            left++;
+            right--;
         }
-    }
-    
-    private void rotate_left(int arr[],int i,int j)
-    {
-        while(i<j)
+
+        left=0;
+        right= k-1;
+        while(left<right)
         {
-            int temp =arr[i];
-            arr[i]= arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
+            swap(left,right,arr);
+            left++;
+            right--;
         }
-    }
-    private void rotate_right(int arr[],int i,int j)
-    {
-        while(i<j)
+
+        left=k;
+        right= arr.length-1;
+        while(left<right)
         {
-            int temp =arr[i];
-            arr[i]= arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
+            swap(left,right,arr);
+            left++;
+            right--;
         }
+
+    }
+    public void swap(int left,int right,int arr[])
+    {
+        int temp= arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
     }
 }
