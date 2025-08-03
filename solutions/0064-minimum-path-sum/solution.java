71665021da0f98348cc19118698992bafdf8585a
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-03 11:39:59 +0000

    Sync LeetCode submission Runtime - 2 ms (78.70%), Memory - 46.7 MB (99.96%)

--- solutions/0064-minimum-path-sum/solution.java
+++ solutions/0064-minimum-path-sum/solution.java
@@ -1,33 +1,24 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        // Fill dp array with -1 to indicate uncalculated cells
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return helper(0, 0, grid, dp);
+
+Long memo[][];
+    public int minPathSum(int[][] arr) {
+        
+        memo= new Long[arr.length][arr[0].length];
+             int ans =(int)A(arr,0,0);
+             return ans;
     }
+    public long A(int arr[][],int i,int j)
+    {
 
-    public int helper(int row, int col, int[][] grid, int[][] dp) {
-        if (row >= grid.length || col >= grid[0].length) {
-            return Integer.MAX_VALUE;
-        }
 
-        if (row == grid.length - 1 && col == grid[0].length - 1) {
-            return grid[row][col];
-        }
+       if(i==arr.length && j==arr[0].length-1) return 0;
+       else if(i==arr.length-1 && j==arr[0].length) return 0;
 
-        if (dp[row][col] != -1) {
-            return dp[row][col]; // Already calculated
-        }
-
-        int down = helper(row + 1, col, grid, dp);
-        int right = helper(row, col + 1, grid, dp);
-
-        dp[row][col] = grid[row][col] + Math.min(down, right);
-        return dp[row][col];
+        if(i>=arr.length || j>=arr[0].length) return Integer.MAX_VALUE;
+      if(memo[i][j]!=null) return memo[i][j];
+        
+        long a= (long)arr[i][j]+A(arr,i+1,j);
+        long b = (long)arr[i][j]+A(arr,i,j+1);
+        return  memo[i][j]=Math.min(a,b);
     }
 }
-
