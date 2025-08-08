8a76e20773a7923f448058c55bb290866211dc9c
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-08 14:25:35 +0000

    Sync LeetCode submission Runtime - 5 ms (2.67%), Memory - 42.3 MB (98.42%)

--- solutions/0394-decode-string/solution.java
+++ solutions/0394-decode-string/solution.java
@@ -1,67 +1,32 @@
 class Solution {
     public String decodeString(String s) {
+        
+         Stack<Integer> countStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+        String currentString = "";
+        int k = 0;
 
-        Stack<Integer>s1= new Stack<>();
-        Stack<String>s2= new Stack<>();
-         
-         int i=0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');  // Handle multi-digit numbers
+            } else if (ch == '[') {
+                countStack.push(k);               // Push the repeat count
+                stringStack.push(currentString);  // Push the string so far
+                currentString = "";               // Reset for new block
+                k = 0;                             // Reset count
+            } else if (ch == ']') {
+                int repeatTimes = countStack.pop();
+                String lastString = stringStack.pop();
+                String temp = "";
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp += currentString;       // Repeat the current string
+                }
+                currentString = lastString + temp; // Combine with previous string
+            } else {
+                currentString += ch;             // Build the current string
+            }
+        }
 
-         String ans="";
-         while(i<s.length())
-         {
-            char c= s.charAt(i);
-               
-             if(Character.isDigit(c))
-             {
-                int temp=0;
-                int num=0;
-                   while(s.charAt(i)!='[')
-                   {
-                        int getNo= s.charAt(i)-'0';
-                        num= num*10+getNo;
-                        temp++;
-                        i++;
-                   }
-
-                System.out.println("NUM "+num);
-                s1.push(num);
-                i--;
-             }
-             else if(c==']')
-             {
-                   int num= s1.pop();
-                   String temp=s2.pop();
-                   while(!s2.isEmpty()&& !s2.peek().equals("["))
-                   {
-                    //   System.out.println("C "+temp);
-                      temp=s2.pop()+temp; 
-
-                   }
-                //    System.out.println("NOT  "+temp);
-                   if(!s2.isEmpty())s2.pop();
-                   String app=temp;
-                   while(num>1)
-                   {
-                        temp=app+temp;
-                        num--;
-                   }
-                   s2.push(temp);
-                //    System.out.println("C "+temp);
-             }
-             else
-             {
-                String pp=c+"";
-                // System.out.println("pp "+pp);
-                s2.push(pp);
-             }
-            i++;
-         }
-
-       while(!s2.isEmpty())
-       {
-       ans= s2.pop()+ans;
-       }
-
-        return ans;
+        return currentString;
     }
 }
