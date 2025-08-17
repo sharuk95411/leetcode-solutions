b2abc391a08338a6c86b8c2dc492b61f096f7212
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-17 05:56:14 +0000

    Sync LeetCode submission Runtime - 1 ms (98.55%), Memory - 45.8 MB (99.98%)

--- solutions/0073-set-matrix-zeroes/solution.java
+++ solutions/0073-set-matrix-zeroes/solution.java
@@ -1,63 +1,57 @@
 class Solution {
     public void setZeroes(int[][] arr) {
         
-     boolean firstrow=false;
-     boolean firstcol=false;
-     int row= arr[0].length;
+        boolean firstRow=false;
+        boolean firstCol= false;
 
-         for(int i=0;i<arr.length;i++)
-         {
+        for(int i=0;i<arr.length;i++)
+        {
             for(int j=0;j<arr[0].length;j++)
             {
-                if(arr[i][j]==0)
-                {
-                    if(i==0) firstrow=true;
-                    if(j==0) firstcol= true;
-                    arr[i][0]=0;
-                    arr[0][j]=0;
-                }
+                      if(i==0 && arr[i][j]==0) firstRow=true;
+                      if(j==0 && arr[i][j]==0) firstCol=true;
+                      else if(arr[i][j]==0)
+                      {
+                        arr[i][0]=0;
+                        arr[0][j]=0;
+                      }
             }
-         }
+        }
 
-         for(int i=1;i<arr[0].length;i++)
-         {
-            if(arr[0][i]==0)
-            {
-                System.out.println("rowwise "+i);
-                for(int j=0;j<arr.length;j++)
+        for(int j=1;j<arr[0].length;j++)
+        {
+                if(arr[0][j]==0)
                 {
-                    arr[j][i]=0;
+                    setColZero(0,j,arr);
                 }
-            }
-         }
+        }
 
-         for(int j=1;j<arr.length;j++)
-         {
-            if(arr[j][0]==0)
-            { 
-               System.out.println("colwise "+j);
-                 for(int i=0;i<arr[0].length;i++)
-                {
-                    arr[j][i]=0;
-                }
-            }
-         }
-         if(firstrow)
-         {
-            System.out.println("FirstRow true");
-            for(int i=0;i<arr[0].length;i++)
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i][0]==0)
             {
-                arr[0][i]=0;
+                setRowZero(i,0,arr);
             }
-         }
-         if(firstcol)
-         {
-             System.out.println("FirstCol true");
-            for(int j=0;j<arr.length;j++)
-            {
-                arr[j][0]=0;
-            }
-         }
-
+        }
+        if(firstRow)  setRowZero(0,0,arr);
+        if(firstCol) setColZero(0,0,arr);
     }
+  
+  public void setColZero(int i,int j,int arr[][] )
+  {
+                 while(i<arr.length)
+                 {
+                    arr[i][j]=0;
+                    i++;
+                 }
+  }
+  public void setRowZero(int i,int j,int arr[][])
+  {
+            while(j<arr[0].length)
+            {
+                arr[i][j]=0;
+                j++;
+            }
+  }
+
 }
