abe8393417b87a146ae90cc834193d7e80ba7ce8
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-17 06:50:16 +0000

    Sync LeetCode submission Runtime - 45 ms (5.05%), Memory - 49.9 MB (34.45%)

--- solutions/0049-group-anagrams/solution.java
+++ solutions/0049-group-anagrams/solution.java
@@ -1,32 +1,42 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] arr) {
         
-       HashMap<String,List<String>>h= new HashMap<>();
-       
-       for(int i=0;i<arr.length;i++)
-       {
+        HashMap<String,List<String>>h= new HashMap<>();
 
-               char[] chars = arr[i].toCharArray();
+        for(int i=0;i<arr.length;i++)
+        {
+            char[] chars = arr[i].toCharArray();
+
+        // Sort the char array
         Arrays.sort(chars);
-        String s = new String(chars);
 
-        if(h.containsKey(s))
-        {
-            h.get(s).add(arr[i]);
-        }
-        else
-        {
-            List<String>l= new ArrayList<>();
-             l.add(arr[i]);
-             h.put(s,l);
-        }
-                 
-       }
-       List<List<String>>ans = new ArrayList<>();
+        // Convert back to string
+        String key = new String(chars);
+          
+          if(h.containsKey(key))
+          {
+                List<String>list= h.get(key);
+                list.add(arr[i]);
+                h.put(key,list);
 
-         for (Map.Entry<String, List<String>> entry : h.entrySet()) {
-            ans.add(entry.getValue());
-         }
-         return ans;
+          }
+          else
+          {
+
+            List<String>list= new ArrayList<>();
+            list.add(arr[i]);
+            h.put(key,list);
+          }
+
+        }
+
+    List<List<String>>ans= new ArrayList<>();
+    for(List<String>inner:h.values())
+    {
+        System.out.println(inner);
+        ans.add(new ArrayList<>(inner));
     }
+    return ans;
+    }
+    
 }
