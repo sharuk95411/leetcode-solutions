9c2a8c7ddc36a0efb3bc849cd5823b8f29eeddf1
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-22 06:10:21 +0000

    Sync LeetCode submission Runtime - 5 ms (5.34%), Memory - 47.7 MB (99.96%)

--- solutions/0143-reorder-list/solution.java
+++ solutions/0143-reorder-list/solution.java
@@ -9,47 +9,35 @@
  * }
  */
 class Solution {
-    // Logic CodeBix And its Awesome llogic and code b mstt likha h 
-   // isme mid 3 pointers ki help se nikala gya h jo ki mstt way h jse cycle detect me krte h
-    // LinkedList revserse krne k liye 3 pointers use hte h and isme reverse hgi list.
-    public void reorderList(ListNode head) {
-        if(head == null || head.next == null) return;
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = head;
 
-        while(fast != null && fast.next != null){
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        prev.next = null;
-        ListNode l1 = head;
-        ListNode l2 = reverse(slow);
-        merge(l1, l2);    
-    }
+ 
+    public void reorderList(ListNode head) {
+           HashMap<Integer,ListNode>h=new HashMap<>();
+           int count=1;
+           ListNode temp= head;
+           while(temp!=null)
+           {
+                     h.put(count++,temp);
+                     temp=temp.next;
+           }
+
+           count= count-1;
+
+           int steps= count/2;
+
+           while(steps>0)
+           {
+                temp= head.next;
+                head.next= h.get(count--);
+                if(head.next!=temp) 
+                {
+                    head.next.next=temp;           
+                }
+                head=temp;
+                steps--;
+           }
     
-    private static ListNode reverse(ListNode l2){
-        if(l2 == null) return null;
-        ListNode prev = null;
-        ListNode curr = l2;
-        ListNode next = l2.next;
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-    
-    private static void merge(ListNode l1, ListNode l2) {
-        while (l2 != null) {
-            ListNode next = l1.next;
-            l1.next = l2;
-            l1 = l2;
-            l2 = next;
-        }
+           head.next=null;
         
     }
 }
