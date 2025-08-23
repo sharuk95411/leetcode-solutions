24d1debe25b93d07bcc3469cfa12d6b93b9ebee4
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-23 11:03:15 +0000

    Sync LeetCode submission Runtime - 33 ms (73.90%), Memory - 51.7 MB (100.00%)

--- solutions/0015-3sum/solution.java
+++ solutions/0015-3sum/solution.java
@@ -1,25 +1,46 @@
-// logic codebix
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-         Set<List<Integer>> res  = new HashSet<>();
-        if(nums.length==0) return new ArrayList<>(res);
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-2;i++){
-            int j =i+1;
-           int  k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0)
-                {     res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+    public List<List<Integer>> threeSum(int[] arr) {
+        
+        Arrays.sort(arr);
+        List<List<Integer>>ans= new ArrayList<>();
+       int i=0;
+
+        while(i<arr.length-2)
+        {
+            if(i>0 && arr[i]==arr[i-1])
+            {
+                i++;
+                continue;
+            }
+           int j=i+1;
+           int k= arr.length-1;
+           while(j<arr.length-1 && j<k)
+           {
+              int sum= arr[i]+arr[j]+arr[k];
+              if(sum==0)
+              {
+                 List<Integer>inner = new ArrayList<>();
+                 inner.add(arr[i]);
+                 inner.add(arr[j]);
+                 inner.add(arr[k]);
+                    ans.add(new ArrayList<>(inner));
+        
                 j++;
                 k--;
-                }
-                else if ( sum >0) k--;
-                else if (sum<0) j++;
-            }
-
+                while(j<k && arr[j]==arr[j-1]) j++;
+                while(k>j && arr[k]==arr[k+1])k--;
+              }
+              else if(sum<0)
+              {
+                   j++;
+              }
+              else
+              {
+                k--;
+              }
+           }
+           i++;
         }
-        return new ArrayList<>(res);
-        
+        return ans;
     }
 }
