2c299ee5908882ab7461c0f519c8420c04e88851
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-08-24 07:22:36 +0000

    Sync LeetCode submission Runtime - 5 ms (86.62%), Memory - 44.6 MB (93.34%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.java
+++ solutions/0003-longest-substring-without-repeating-characters/solution.java
@@ -1,28 +1,28 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        
         if(s.length()==0) return 0;
-        int ans=0;
-        HashMap<Character,Integer>h= new HashMap<>();
         int i=0;
         int j=1;
-       h.put(s.charAt(i),0);
+        int ans=0;
+
+        HashMap<Character,Integer>h= new HashMap<>();
+        h.put(s.charAt(0),0);
+
         while(j<s.length())
         {
-           char c= s.charAt(j);
-           if(h.containsKey(c))
-           {
-                int index= h.get(c);
-               if(index>=i)
-               {
-                   ans=Math.max((j-i),ans);
-                   i=index+1;
-               }
-
-           }
-           h.put(c,j);
-           j++;
+            char c= s.charAt(j);
+ // h.get(c)>=i ye case niche "abba" case lena smjh aa jyga ise q lia gya h            
+            if(h.containsKey(c) && h.get(c)>=i)
+            {
+               ans=Math.max(ans,j-i);
+               i = h.get(c)+1;
+            }
+              h.put(c,j);
+              j++;
         }
-        ans=Math.max((j-i),ans);
-        return ans;
+
+        return Math.max(ans,j-i);
+
     }
 }
