c8ae9026d826ae31f29c6e242f39eea21cb2cdde
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-09-01 11:02:45 +0000

    Sync LeetCode submission Runtime - 315 ms (7.95%), Memory - 45.4 MB (99.98%)

--- solutions/0055-jump-game/solution.java
+++ solutions/0055-jump-game/solution.java
@@ -1,29 +1,27 @@
 class Solution {
-    Boolean check[];
+    
+    Boolean memo[];
     public boolean canJump(int[] arr) {
         
-         check= new Boolean[arr.length+1];
-        if(arr.length==1) return true;
-        return A(0,arr);
+        memo= new Boolean[arr.length];
+        return A(arr,0);
+        
     }
-    public boolean A(int i,int arr[])
+    public boolean A(int arr[],int index)
     {
-        if(i>=arr.length-1)
+        if(index>=arr.length) return false;
+        else if(index==arr.length-1) return true;
+
+    if(memo[index]!=null) return memo[index];
+        int steps= arr[index];
+        for(int i=1;i<=steps;i++)
         {
-            return true;
-        }
-        if(check[i]!=null) return check[i];
-        int steps= arr[i];
-        boolean b=false;
-        for(int j=1;j<=steps;j++)
-        {
-            if(A(i+j,arr))
+            if(A(arr,i+index)) 
             {
-                b=true;
-                break;
+                     return memo[index]=true;
             }
             
         }
-        return check[i]= b;
+        return memo[index]=false;
     }
 }
