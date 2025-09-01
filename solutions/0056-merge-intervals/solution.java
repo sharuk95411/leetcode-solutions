16edfed09bab8bf83565feb5d6412d8a29d1a1dd
sharuk <78816538+sharuk95411@users.noreply.github.com>
2025-09-01 11:41:38 +0000

    Sync LeetCode submission Runtime - 8 ms (91.64%), Memory - 46.9 MB (99.98%)

--- solutions/0056-merge-intervals/solution.java
+++ solutions/0056-merge-intervals/solution.java
@@ -1,49 +1,45 @@
 class Solution {
+
     class Pair
     {
         int a,b;
-        Pair(int i,int j)
+        Pair(int row,int col)
         {
-            a=i;
-            b=j;
+            a=row;
+            b=col;
         }
     }
     public int[][] merge(int[][] arr) {
-
-             Arrays.sort(arr, (a, b) ->  Integer.compare(a[0],b[0]));
-        List<Pair>list= new ArrayList<>();
-        list.add(new Pair(arr[0][0],arr[0][1]));
-         
         
+        List<Pair>l= new ArrayList<>();
+        Arrays.sort(arr, (a, b) ->  Integer.compare(a[0],b[0]));
+        int start= arr[0][0];
+        int end= arr[0][1];
 
         for(int i=1;i<arr.length;i++)
         {
-            int right= list.get(list.size()-1).b;
-             if(arr[i][1]>right)
-             {
-                 if(arr[i][0]>right)
-                 {
-                   list.add(new Pair(arr[i][0],arr[i][1]));
-                 }
-                 else
-                 {
-                    int left=  list.remove(list.size()-1).a;     
-                    list.add(new Pair(left,arr[i][1]));
-                 }
-             }
-             
+            if(end>=arr[i][0])
+            {
+                end= Math.max(arr[i][1],end);
+            }
+            else
+            {
+                l.add(new Pair(start,end));
+                start=arr[i][0];
+                end= arr[i][1];
+            }
         }
-        int ans[][]= new int[list.size()][2];
-        int i=0;
-        while(i<list.size())
-        {
-            int left= list.get(i).a;
-            int right= list.get(i).b;
+        l.add(new Pair(start,end));
 
-            ans[i][0]=left;
-            ans[i][1]=right;
-            i++;
+        int ans[][]= new int[l.size()][2];
+        for(int i=0;i<l.size();i++)
+        {
+            Pair p=l.get(i);
+            ans[i][0]=p.a;
+            ans[i][1]=p.b;
         }
         return ans;
+
+
     }
 }
