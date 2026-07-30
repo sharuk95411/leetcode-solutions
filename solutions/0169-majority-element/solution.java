de9c1a0034a428e4914e3fc9c4957c23a6422901
sharuk <78816538+sharuk95411@users.noreply.github.com>
2026-07-30 18:42:49 +0000

    Sync LeetCode submission Runtime - 1 ms (99.88%), Memory - 55.4 MB (69.18%)

--- solutions/0169-majority-element/solution.java
+++ solutions/0169-majority-element/solution.java
@@ -1,19 +1,26 @@
 class Solution {
-    public int majorityElement(int[] arr) {
-        int ans= arr[0];
-        int count=1;
+    public int majorityElement(int[] nums) {
+        
+        int freq=1;
+        int ans=nums[0];
 
-        for(int i=1;i<arr.length;i++)
+        for(int i=1;i<nums.length;i++)
         {
-            if(ans==arr[i]) count++;
+            if(nums[i]==ans)
+            {
+                   freq++;
+            }
             else
             {
-                count--;
-                if(count==0)
-                {
-                    ans=arr[i];
-                    count++;
-                }
+                 if(freq==0)
+                 {
+                     ans=nums[i];
+                     freq=1;
+                 }
+                 else
+                 {
+                  freq--;
+                 }
             }
         }
         return ans;
