fe51876d8b3869e48bd0e72693965e0e3bee81bd
sharuk <78816538+sharuk95411@users.noreply.github.com>
2026-08-02 10:43:48 +0000

    Sync LeetCode submission Runtime - 2 ms (96.30%), Memory - 46 MB (18.50%)

--- solutions/0039-combination-sum/solution.java
+++ solutions/0039-combination-sum/solution.java
@@ -1,25 +1,28 @@
 class Solution {
-
     List<List<Integer>>ans= new ArrayList<>();
+    List<Integer>inner = new ArrayList<>();
+
     public List<List<Integer>> combinationSum(int[] arr, int target) {
         
-        List<Integer>list= new ArrayList<>();
-        Arrays.sort(arr);
-        A(arr,target,0,list);
+        A(arr,target,0);
         return ans;
     }
-    public void A(int arr[],int sum,int i,List<Integer>list)
-    {
-        if(i>=arr.length|| sum<0) return ;
 
-        if(sum==0)
+    public void A(int arr[],int target,int i)
+    {
+        if(target==0)
         {
-            ans.add(new ArrayList<>(list));
-           return ;
-        } 
-        list.add(arr[i]);
-        A(arr,sum-arr[i],i,list);
-        list.remove(list.size()-1);
-        A(arr,sum,i+1,list);
+            ans.add(new ArrayList<>(inner));
+            return;
+        }
+        else if(target<0 || i==arr.length) return ;
+        else
+        {
+            inner.add(arr[i]);
+            A(arr,target-arr[i],i);
+            inner.remove(inner.size()-1);
+            A(arr,target,i+1);
+        }
+
     }
 }
