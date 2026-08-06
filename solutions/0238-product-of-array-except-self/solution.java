8543f31dc2f4645d6646c370c2121fb5d3dc00b0
sharuk <78816538+sharuk95411@users.noreply.github.com>
2026-08-06 02:08:32 +0000

    Sync LeetCode submission Runtime - 31 ms (2.83%), Memory - 75.6 MB (6.59%)

--- solutions/0238-product-of-array-except-self/solution.java
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -1,35 +1,34 @@
 class Solution {
     public int[] productExceptSelf(int[] arr) {
         
-        int pre[]= new int[arr.length];
-        int no =1;
-        pre[0]=1;
-        for(int i=1;i<arr.length;i++)
-        {
-            no = no*arr[i-1];
-            pre[i]=no;
-        }
-         
-         System.out.println("Pre "+Arrays.toString(pre));
-        int post[]=new int[arr.length];
+        int left[]= new int[arr.length];
+        int right[]= new int[arr.length];
+        int ans[]= new int[arr.length];
 
-         no=1;
-         post[arr.length-1]=1;
-         for(int i=arr.length-2;i>=0;i--)
-        {
-        no=no*arr[i+1];
-        post[i]=no;
-        }
-         System.out.println("Post "+Arrays.toString(post));
-          
-          int ans[]= new int[arr.length];
-         
-         for(int i=0;i<arr.length;i++)
-         {
-            ans[i]= pre[i]*post[i];
-         }
-         return ans;
+       int temp=arr[0];
+       left[0]= 1;
+       for(int i=1;i<arr.length;i++)
+       {
+          left[i]= temp;
+          temp=temp*arr[i];
+       }
 
-        
+    System.out.println("left Array "+Arrays.toString(left));
+       temp=arr[arr.length-1];
+       right[arr.length-1]=1;
+       for(int i=arr.length-2;i>=0;i--)
+       {
+             right[i]= temp;
+             temp=temp*arr[i];
+       }
+
+        System.out.println("Right Array "+Arrays.toString(right));
+
+       for(int i=0;i<arr.length;i++)
+       {
+        ans[i]= left[i]*right[i];
+       }
+       return ans;
+
     }
 }
