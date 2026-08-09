a82f1e1ce88a241728b9a1d56e10398a920f79f1
sharuk <78816538+sharuk95411@users.noreply.github.com>
2026-08-09 05:25:12 +0000

    Sync LeetCode submission Runtime - 2 ms (89.91%), Memory - 44.3 MB (24.98%)

--- solutions/1036-rotting-oranges/solution.java
+++ solutions/1036-rotting-oranges/solution.java
@@ -1,71 +1,72 @@
-class Solution {
-    
-    class Pair
+
+class Pair{
+
+    int row,col;
+    Pair(int i ,int j)
     {
-        int i,j;
-        Pair(int a,int b)
-        {
-            i=a;
-            j=b;
-        }
+        row=i;
+        col=j;
     }
+}
+
+class Solution {
     public int orangesRotting(int[][] arr) {
-        
+
         Queue<Pair>q= new LinkedList<>();
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr[0].length;j++)
-            {
-                if(arr[i][j]==2)q.add(new Pair(i,j));
-            }
-        }
-        int time=0;
-        while(!q.isEmpty())
-        { 
-            boolean b=false;
-            int size=q.size();
-         while(size>0)
-        {
-            Pair p=q.poll();
-            int i=p.i;
-            int j=p.j;
-            if(j+1<arr[0].length && arr[i][j+1]==1)
-            {
-                arr[i][j+1]=2;
-                q.add(new Pair(i,j+1));
-                b=true;
-            }
-            if(j-1>=0 && arr[i][j-1]==1)
-            {
-                arr[i][j-1]=2;
-                q.add(new Pair(i,j-1));
-                 b=true;
-            }
-            if(i+1<arr.length && arr[i+1][j]==1)
-            {
-                arr[i+1][j]=2;
-                q.add(new Pair(i+1,j));
-                 b=true;
-            }
-            if(i-1>=0 && arr[i-1][j]==1)
-            {
-                arr[i-1][j]=2;
-                q.add(new Pair(i-1,j));
-                 b=true;
-            }
-            size--;
-        }
-            if(b)time++;
-        }
+        int ans=0;
          for(int i=0;i<arr.length;i++)
+         {
+            for(int j=0;j<arr[0].length;j++)
+            {
+                if(arr[i][j]==2)
+                q.add(new Pair(i,j));
+            }
+         }
+             Boolean b=true;
+                   while(b)
+                    {     
+                       boolean find=false;
+                         int size= q.size();
+                         int left=0,right=0,top=0,bottom=0;
+                         while(size>0)
+                         {
+                            Pair p= q.poll();
+                            int row=p.row;
+                            int col= p.col;
+                             left= A(row,col-1,arr,q);
+                             right = A(row,col+1,arr,q);
+                             top = A(row-1,col,arr,q);
+                             bottom = A(row+1,col,arr,q);
+                            if(left==1 || right==1 || top ==1 || bottom ==1)
+                            {
+                                find=true;
+                            }
+                             size--;
+                            
+                         }
+                          if(find)ans++;
+                         else b=false;
+                    }
+                    for(int i=0;i<arr.length;i++)
         {
             for(int j=0;j<arr[0].length;j++)
             {
-                if(arr[i][j]==1)return -1;
+                if(arr[i][j]==1) return -1;
             }
         }
+        return ans;
         
+    }
+     public int A(int row,int col,int arr[][],Queue<Pair>q)
+    {
         
-        return time;
+        if(row<0 || row==arr.length || col<0 || col==arr[0].length) return 0;
+        if(arr[row][col]==1)
+        {
+            q.add(new Pair(row,col));
+            arr[row][col]=-1;
+            return 1;
+        }
+        return 0;
     }
 }
